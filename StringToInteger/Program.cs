27ff68851aa97a76de238d3995c12c7511180c47
thoradrik <thoradrik@gmail.com>
2018-06-25 23:46:01 +0500

    Solved https://leetcode.com/problems/string-to-integer-atoi/description/

---
+++ StringToInteger/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Quiz {
+    public static class Program {
+        
+        private static void Main(string[] args) {
+            Solution solution = new Solution();
+            
+            Check(solution, "12345", 12345);
+            Check(solution, "+12345", 12345);
+            Check(solution, "-12345", -12345);
+            Check(solution, "+12345-", 12345);
+            Check(solution, "+12345+", 12345);
+            Check(solution, "-12345+", -12345);
+            Check(solution, "-12345-", -12345);
+            Check(solution, "+-12345", 0);
+            Check(solution, "-+12345", 0);
+            Check(solution, "- 12345", 0);
+            Check(solution, "- +12345", 0);
+            Check(solution, "+ -12345", 0);
+            Check(solution, "  12345", 12345);
+            Check(solution, "  +12345", 12345);
+            Check(solution, "  -12345", -12345);
+            Check(solution, "  12345 word", 12345);
+            Check(solution, "  +12345 word", 12345);
+            Check(solution, "  -12345 word", -12345);
+            Check(solution, "  +12.345 word", 12);
+            Check(solution, "  -12.345 word", -12);
+            Check(solution, "  +.345 word", 0);
+            Check(solution, "  -.345 word", 0);
+            Check(solution, "word  +.345 word", 0);
+            Check(solution, "word  -.345 word", 0);
+            Check(solution, "-12345678912345678", Int32.MinValue);
+            Check(solution, "12345678912345678", Int32.MaxValue);
+
+        }
+
+        private static void Check(Solution solution, string str, int check) {
+            int result = solution.MyAtoi(str);
+            if (result != check) {
+                Console.WriteLine("'{0}' got {1} expected {2}", str, result, check);
+            }
+        }
+
+    }
+}
---
+++ StringToInteger/Solution.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Quiz {
+    public class Solution {
+
+        private const int STATE_WHITESPACE = 0;
+        private const int STATE_PLUSMINUS = 1;
+        private const int STATE_NUMBER = 2;
+        
+        public int MyAtoi(string str) {
+            char[] cs = str.ToCharArray();
+
+            int status = STATE_WHITESPACE;
+            bool sign = true;
+            long result = 0;
+
+            for (int i = 0; i < cs.Length; i++) {
+                char c = cs[i];
+
+                if (c == ' ') {
+                    if (status == STATE_WHITESPACE) {
+                        status = STATE_WHITESPACE;
+                    } else if (status == STATE_NUMBER) {
+                        break;
+                    } else {
+                        return 0;
+                    }
+                } else if (c == '-') {
+                    if (status == STATE_WHITESPACE) {
+                        sign = false;
+                        status = STATE_PLUSMINUS;
+                    } else if (status == STATE_NUMBER) {
+                        break;
+                    } else {
+                        return 0;
+                    }
+                } else if (c == '+') {
+                    if (status == STATE_WHITESPACE) {
+                        sign = true;
+                        status = STATE_PLUSMINUS;
+                    } else if (status == STATE_NUMBER) {
+                        break;
+                    } else {
+                        return 0;
+                    }
+                } else if (c >= '0' && c <= '9') {
+                    if (status == STATE_WHITESPACE || status == STATE_PLUSMINUS || status == STATE_NUMBER) {
+                        int n = c - '0';
+                        result = result * 10 + n;
+                       
+                        if (result > Int32.MaxValue) {
+                            if (sign) {
+                                return Int32.MaxValue;
+                            } else {
+                                return Int32.MinValue;
+                            }
+                        }
+                        
+                        status = STATE_NUMBER;
+                    } else {
+                        return 0;
+                    }
+                } else {
+                    break;
+                }
+            }
+            
+            return (int)(sign ? result : -result);
+        }
+        
+    }
+}
