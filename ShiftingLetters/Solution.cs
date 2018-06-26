e95ac9737857e4e99adcb1c21e57f8a969fe7503
thoradrik <thoradrik@gmail.com>
2018-06-26 13:49:25 +0500

    Solved https://leetcode.com/problems/shifting-letters/description/

---
+++ ShiftingLetters/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public string ShiftingLetters(string str, int[] shifts) {
+            int len = str.Length;
+            
+            long[] sum = new long[len];
+
+            sum[len - 1] = shifts[len - 1];
+
+            for (int i = len - 2; i >= 0; i--) {
+                sum[i] = sum[i + 1] +  shifts[i];
+            }
+
+            char[] sb = new char[len];
+
+            for (int i = 0; i < len; i++) {
+                sb[i] = (char)((str[i] + sum[i] - 'a') % 26 + 'a');
+            }
+
+            return new string(sb);
+        }  
+        
+    }
+}
