fe50007f7fc3ff43f5be7f50780b14631f1ebb1a
thoradrik <thoradrik@gmail.com>
2018-10-26 17:07:28 +0500

    Solved https://leetcode.com/problems/prime-palindrome/description/

---
+++ PrimePalindrome/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Quiz {
+    public static class Program {
+        
+        private static void Main(string[] args) {
+            Solution solution = new Solution();
+            
+            solution.BuildPrimes();
+            solution.CheckPalyndromes();
+
+
+        }
+
+    }
+}
