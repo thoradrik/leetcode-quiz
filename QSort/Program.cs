e22d0b99439bf5444b20a54c4ac8bbfddac79021
thoradrik <thoradrik@gmail.com>
2020-11-17 22:28:51 +0500

    Added QSort

---
+++ QSort/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quiz {
+    public static class Program {
+        
+        private static void Main(string[] args) {
+            int[] array = new int[20];
+
+            for (int i = 0; i < array.Length; i++) {
+                array[i] = i % 2 + i % 3 + i % 9;
+            }
+            
+            for (int i = 0; i < array.Length; i++) {
+                Console.Write("{0:00} ", array[i]);
+            }
+            
+            Console.WriteLine(" ");
+            
+            Solution solution = new Solution();
+
+            solution.QSort(array, Comparer<int>.Default);
+            
+            for (int i = 0; i < array.Length; i++) {
+                Console.Write("{0:00} ", array[i]);
+            }
+            
+            Console.WriteLine(" ");
+        }
+
+    }
+}
