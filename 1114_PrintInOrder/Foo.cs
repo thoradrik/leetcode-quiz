15511f90bb74d7085215d6e0c2ca3c0acb63959a
thoradrik <thoradrik@gmail.com>
2020-11-17 21:28:23 +0500

    Solved https://leetcode.com/problems/print-in-order/

---
+++ 1114_PrintInOrder/Foo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace Quiz {
+    public class Foo {
+
+        public Foo() {
+        
+        }
+
+        private ManualResetEvent EVT_FIRST = new ManualResetEvent(false);
+        
+        public void First(Action printFirst) {
+        
+            // printFirst() outputs "first". Do not change or remove this line.
+            printFirst();
+
+            EVT_FIRST.Set();
+        }
+
+        private ManualResetEvent EVT_SECOND = new ManualResetEvent(false);
+
+        public void Second(Action printSecond) {
+            WaitHandle.WaitAll(new WaitHandle[] { EVT_FIRST });
+        
+            // printSecond() outputs "second". Do not change or remove this line.
+            printSecond();
+
+            EVT_SECOND.Set();
+        }
+
+        private ManualResetEvent EVT_THIRD = new ManualResetEvent(false);
+
+        public void Third(Action printThird) {
+            WaitHandle.WaitAll(new WaitHandle[] { EVT_FIRST, EVT_SECOND });
+            
+            // printThird() outputs "third". Do not change or remove this line.
+            printThird();
+
+            EVT_THIRD.Set();
+        }
+    }
+}
---
+++ 1114_PrintInOrder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace Quiz {
+    public static class Program {
+        
+        private static void Main(string[] args) {
+            Foo solution = new Foo();
+            
+            Thread first = new Thread(() => solution.First(() => Console.WriteLine("First")));
+
+            Thread second = new Thread(() => solution.Second(() => Console.WriteLine("Second")));
+
+            Thread third = new Thread(() => solution.Third(() => Console.WriteLine("Third")));
+            
+            first.Start();
+            third.Start();
+            second.Start();
+        }
+        
+    }
+}
