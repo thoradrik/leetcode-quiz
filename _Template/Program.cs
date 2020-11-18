81a7db5bc2304f898144a6f29a13646b85952078
thoradrik <thoradrik@gmail.com>
2020-11-18 20:29:13 +0500

    Minor fixes

--- _Template/Program.cs
+++ _Template/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace Quiz {
+﻿using System;
+
+namespace Quiz {
     public static class Program {
         
         private static void Main(string[] args) {
             Solution solution = new Solution();
 
+            Check(solution, 1, 1);
+        }
+
+        private static void Check(Solution solution, int test, int expected) {
+            Console.WriteLine("TEST {0}", test);
+            
+            int answer = solution.Method(test);
+            if (expected != answer) {
+                Console.WriteLine("  !!! FAILED {0} {1}", answer, expected);
+            } else {
+                Console.WriteLine("  PASSED {0} {1}", answer, expected);
+            }
         }
 
     }
