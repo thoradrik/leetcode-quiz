65cf11c189956863217d9a84f7ac8f802ef2cbcf
thoradrik <thoradrik@gmail.com>
2020-12-06 22:51:52 +0500

    Solved 300. Longest Increasing Subsequence https://leetcode.com/problems/longest-increasing-subsequence/

---
+++ 0300_LongestIncreasingSubsequence/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Quiz {
+    public static class Program {
+        
+        private static void Main(string[] args) {
+            Solution solution = new Solution();
+
+            Check(solution.LengthOfLIS, Parse("[]"), 0);
+            Check(solution.LengthOfLIS, Parse("[1]"), 1);
+            Check(solution.LengthOfLIS, Parse("[1,2]"), 2);
+            Check(solution.LengthOfLIS, Parse("[2,1]"), 1);
+            Check(solution.LengthOfLIS, Parse("[10,9,2,5,3,7,101,18]"), 4);
+            Check(solution.LengthOfLIS, Parse("[0,1,0,3,2,3]"), 4);
+            Check(solution.LengthOfLIS, Parse("[7,7,7,7,7,7,7]"), 1);
+        }
+
+        private static int[] Parse(string s) {
+            return JsonSerializer.Deserialize<int[]>(s);
+        }
+
+
+        private static void Check<T, R>(Func<T, R> func, T test, R expected) {
+            Console.WriteLine("TEST {0}", test);
+            
+            R answer = func(test);
+            if (!Equals(expected, answer)) {
+                Console.WriteLine("  !!! FAILED {0} {1}", answer, expected);
+            } else {
+                Console.WriteLine("  PASSED {0} {1}", answer, expected);
+            }
+        }
+
+    }
+}
---
+++ 0300_LongestIncreasingSubsequence/Solution.cs
@@ -0,0 +1,48 @@
+﻿namespace Quiz {
+    public class Solution {
+
+        private int[] m_Nums;
+        
+        private int[] m_Maxes;
+        
+        public int LengthOfLIS(int[] nums) {
+            m_Nums = nums;
+            m_Maxes = new int[nums.Length];
+
+            int max_seq = 0;
+            
+            for (int i = m_Nums.Length - 1; i >= 0; i--) {
+                int path = FindMaxSeq(i);
+                if (path > max_seq) {
+                    max_seq = path;
+                }
+            }
+
+            return max_seq;
+        }
+
+        public int FindMaxSeq(int e_i) {
+            if (m_Maxes[e_i] > 0) {
+                return m_Maxes[e_i];
+            }
+            
+            int max_path = 0;
+            
+            for (int i = e_i + 1; i < m_Nums.Length; i++) {
+                if (m_Nums[i] > m_Nums[e_i]) {
+                    int path = FindMaxSeq(i);    
+                    if (path > max_path) {
+                        max_path = path;
+                    }
+                }
+            }
+
+            int result = max_path + 1;
+            
+            m_Maxes[e_i] = result;
+        
+            return result;
+        }
+
+    }
+}
