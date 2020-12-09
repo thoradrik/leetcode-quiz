ad93ce7ae1d02dea95b28b0cd4305c63b494a1df
thoradrik <thoradrik@gmail.com>
2020-12-09 20:23:48 +0500

    Solved 372. Super Pow https://leetcode.com/problems/super-pow/

---
+++ 0372_SuperPow/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Quiz {
+    public static class Program {
+        
+        private static void Main(string[] args) {
+            Solution solution = new Solution();
+
+            Check(solution.SuperPow, 0, "[0]", 1);
+            Check(solution.SuperPow, 0, "[1]", 0);
+            Check(solution.SuperPow, 0, "[2]", 0);
+            Check(solution.SuperPow, 0, "[3]", 0);
+            Check(solution.SuperPow, 1, "[0]", 1);
+            Check(solution.SuperPow, 1, "[1]", 1);
+            Check(solution.SuperPow, 1, "[3]", 1);
+            Check(solution.SuperPow, 1, "[3]", 1);
+            Check(solution.SuperPow, 2, "[0]", 1);
+            Check(solution.SuperPow, 2, "[1]", 2);
+            Check(solution.SuperPow, 2, "[2]", 4);
+            Check(solution.SuperPow, 2, "[3]", 8);
+            Check(solution.SuperPow, 2, "[1,0]", 1024);
+            Check(solution.SuperPow, 2, "[1,1]", 711);
+            Check(solution.SuperPow, 2, "[1,0,1]", 1019);
+            Check(solution.SuperPow, 1, "[4,3,3,8,5,2]", 1);
+            Check(solution.SuperPow, 2147483647, "[2,0,0]", 1198);
+        }
+        
+        private const double EPSILON = 00000005;
+
+        private static void Check(Func<int, int[], int> func, int test1, string test2, int expected) {
+            Console.WriteLine("TEST {0}^{1} mod 1337", test1, test2);
+            
+            double answer = func(test1, JsonSerializer.Deserialize<int[]>(test2));
+            if (Math.Abs(expected - answer) > EPSILON) {
+                Console.WriteLine("  !!! FAILED {0} {1}", answer, expected);
+            } else {
+                Console.WriteLine("  PASSED {0} {1}", answer, expected);
+            }
+        }
+
+    }
+}
---
+++ 0372_SuperPow/Solution.cs
@@ -0,0 +1,53 @@
+﻿namespace Quiz {
+    public class Solution {
+
+        public int SuperPow(int a, int[] b) {
+            return (int)PowRec(a, b, 1337);    
+        }
+        
+        private long PowRec(long x, int[] n, long k) {
+            long a = 1;
+            long b = x;
+            
+            while (IsGreater0(n)) {
+                if (n[^1] % 2 == 1) {
+                    a = (a * b) % k;
+                }
+
+                b = (b * b) % k;
+                
+                Div2(n);
+            }
+
+            return a % k;
+        }
+
+        private bool IsGreater0(int[] n) {
+            for (int i = n.Length - 1; i >= 0; i--) {
+                if (n[i] > 0) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void Div2(int[] n) {
+            int i = 0;
+
+            while (i < n.Length && n[i] == 0) {
+                i++;
+            }
+
+            int rem = 0;
+            
+            while (i < n.Length) {
+                int k = n[i] + 10 * rem;
+                rem = k % 2;
+                n[i] = k / 2;
+                i++;
+            }
+        }
+
+    }
+}
