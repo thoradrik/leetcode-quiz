941d43e857eccad7ee945c401a45ef3ffd0664c0
thoradrik <thoradrik@gmail.com>
2020-12-09 19:47:44 +0500

    Solved 50. Pow(x, n) https://leetcode.com/problems/powx-n/

---
+++ 0050_Pow/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Quiz {
+    public static class Program {
+        
+        private static void Main(string[] args) {
+            Solution solution = new Solution();
+
+            Check(solution.MyPow, 0.0, 0, 1.0);
+            Check(solution.MyPow, 0.0, 1, 0.0);
+            Check(solution.MyPow, 0.0, 2, 0.0);
+            Check(solution.MyPow, 0.0, 3, 0.0);
+            Check(solution.MyPow, 1.0, 0, 1.0);
+            Check(solution.MyPow, 1.0, 1, 1.0);
+            Check(solution.MyPow, 1.0, 2, 1.0);
+            Check(solution.MyPow, 1.0, 3, 1.0);
+            Check(solution.MyPow, 2.0, 0, 1.0);
+            Check(solution.MyPow, 2.0, 1, 2.0);
+            Check(solution.MyPow, 2.0, 2, 4.0);
+            Check(solution.MyPow, 2.0, 3, 8.0);
+            Check(solution.MyPow, 2.0, 10, 1024.0);
+            Check(solution.MyPow, 2.0, 11, 2048.0);
+            Check(solution.MyPow, 2.1, 3, 9.261);
+            Check(solution.MyPow, 2.0, -2, 0.25);
+            Check(solution.MyPow, 0.00001, 2147483647, 0.0);
+            Check(solution.MyPow, 1.00000, -2147483648, 1.0);
+        }
+        
+        private const double EPSILON = 0.00000005;
+
+        private static void Check(Func<double, int, double> func, double test1, int test2, double expected) {
+            Console.WriteLine("TEST {0}^{1}", test1, test2);
+            
+            double answer = func(test1, test2);
+            if (Math.Abs(expected - answer) > EPSILON) {
+                Console.WriteLine("  !!! FAILED {0} {1}", answer, expected);
+            } else {
+                Console.WriteLine("  PASSED {0} {1}", answer, expected);
+            }
+        }
+
+    }
+}
---
+++ 0050_Pow/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace Quiz {
+    public class Solution {
+
+        public double MyPow(double x, int n) {
+            if (n < 0) {
+                return 1.0 /  PowRec(x, - (long)n);
+            } else {
+                return PowRec(x, n);    
+            }
+        }
+        
+        private double PowRec(double x, long n) {
+            double a = 1.0;
+            double b = x;
+            
+            while (n > 0) {
+                if (n % 2 == 1) {
+                    a = a * b;
+                }
+
+                b = b * b;
+                
+                n /= 2;
+            }
+
+            return a;
+        }
+
+    }
+}
