23add47414c9dafec0f579d8e3196e7524b20b9b
thoradrik <thoradrik@gmail.com>
2020-12-10 23:29:39 +0500

    Solved 360. Sort Transformed Array https://leetcode.com/problems/sort-transformed-array/

---
+++ 0360_SortTransformedArray/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace Quiz {
+    public static class Program {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.SortTransformedArray, Array("[-4,-2,2,4]"), 1, 3, 5, Array("[3,9,15,33]"));
+            Test.Check(solution.SortTransformedArray, Array("[-4,-2,2,4]"), -1, 3, 5, Array("[-23,-5,1,7]"));
+        }
+
+        private static int[] Array(string s) => JsonSerializer.Deserialize<int[]>(s);
+
+    }
+}
