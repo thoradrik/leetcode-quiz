b7d3e3f09cf244b5e4e61cffbb2233ff6cbda2b6
thoradrik <thoradrik@gmail.com>
2020-12-10 15:09:49 +0500

    Solved 3. Longest Substring Without Repeating Characters https://leetcode.com/problems/longest-substring-without-repeating-characters/

---
+++ 0003_LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Quiz {
+    public static class Program {
+        
+        private static void Main(string[] args) {
+            Solution solution = new Solution();
+
+            Check(solution.LengthOfLongestSubstring, "", 0);
+            Check(solution.LengthOfLongestSubstring, "0", 1);
+            Check(solution.LengthOfLongestSubstring, "abcabcbb", 3);
+            Check(solution.LengthOfLongestSubstring, "bbbbb", 1);
+            Check(solution.LengthOfLongestSubstring, "pwwkew", 3);
+            Check(solution.LengthOfLongestSubstring, "abcabcdabcdeabca", 5);
+        }
+
+        private static void Check<T, R>(Func<T, R> func, T test, R expected) {
+            Console.WriteLine("TEST {0}", test);
+            
+            R answer = func(test);
+            if (!Equals(expected, answer)) {
+                Console.WriteLine("  !!! FAILED {0} {1}", answer, expected);
+            } else {
+                Console.WriteLine("  PASSED {0} {1}", answer, expected);
+            }
+        }
+
+    }
+}
---
+++ 0003_LongestSubstringWithoutRepeatingCharacters/Solution.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Quiz {
+    public class Solution {
+        
+        public int LengthOfLongestSubstring(string s) {
+            if (s.Length <= 0) {
+                return 0;
+            }
+            
+            int max_start = 0;
+            int max_length = 0;
+            
+            bool[] map = new bool[256];
+
+            int start = 0;
+            int length = 1;
+            map[s[start]] = true;                
+                
+            while (start + length < s.Length) {
+                int k = start + length;
+                
+                if (map[s[k]]) {
+                    if (length >= max_length) {
+                        Console.WriteLine("MAX: {0}", s.Substring(start, length));
+                        max_start = start;
+                        max_length = length;
+                    }
+                        
+                    map[s[start]] = false;
+                    start++;
+                    
+                    if (length > 0) {
+                        length--;
+                    }
+
+                    goto next;
+                }
+                
+                Console.WriteLine("     {0}", s.Substring(start, length));
+                
+                map[s[k]] = true;
+                length++;
+            next: 
+                ;
+            }
+
+            if (length > max_length) {
+                Console.WriteLine("MAX: {0}", s.Substring(start, length));
+                max_start = start;
+                max_length = length;
+            }
+            
+            return max_length;
+        }
+        
+    }
+}
