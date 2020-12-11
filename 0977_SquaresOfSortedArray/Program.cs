0a1432897c8c3404b80c2d608db9611633432541
thoradrik <thoradrik@gmail.com>
2020-12-11 15:59:06 +0500

    Solved 977. Squares of a Sorted Array https://leetcode.com/problems/squares-of-a-sorted-array/

---
+++ 0977_SquaresOfSortedArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace Quiz {
+    public static class Program {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.SortedSquares, Array("[]"), Array("[]"));
+            Test.Check(solution.SortedSquares, Array("[1]"), Array("[1]"));
+            Test.Check(solution.SortedSquares, Array("[0,1,2]"), Array("[0,1,4]"));
+            Test.Check(solution.SortedSquares, Array("[-2,-1,0]"), Array("[0,1,4]"));
+            Test.Check(solution.SortedSquares, Array("[-1,1]"), Array("[1,1]"));
+            Test.Check(solution.SortedSquares, Array("[-2,0,2]"), Array("[0,4,4]"));
+            Test.Check(solution.SortedSquares, Array("[-4,-1,0,3,10]"), Array("[0,1,9,16,100]"));
+            Test.Check(solution.SortedSquares, Array("[-7,-3,2,3,11]"), Array("[4,9,9,49,121]"));
+        }
+
+        private static int[] Array(string s) => JsonSerializer.Deserialize<int[]>(s);
+
+    }
+}
---
+++ 0977_SquaresOfSortedArray/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public int[] SortedSquares(int[] nums) {
+            int i_left = 0;
+            int i_right = nums.Length - 1;
+            int i = i_right;
+            int[] result = new int[nums.Length];
+            
+            while (i_left <= i_right) {
+                int s_left = nums[i_left] * nums[i_left];
+                int s_right = nums[i_right] * nums[i_right];
+                if (s_left < s_right) {
+                    result[i] = s_right;
+                    i_right--;
+                } else {
+                    result[i] = s_left;
+                    i_left++;
+                }
+
+                i--;
+            }
+
+            return result;
+        }
+        
+    }
+}
