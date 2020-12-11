aa357c8a5d7a09a0dc15e9917a9d703b6e0ec3fd
thoradrik <thoradrik@gmail.com>
2020-12-11 16:16:48 +0500

    Solved 88. Merge Sorted Array https://leetcode.com/problems/merge-sorted-array/

---
+++ 0088_MergeSortedArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+
+namespace Quiz {
+    public static class Program {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            int[] Func(int[] a1, int a2, int[] a3, int a4) {
+                solution.Merge(a1, a2, a3, a4);
+                return a1;
+            }
+
+            Test.Check(Func, Array("[]"), 0, Array("[]"), 0, Array("[]"));
+            Test.Check(Func, Array("[1]"), 1, Array("[]"), 0, Array("[1]"));
+            Test.Check(Func, Array("[0]"), 0, Array("[1]"), 1, Array("[1]"));
+            Test.Check(Func, Array("[1,2,3,0,0,0]"), 3, Array("[2,5,6]"), 3, Array("[1,2,2,3,5,6]"));
+        }
+
+        private static int[] Array(string s) => JsonSerializer.Deserialize<int[]>(s);
+
+    }
+}
---
+++ 0088_MergeSortedArray/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public void Merge(int[] nums1, int m, int[] nums2, int n) {
+            int i_1 = m - 1;
+            int i_2 = n - 1;
+            int i = m + n - 1;
+            
+            while (i_1 >= 0 || i_2 >= 0) {
+                bool z;
+                
+                if (i_1 >= 0 && i_2 >= 0) {
+                    int n_1 = nums1[i_1];
+                    int n_2 = nums2[i_2];
+
+                    z = n_1 > n_2;
+                } else {
+                    z = i_1 >= 0;
+                } 
+                
+                if (z) {
+                    nums1[i] = nums1[i_1];
+                    i_1--;
+                } else {
+                    nums1[i] = nums2[i_2];
+                    i_2--;
+                }
+
+                i--;
+            }
+        }
+        
+    }
+}
