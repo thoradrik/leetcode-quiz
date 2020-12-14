35e301e0feffcdbad67cf5c11df1c41022aed33e
thoradrik <thoradrik@gmail.com>
2020-12-14 20:23:05 +0500

    Solved 279. Perfect Squares https://leetcode.com/problems/perfect-squares/

---
+++ 0279_PerfectSquares/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Quiz {
+    public static class Program {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.NumSquares, 1, 1);
+            Test.Check(solution.NumSquares, 2, 2);
+            Test.Check(solution.NumSquares, 3, 3);
+            Test.Check(solution.NumSquares, 5, 2);
+            Test.Check(solution.NumSquares, 8, 2);
+            Test.Check(solution.NumSquares, 9, 1);
+            Test.Check(solution.NumSquares, 10, 2);
+            Test.Check(solution.NumSquares, 12, 3);
+            Test.Check(solution.NumSquares, 121, 1);
+            Test.Check(solution.NumSquares, 1212, 4);
+            Test.Check(solution.NumSquares, 12124, 4);
+            Test.Check(solution.NumSquares, 121245, 3);
+            Test.Check(solution.NumSquares, 1212456, 3);
+        }
+
+    }
+}
---
+++ 0279_PerfectSquares/Solution.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+
+        private static readonly Dictionary<int, int> MAP = new Dictionary<int, int> {
+            { 0, 0 }, 
+            { 1, 1 }, 
+            { 2, 2 }, 
+            { 3, 3 }, 
+            { 4, 1 }, 
+            { 5, 2 }, 
+            { 9, 1 }, 
+            { 10, 2 } 
+        };
+
+        public int NumSquares(int n) {
+            if (MAP.TryGetValue(n, out int result)) {
+                return result;
+            }
+            
+            int n_sq = (int)Math.Sqrt(n);
+            
+            int min = Int32.MaxValue;
+            
+            for (int k = n_sq; k >= 1; k--) {
+                int squares = NumSquares(n - k * k) + 1;
+                if (squares <= 2) {
+                    min = squares;
+                    break;
+                }
+                if (squares < min) {
+                    min = squares;
+                }
+            }
+
+            MAP[n] = min;
+
+            return min;
+        }
+        
+        /*
+
+        // OPTIMAL MATH SOLUTION (NOT MINE UNFORTUNATELY)
+
+        public bool IsPerfectSquare(int num) {
+            int k = (int)Math.Sqrt(num);
+            return num == k * k;
+        }
+        
+        public int NumSquares(int n) {
+            while (n % 4 == 0) {
+                n /= 4;
+            }
+
+            if (n % 8 == 7) {
+                return 4;
+            }
+            
+            if (IsPerfectSquare(n)) {
+                return 1;
+            }
+            
+            for (int i = 1; i * i <= n; ++i) {
+                if (IsPerfectSquare(n - i * i)) {
+                    return 2;
+                }
+            }
+
+            return 3;
+        }
+
+        */
+        
+    }
+}
