fc3b4f4bd15103ccf1d8e0a4ac6fa554decddec8
thoradrik <thoradrik@gmail.com>
2020-12-18 19:55:14 +0500

    Solved 949. Largest Time for Given Digits https://leetcode.com/problems/largest-time-for-given-digits/

---
+++ 0949_LargestTimeForGivenDigits/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace Quiz {
+    public static class Program {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.LargestTimeFromDigits, Array("[1,2,3,4]"), "23:41");
+            Test.Check(solution.LargestTimeFromDigits, Array("[5,5,5,5]"), "");
+            Test.Check(solution.LargestTimeFromDigits, Array("[0,0,0,0]"), "00:00");
+            Test.Check(solution.LargestTimeFromDigits, Array("[0,0,1,0]"), "10:00");
+            Test.Check(solution.LargestTimeFromDigits, Array("[5,2,3,9]"), "23:59");
+        }
+
+        private static int[] Array(string s) => JsonSerializer.Deserialize<int[]>(s);
+
+    }
+}
---
+++ 0949_LargestTimeForGivenDigits/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using AlgorithmLib;
+
+namespace Quiz {
+    public class Solution {
+
+        public string LargestTimeFromDigits(int[] arr) {
+            int? max_hm = null;
+            
+            PermutationGenerator.Generate(arr, a => {
+                int h = a[0] * 10 + a[1];
+                int m = a[2] * 10 + a[3];
+                if (h <= 23 && m <= 59) {
+                    int hm = h * 100 + m;
+                    if (max_hm.HasValue) {
+                        if (hm > max_hm) {
+                            max_hm = hm;
+                        }
+                    } else {
+                        max_hm = hm;
+                    }
+                }
+            });
+
+            if (max_hm.HasValue) {
+                return String.Format("{0:00}:{1:00}", max_hm / 100, max_hm % 100);
+            } else {
+                return String.Empty;
+            }
+        }
+
+    }
+}
