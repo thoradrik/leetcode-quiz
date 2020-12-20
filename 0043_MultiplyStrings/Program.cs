e264f7a4d547b5d1b4bda4747ebd87cd91d78507
thoradrik <thoradrik@gmail.com>
2020-12-20 14:54:52 +0500

    Solved 43. Multiply Strings https://leetcode.com/problems/multiply-strings/

---
+++ 0043_MultiplyStrings/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Quiz {
+    public static class Program {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+            
+            Test.Check(solution.AddStrings, new[] { "492", "2460", "12300" }, "15252");
+
+            Test.Check(solution.Multiply, "123", "124", "15252");
+
+            Test.Check(solution.Multiply, "0", "0", "0");
+            Test.Check(solution.Multiply, "0", "1", "0");
+            Test.Check(solution.Multiply, "1", "0", "0");
+            Test.Check(solution.Multiply, "9133", "0", "0");
+            Test.Check(solution.Multiply, "0", "9133", "0");
+            Test.Check(solution.Multiply, "1", "1", "1");
+            Test.Check(solution.Multiply, "10", "1", "10");
+            Test.Check(solution.Multiply, "1", "11", "11");
+            Test.Check(solution.Multiply, "11", "2", "22");
+            Test.Check(solution.Multiply, "11", "11", "121");
+            Test.Check(solution.Multiply, "121", "112", "13552");
+            Test.Check(solution.Multiply, "1234568", "897561646", "1108100886178928");
+            Test.Check(solution.Multiply, "987413248761378423", "135687461634894351", "133979597309095926818210910735988473");
+            Test.Check(solution.Multiply, "9999999999999999999999999999999999999999999999999999999999999999999", "8888888888888888888888888888888888888888888888888888888888", "88888888888888888888888888888888888888888888888888888888879999999991111111111111111111111111111111111111111111111111111111112");
+        }
+
+    }
+}
---
+++ 0043_MultiplyStrings/Solution.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+
+        public string AddStrings(IList<string> nums) {
+            int[] ni = new int[nums.Count];
+            
+            for (int i = 0; i < nums.Count; i++) {
+                ni[i] = nums[i].Length - 1;
+            }
+            
+            int k = 0;
+            
+            Stack<char> result = new Stack<char>();
+
+            while (true) {
+                bool b = k > 0;
+                
+                for (int i = 0; i < ni.Length; i++) {
+                    b |= ni[i] >= 0;
+                }
+
+                if (!b) {
+                    break;
+                }
+
+                for (int i = 0; i < ni.Length; i++) {
+                    if (ni[i] >= 0) {
+                        int c = nums[i][ni[i]] - '0';
+                        ni[i]--;
+                        k += c;
+                    }
+                }
+                
+                result.Push((char)(k % 10 + '0'));
+                
+                k /= 10;
+            }
+
+            return new string(result.ToArray());
+        }
+
+        private string MultiplyHelper(string num1, int num2, int trailing_zeroes) {
+            int ai = num1.Length - 1;
+            int k = 0;
+            
+            Stack<char> result = new Stack<char>();
+
+            for (int i = 0; i < trailing_zeroes; i++) {
+                result.Push('0');
+            }
+            
+            while (ai >= 0 || k > 0) {
+                int c1 = ai >= 0 ? num1[ai--] - '0' : 0;
+            
+                k += c1 * num2;
+
+                result.Push((char)(k % 10 + '0'));
+
+                k /= 10;
+            }
+
+            return new string(result.ToArray());
+        }
+
+        public string Multiply(string num1, string num2) {
+            if (num1 == "0" || num2 == "0") {
+                return "0";
+            } else if (num1 == "1") {
+                return num2;
+            } else if (num2 == "1") {
+                return num1;
+            } else if (num1.Length < num2.Length) {
+                return Multiply(num2, num1);
+            }
+            
+            int ai = num2.Length - 1;
+
+            List<string> list = new List<string>(num2.Length);
+
+            for (int i = ai; i >= 0; i--) {
+                int n = num2[i] - '0';
+                if (n > 0) {
+                    int trailing_zeroes = ai - i;
+                    list.Add(MultiplyHelper(num1, n, trailing_zeroes));
+                }
+            }
+
+            if (list.Count > 0) {
+                return AddStrings(list);
+            } else {
+                return "0";
+            }
+        }
+
+    }
+}
