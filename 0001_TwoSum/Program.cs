6e05454b68722c3ef10deac59860269630711cc0
thoradrik <thoradrik@gmail.com>
2020-12-20 15:54:25 +0500

    Solved 1. Two Sum https://leetcode.com/problems/two-sum/

---
+++ 0001_TwoSum/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.TwoSum, IntArray("[2,7,11,15]"), 9, IntArray("[0,1]"));
+            Test.Check(solution.TwoSum, IntArray("[3,2,4]"), 6, IntArray("[1,2]"));
+            Test.Check(solution.TwoSum, IntArray("[3,3]"), 6, IntArray("[0,1]"));
+            Test.Check(solution.TwoSum, IntArray("[-2,-1,0,0,3,5]"), 0, IntArray("[2,3]"));
+        }
+
+    }
+}
---
+++ 0001_TwoSum/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+        
+        /*
+         
+        public int[] TwoSum(int[] nums, int target) {
+            for (int i = 0; i < nums.Length; i++) {
+                for (int j = 0; j < nums.Length; j++) {
+                    if (i != j && nums[i] + nums[j] == target) {
+                        return new[] { i, j };
+                    }
+                }
+            }
+
+            return Array.Empty<int>();
+        }
+        
+        */
+        
+        public int[] TwoSum(int[] nums, int target) {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            
+            for (int i = 0; i < nums.Length; i++) {
+                int num = nums[i];
+                if (map.TryGetValue(num, out int j)) {
+                    return new[] { j, i };
+                } else {
+                    map[target - num] = i;
+                }
+            }
+
+            return Array.Empty<int>();
+        }
+
+    }
+}
