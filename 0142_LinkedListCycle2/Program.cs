25bda5ba6a3d854cc1b26ebc8cf2637199d2f559
thoradrik <thoradrik@gmail.com>
2020-12-20 18:56:33 +0500

    Solved 142. Linked List Cycle II https://leetcode.com/problems/linked-list-cycle-ii/

---
+++ 0142_LinkedListCycle2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json;
+
+namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            ListNode[] nodes;
+
+            Test.Check(solution.DetectCycle, (ListNode)null, (ListNode)null);
+            Test.Check(solution.DetectCycle, LinkedList("[1]", 0, out nodes), nodes[0]);
+            Test.Check(solution.DetectCycle, LinkedList("[1]", -1, out nodes), null);
+            Test.Check(solution.DetectCycle, LinkedList("[1,2]", 0, out nodes), nodes[0]);
+            Test.Check(solution.DetectCycle, LinkedList("[1,2]", 1, out nodes), nodes[1]);
+            Test.Check(solution.DetectCycle, LinkedList("[1,2]", -1, out nodes), null);
+            Test.Check(solution.DetectCycle, LinkedList("[3,2,0,-4]", 0, out nodes), nodes[0]);
+            Test.Check(solution.DetectCycle, LinkedList("[3,2,0,-4]", 1, out nodes), nodes[1]);
+            Test.Check(solution.DetectCycle, LinkedList("[3,2,0,-4]", 2, out nodes), nodes[2]);
+            Test.Check(solution.DetectCycle, LinkedList("[3,2,0,-4]", 3, out nodes), nodes[3]);
+            Test.Check(solution.DetectCycle, LinkedList("[3,2,0,-4]", -1, out nodes), null);
+        }
+
+        private static ListNode LinkedList(string s, int cycle, out ListNode[] nodes) {
+            int[] n = JsonSerializer.Deserialize<int[]>(s);
+            nodes = new ListNode[n.Length];
+            
+            ListNode node = null;
+            ListNode last = null;
+
+            for (int i = n.Length - 1; i >= 0; i--) {
+                node = new ListNode(n[i]) {
+                    next = node
+                };
+
+                if (last == null) {
+                    last = node;
+                }
+
+                if (i == cycle) {
+                    last.next = node;
+                }
+
+                nodes[i] = node;
+            }
+
+            return node;
+        }        
+    }
+}
---
+++ 0142_LinkedListCycle2/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace Quiz {
+    public class Solution {
+
+        public ListNode DetectCycle(ListNode head) {
+            if (head == null) {
+                return null;
+            }
+
+            ListNode turtle = head;
+            ListNode hear = head;
+
+            while (true) {
+                turtle = turtle.next;
+                if (turtle == null) {
+                    return null;
+                }
+
+                hear = hear.next?.next;
+                if (hear == null) {
+                    return null;
+                }
+
+                if (turtle == hear) {
+                    break;
+                }
+            }
+            
+            ListNode a = head;
+            ListNode b = turtle;
+
+            while (a != b) {
+                a = a.next;
+                b = b.next;
+            }
+
+            return a;
+        }
+
+    }
+}
