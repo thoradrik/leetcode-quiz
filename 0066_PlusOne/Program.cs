793596156c04c61ece2ecabe3c86477b56982ef1
thoradrik <thoradrik@gmail.com>
2020-12-20 16:39:24 +0500

    Solved 66. Plus One https://leetcode.com/problems/plus-one/

---
+++ 0066_PlusOne/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.PlusOne, IntArray("[]"), IntArray("[]"));
+            Test.Check(solution.PlusOne, IntArray("[0]"), IntArray("[1]"));
+            Test.Check(solution.PlusOne, IntArray("[1]"), IntArray("[2]"));
+            Test.Check(solution.PlusOne, IntArray("[9]"), IntArray("[1,0]"));
+            Test.Check(solution.PlusOne, IntArray("[1,0]"), IntArray("[1,1]"));
+            Test.Check(solution.PlusOne, IntArray("[1,9]"), IntArray("[2,0]"));
+            Test.Check(solution.PlusOne, IntArray("[1,9,9]"), IntArray("[2,0,0]"));
+            Test.Check(solution.PlusOne, IntArray("[9,9,9]"), IntArray("[1,0,0,0]"));
+        }
+
+    }
+}
---
+++ 0066_PlusOne/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+
+        public int[] PlusOne(int[] digits) {
+            int i = digits.Length - 1;
+
+            if (i >= 0) {
+                digits[i]++;
+            }
+
+            Stack<int> result = new Stack<int>(digits.Length);
+            
+            int k = 0;
+
+            while (i >= 0) {
+                k += digits[i];
+                result.Push(k % 10);
+                k /= 10;
+                i--;
+            }
+
+            if (k > 0) {
+                result.Push(k);
+            }
+            
+            return result.ToArray();
+        }
+
+    }
+}
