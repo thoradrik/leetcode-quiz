4fba83e99037120b4bcf8195ba13f06613269578
thoradrik <thoradrik@gmail.com>
2020-12-20 18:11:10 +0500

    Solved 202. Happy Number https://leetcode.com/problems/happy-number/

---
+++ 0202_HappyNumber/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.IsHappy, 1, true);
+            Test.Check(solution.IsHappy, 2, false);
+            Test.Check(solution.IsHappy, 10, true);
+            Test.Check(solution.IsHappy, 19, true);
+            Test.Check(solution.IsHappy, 191, false);
+            Test.Check(solution.IsHappy, 192, true);
+            Test.Check(solution.IsHappy, 1024, false);
+            Test.Check(solution.IsHappy, 2048, false);
+            Test.Check(solution.IsHappy, 4096, true);
+            Test.Check(solution.IsHappy, 1926, false);
+            Test.Check(solution.IsHappy, 13, true);
+            Test.Check(solution.IsHappy, 103, true);
+            Test.Check(solution.IsHappy, 31, true);
+            Test.Check(solution.IsHappy, 301, true);
+            Test.Check(solution.IsHappy, 68, true);
+            Test.Check(solution.IsHappy, 608, true);
+            Test.Check(solution.IsHappy, 86, true);
+            Test.Check(solution.IsHappy, 806, true);
+            Test.Check(solution.IsHappy, 807, false);
+            Test.Check(solution.IsHappy, 8070, false);
+            Test.Check(solution.IsHappy, 80701, false);
+            Test.Check(solution.IsHappy, 807012, false);
+            Test.Check(solution.IsHappy, 8070124, false);
+            Test.Check(solution.IsHappy, 80701245, false);
+            Test.Check(solution.IsHappy, 2147079784, true);
+            Test.Check(solution.IsHappy, 2147041584, true);
+            Test.Check(solution.IsHappy, 2147079756, true);
+            /*
+            for (int i = 0; i <= 100; i++) {
+                if (solution.IsHappy(i)) {
+                    Console.WriteLine(i);
+                } 
+            }
+
+            for (int i = Int32.MaxValue; i >= 0; i--) {
+                if (solution.IsHappy(i)) {
+                    Console.WriteLine(i);
+                } 
+            }
+            */
+        }
+
+    }
+}
---
+++ 0202_HappyNumber/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+
+        private static Dictionary<int, bool> m_Map = new Dictionary<int, bool> {
+            { 1, true },
+            { 10, true },
+            { 100, true }
+        };
+        
+        public bool IsHappy(int n) {
+            if (m_Map.TryGetValue(n, out bool result)) {
+                return result;
+            }
+
+            m_Map[n] = false;
+            
+            int k = n;
+            int s = 0;
+            while (k > 0) {
+                int r = k % 10;
+                s += r * r;
+                k /= 10;
+            }
+
+            if (IsHappy(s)) {
+                m_Map[n] = true;
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    }
+}
