0e7f6ef6ac96e4658ad0e5589d253d0c4a34f3c0
thoradrik <thoradrik@gmail.com>
2020-12-20 19:02:32 +0500

    Solved 287. Find the Duplicate Number https://leetcode.com/problems/find-the-duplicate-number/

---
+++ 0287_FindDuplicateNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.FindDuplicate, IntArray("[1,1]"), 1);
+            Test.Check(solution.FindDuplicate, IntArray("[1,2,1]"), 1);
+            Test.Check(solution.FindDuplicate, IntArray("[1,2,2]"), 2);
+            Test.Check(solution.FindDuplicate, IntArray("[1,2,2,3]"), 2);
+            Test.Check(solution.FindDuplicate, IntArray("[1,2,3,3]"), 3);
+            Test.Check(solution.FindDuplicate, IntArray("[3,2,1,1,4]"), 1);
+            Test.Check(solution.FindDuplicate, IntArray("[7,7,6,5,4,3,2,1]"), 7);
+            Test.Check(solution.FindDuplicate, IntArray("[2,2,2,2,2]"), 2);
+            Test.Check(solution.FindDuplicate, IntArray("[1,2,3,4,5,5]"), 5);
+        }
+
+    }
+}
---
+++ 0287_FindDuplicateNumber/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Quiz {
+    public class Solution {
+
+        public int FindDuplicate(int[] nums) {
+            Array.Sort(nums);
+
+            int a = nums[0];
+            int i = 1;
+            
+            while (i < nums.Length) {
+                int b = nums[i];
+                if (a == b) {
+                    return a;
+                }
+                
+                a = b;
+             
+                i++;
+            }
+            
+            return -1;
+        }
+
+    }
+}
