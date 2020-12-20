c56a48f83d9e2eba4826cf990fbceba93e1e583a
thoradrik <thoradrik@gmail.com>
2020-12-20 17:09:38 +0500

    Solved 136. Single Number https://leetcode.com/problems/single-number/

---
+++ 0136_SingleNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.SingleNumber, IntArray("[0]"), 0);
+            Test.Check(solution.SingleNumber, IntArray("[1]"), 1);
+            Test.Check(solution.SingleNumber, IntArray("[0,1,0]"), 1);
+            Test.Check(solution.SingleNumber, IntArray("[0,2,0]"), 2);
+            Test.Check(solution.SingleNumber, IntArray("[0,2,1,0,2]"), 1);
+            Test.Check(solution.SingleNumber, IntArray("[-1,0,2,1,0,2,-1]"), 1);
+            Test.Check(solution.SingleNumber, IntArray("[-5,-1,0,2,10,-5,0,2,-1]"), 10);
+            Test.Check(solution.SingleNumber, IntArray("[23456,-5,-1,0,2,10,23456,-5,0,2,-1]"), 10);
+            Test.Check(solution.SingleNumber, IntArray("[23456,-5,-1,0,2,10,23456,-5,0,2,-1,10,66]"), 66);
+        }
+
+    }
+}
---
+++ 0136_SingleNumber/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public int SingleNumber(int[] nums) {
+            int result = 0;
+
+            for (int index = 0; index < nums.Length; index++) {
+                result ^= nums[index];
+            }
+
+            return result;
+        }
+        
+    }
+}
