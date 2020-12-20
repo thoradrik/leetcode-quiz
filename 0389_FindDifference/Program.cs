bb9c9ea5e28904a3b660ebb677c50eac9fd6257e
thoradrik <thoradrik@gmail.com>
2020-12-20 19:13:00 +0500

    Solved 389. Find the Difference https://leetcode.com/problems/find-the-difference/

---
+++ 0389_FindDifference/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.FindTheDifference, "abcd", "abcde", 'e');
+            Test.Check(solution.FindTheDifference, "", "y", 'y');
+            Test.Check(solution.FindTheDifference, "a", "aa", 'a');
+            Test.Check(solution.FindTheDifference, "ae", "aea", 'a');
+        }
+
+    }
+}
---
+++ 0389_FindDifference/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace Quiz {
+    public class Solution {
+
+        public char FindTheDifference(string s, string t) {
+            char r = '\0';
+            
+            for (int i = 0; i < s.Length; i++) {
+                r ^= s[i];
+            }
+            for (int i = 0; i < t.Length; i++) {
+                r ^= t[i];
+            }
+
+            return r;
+        }
+
+    }
+}
