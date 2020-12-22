27ff6e54d858f8e71bd655db1f8e1eed399ce222
thoradrik <thoradrik@gmail.com>
2020-12-22 18:23:09 +0500

    Solved 735. Asteroid Collision https://leetcode.com/problems/asteroid-collision/

---
+++ problems/0735_AsteroidCollision/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.AsteroidCollision, IntArray("[]"), IntArray("[]"));
+            Test.Check(solution.AsteroidCollision, IntArray("[1]"), IntArray("[1]"));
+            Test.Check(solution.AsteroidCollision, IntArray("[-1]"), IntArray("[-1]"));
+            Test.Check(solution.AsteroidCollision, IntArray("[-1,1]"), IntArray("[-1,1]"));
+            Test.Check(solution.AsteroidCollision, IntArray("[-1,10]"), IntArray("[-1,10]"));
+            Test.Check(solution.AsteroidCollision, IntArray("[10,-1]"), IntArray("[10]"));
+            Test.Check(solution.AsteroidCollision, IntArray("[10,-1,-2]"), IntArray("[10]"));
+            Test.Check(solution.AsteroidCollision, IntArray("[10,-1,-2,-3]"), IntArray("[10]"));
+            Test.Check(solution.AsteroidCollision, IntArray("[10,-1,-2,-3,-11]"), IntArray("[-11]"));
+            Test.Check(solution.AsteroidCollision, IntArray("[10,-1,-2,-3,-11,-10]"), IntArray("[-11,-10]"));
+            Test.Check(solution.AsteroidCollision, IntArray("[10,-1,-2,-3,-11,-10,2]"), IntArray("[-11,-10,2]"));
+            Test.Check(solution.AsteroidCollision, IntArray("[5,10,-5]"), IntArray("[5,10]"));
+            Test.Check(solution.AsteroidCollision, IntArray("[8,-8]"), IntArray("[]"));
+            Test.Check(solution.AsteroidCollision, IntArray("[10,2,-5]"), IntArray("[10]"));
+            Test.Check(solution.AsteroidCollision, IntArray("[-2,-1,1,2]"), IntArray("[-2,-1,1,2]"));
+            Test.Check(solution.AsteroidCollision, IntArray("[5,-2,-1,1,2,-4]"), IntArray("[5]"));
+            Test.Check(solution.AsteroidCollision, IntArray("[5,-2,-1,1,2,-4,-6]"), IntArray("[-6]"));
+            Test.Check(solution.AsteroidCollision, IntArray("[5,-2,-1,1,2,-4,-6,10]"), IntArray("[-6,10]"));
+        }
+
+    }
+}
---
+++ problems/0735_AsteroidCollision/Solution.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+
+        public int[] AsteroidCollision(int[] asteroids) {
+            List<int> list = new List<int>(asteroids.Length);
+            
+            foreach (int asteroid in asteroids) {
+                if (asteroid < 0) {
+                    while (list.Count > 0) {
+                        int prev = list[^1];
+                        if (prev < 0) {
+                            break;
+                        }
+                        
+                        if (prev < -asteroid) {
+                            list.RemoveAt(list.Count - 1);
+                        } else {
+                            if (prev == -asteroid) {
+                                list.RemoveAt(list.Count - 1);
+                            }
+                            
+                            goto next_asteroid;
+                        }
+                    }
+                }
+
+                list.Add(asteroid);
+                
+            next_asteroid: 
+                ;
+            }
+
+            return list.ToArray();
+        }
+        
+        /*
+         
+        public int[] AsteroidCollision(int[] asteroids) {
+            bool[] exploded = new bool[asteroids.Length];
+            
+            bool was_explodes;
+            do {
+                was_explodes = false;
+
+                for (int i = 0; i < asteroids.Length; i++) {
+                    if (exploded[i]) {
+                        continue;
+                    }
+
+                    int i_mass = asteroids[i];
+                    if (i_mass < 0) {
+                        continue;
+                    }
+
+                    for (int j = i + 1; j < asteroids.Length; j++) {
+                        if (exploded[j]) {
+                            continue;
+                        } 
+                        
+                        int j_mass = asteroids[j];
+                        if (j_mass < 0) {
+                            int d = i_mass + j_mass;
+                            if (d == 0) {
+                                was_explodes = true;
+                                exploded[i] = true;
+                                exploded[j] = true;
+                                i = j;
+                                break;
+                            } else if (d < 0) {
+                                was_explodes = true;
+                                exploded[i] = true;
+                                i = j - 1;
+                                break;
+                            } else {
+                                was_explodes = true;
+                                exploded[j] = true;
+                            }
+                        } else {
+                            i = j - 1;
+                            break;
+                        }
+                    }
+                }
+            } while (was_explodes);
+            
+            List<int> list = new List<int>();
+
+            for (int index = 0; index < asteroids.Length; index++) {
+                if (!exploded[index]) {
+                    list.Add(asteroids[index]);
+                }
+            }
+
+            return list.ToArray();
+        }
+        
+        */       
+    }
+    
+}
