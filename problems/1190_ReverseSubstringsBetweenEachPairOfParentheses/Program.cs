904264fc44a85000d31c4ef2b02d8efc2cccf664
thoradrik <thoradrik@gmail.com>
2020-12-23 18:51:31 +0500

    Solved 1190. Reverse Substrings Between Each Pair of Parentheses https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses/

---
+++ problems/1190_ReverseSubstringsBetweenEachPairOfParentheses/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.ReverseParentheses, "", "");
+            Test.Check(solution.ReverseParentheses, "a", "a");
+            Test.Check(solution.ReverseParentheses, "(a)", "a");
+            Test.Check(solution.ReverseParentheses, "(ab)", "ba");
+            Test.Check(solution.ReverseParentheses, "(ab)(c)", "bac");
+            Test.Check(solution.ReverseParentheses, "(ab)(cd)", "badc");
+            Test.Check(solution.ReverseParentheses, "((ba)(cd))", "cdba");
+            Test.Check(solution.ReverseParentheses, "(abcd)", "dcba");
+            Test.Check(solution.ReverseParentheses, "(u(love)i)", "iloveu");
+            Test.Check(solution.ReverseParentheses, "(ed(et(oc))el)", "leetcode");
+            Test.Check(solution.ReverseParentheses, "(ed(et(oc))el)", "leetcode");
+            Test.Check(solution.ReverseParentheses, "a(bcdefghijkl(mno)p)q", "apmnolkjihgfedcbq");
+            Test.Check(solution.ReverseParentheses, "a(bcde()fghijkl(mno)p)q", "apmnolkjihgfedcbq");
+        }
+
+    }
+}
---
+++ problems/1190_ReverseSubstringsBetweenEachPairOfParentheses/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+        
+        public string ReverseParentheses(string s) {
+            List<char> Recursive(int start, out int stop) {
+                List<char> result = new List<char>();
+
+                for (stop = start + 1; stop < s.Length; stop++) {
+                    char c = s[stop];
+                    if (c == '(') {
+                        List<char> sub = Recursive(stop, out stop);
+                    
+                        sub.Reverse();
+                    
+                        result.AddRange(sub);
+                    } else if (c == ')') {
+                        break;
+                    } else {
+                        result.Add(c);
+                    }
+                }
+
+                return result;
+            }
+            
+            return String.Concat(Recursive(-1, out _));
+        }
+
+    }
+}
