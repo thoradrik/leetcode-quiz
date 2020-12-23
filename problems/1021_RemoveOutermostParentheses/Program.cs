63d5d33c5c8a6cd978f42a48c37a0b9fac71a8a9
thoradrik <thoradrik@gmail.com>
2020-12-23 18:29:35 +0500

    Solved 1021. Remove Outermost Parentheses https://leetcode.com/problems/remove-outermost-parentheses/

---
+++ problems/1021_RemoveOutermostParentheses/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            // Test.Check(solution.RemoveOuterParentheses, "", "");
+            // Test.Check(solution.RemoveOuterParentheses, "()", "");
+            // Test.Check(solution.RemoveOuterParentheses, "()()", "");
+            Test.Check(solution.RemoveOuterParentheses, "(())()", "()");
+            Test.Check(solution.RemoveOuterParentheses, "(())(())", "()()");
+            Test.Check(solution.RemoveOuterParentheses, "(())(()())", "()()()");
+            Test.Check(solution.RemoveOuterParentheses, "()(())(()())", "()()()");
+            Test.Check(solution.RemoveOuterParentheses, "(()())(()())", "()()()()");
+            Test.Check(solution.RemoveOuterParentheses, "(()())()(()())", "()()()()");
+            Test.Check(solution.RemoveOuterParentheses, "(()())()((())())", "()()(())()");
+        }
+
+    }
+}
---
+++ problems/1021_RemoveOutermostParentheses/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace Quiz {
+    public class Solution {
+
+        public string RemoveOuterParentheses(string S) {
+            StringBuilder sb = new StringBuilder();
+
+            int depth = 0;
+            foreach (char c in S) {
+                if (c == '(') {
+                    if (depth > 0) {
+                        sb.Append(c);
+                    }
+                    depth++;
+                } else {
+                    depth--;
+                    if (depth > 0) {
+                        sb.Append(c);
+                    }
+                }
+            }
+            
+            return sb.ToString();
+        }
+        
+    }
+}
