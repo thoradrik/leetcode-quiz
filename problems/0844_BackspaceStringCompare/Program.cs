36423798559378540a88b24a6cce154d1c1da8e7
thoradrik <thoradrik@gmail.com>
2020-12-25 09:39:30 +0500

    Solved 844. Backspace String Compare https://leetcode.com/problems/backspace-string-compare/

---
+++ problems/0844_BackspaceStringCompare/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.BackspaceCompare, "", "", true);
+            Test.Check(solution.BackspaceCompare, "#", "", true);
+            Test.Check(solution.BackspaceCompare, "", "q", false);
+            Test.Check(solution.BackspaceCompare, "", "q#", true);
+            Test.Check(solution.BackspaceCompare, "s", "s", true);
+            Test.Check(solution.BackspaceCompare, "s", "q", false);
+            Test.Check(solution.BackspaceCompare, "ps#", "p", true);
+            Test.Check(solution.BackspaceCompare, "ps#", "pk#", true);
+            Test.Check(solution.BackspaceCompare, "ps#", "pk##", false);
+            Test.Check(solution.BackspaceCompare, "ps#p", "pkp##p", true);
+            Test.Check(solution.BackspaceCompare, "ps#pp##p", "pkp##p", true);
+        }
+
+    }
+}
---
+++ problems/0844_BackspaceStringCompare/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace Quiz {
+    public class Solution {
+
+        public bool BackspaceCompare(string S, string T) {
+            return Backspace(S) == Backspace(T);
+        }
+        
+        private string Backspace(string S) {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in S) {
+                if (c == '#') {
+                    if (sb.Length > 0) {
+                        sb.Remove(sb.Length - 1, 1);
+                    }
+                } else {
+                    sb.Append(c);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
