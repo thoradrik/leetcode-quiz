c2d873de02129304cd6bf887cd65d2ff2c1c8554
thoradrik <thoradrik@gmail.com>
2020-12-27 23:59:00 +0500

    Solved 32. Longest Valid Parentheses https://leetcode.com/problems/longest-valid-parentheses/submissions/

---
+++ problems/0032_LongestValidParentheses/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            // Test.Check(solution.LongestValidParentheses, "", 0);
+            // Test.Check(solution.LongestValidParentheses, "(", 0);
+            // Test.Check(solution.LongestValidParentheses, ")", 0);
+            // Test.Check(solution.LongestValidParentheses, "()", 2);
+            // Test.Check(solution.LongestValidParentheses, "(())", 4);
+            // Test.Check(solution.LongestValidParentheses, "(())()", 6);
+            // Test.Check(solution.LongestValidParentheses, "(())())", 6);
+            // Test.Check(solution.LongestValidParentheses, "((())())", 8);
+            // Test.Check(solution.LongestValidParentheses, "((())())()", 10);
+            // Test.Check(solution.LongestValidParentheses, "((())())()()", 12);
+            // Test.Check(solution.LongestValidParentheses, "((())())()())", 12);
+            // Test.Check(solution.LongestValidParentheses, "((())())()()))", 12);
+            // Test.Check(solution.LongestValidParentheses, ")((())())()()))", 12);
+            // Test.Check(solution.LongestValidParentheses, "))((())())()()))", 12);
+            Test.Check(solution.LongestValidParentheses, "))((())())()()))(()()()())(())(())", 18);
+            
+            
+        }
+
+    }
+}
---
+++ problems/0032_LongestValidParentheses/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Quiz {
+    public class Solution {
+        
+        public int LongestValidParentheses(string s) {
+            bool[] flags = new bool[s.Length];
+            int max = 0;
+
+            for (int end = 1; end < s.Length; end++) {
+                int start = end - 1;
+                if (s[start] == '(' && s[end] == ')') {
+                    flags[start] = true;
+                    flags[end] = true;
+                }
+
+                bool expanded;
+                do {
+                    expanded = false;
+                    
+                    while (start >= 0 && flags[start]) {
+                        start--;
+                        expanded = true;
+                    }
+                    while (end < s.Length && flags[end]) {
+                        end++;
+                        expanded = true;
+                    }
+                    while (start >= 0 && s[start] == '(' && end < s.Length && s[end] == ')') {
+                        flags[start] = true;
+                        start--;
+
+                        flags[end] = true;
+                        end++;
+                        
+                        expanded = true;
+                    }
+                } while (expanded);
+
+                max = Math.Max(max, end - start - 1);
+            }
+            
+            return max;
+        }
+        
+    }
+}
