b7f6003f17395685ba7a6be4da56390c62abb0c3
thoradrik <thoradrik@gmail.com>
2020-12-27 17:49:57 +0500

    Solved 1003. Check If Word Is Valid After Substitutions https://leetcode.com/problems/check-if-word-is-valid-after-substitutions/

---
+++ problems/1003_CheckIfWordIsValidAfterSubstitutions/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            // Test.Check(solution.IsValid, "", true);
+            // Test.Check(solution.IsValid, "a", false);
+            Test.Check(solution.IsValid, "abc", true);
+            Test.Check(solution.IsValid, "abcabc", true);
+            Test.Check(solution.IsValid, "ababccabc", true);
+            Test.Check(solution.IsValid, "abababcccabc", true);
+            Test.Check(solution.IsValid, "abababcabcccabc", true);
+            Test.Check(solution.IsValid, "abababcababccccabc", true);
+            Test.Check(solution.IsValid, "abababcababcabccccabc", true);
+            Test.Check(solution.IsValid, "abababcababcabcabccccabc", true);
+            Test.Check(solution.IsValid, "abababcababcabcabcabccccabc", true);
+            Test.Check(solution.IsValid, "aabcbc", true);
+            Test.Check(solution.IsValid, "abcabcababcc", true);
+            Test.Check(solution.IsValid, "abccba", false);
+        }
+
+    }
+}
---
+++ problems/1003_CheckIfWordIsValidAfterSubstitutions/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+
+        public bool IsValid(string s) {
+            Stack<char> stack = new Stack<char>();
+            
+            foreach (char c in s) {
+                if (c != 'c') {
+                    stack.Push(c);
+                } else if (!stack.TryPop(out char b) || b != 'b' || !stack.TryPop(out char a) || a != 'a') {
+                    return false;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+
+    }
+}
