22ce92ea4de5d3c14b9c87c526d26674a11dce6a
thoradrik <thoradrik@gmail.com>
2020-12-29 19:36:26 +0500

    Solved 401. Binary Watch https://leetcode.com/problems/binary-watch/

---
+++ problems/0401_BinaryWatch/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            // Test.Check(solution.ReadBinaryWatch, 0, StringArray("[\"0:00\"]"));
+            // Test.Check(solution.ReadBinaryWatch, 1, StringArray("[\"0:01\", \"0:02\", \"0:04\", \"0:08\", \"0:16\", \"0:32\", \"1:00\", \"2:00\", \"4:00\", \"8:00\"]"));
+            // Test.Print(solution.ReadBinaryWatch, 2);
+            // Test.Print(solution.ReadBinaryWatch, 3);
+            // Test.Print(solution.ReadBinaryWatch, 4);
+            // Test.Print(solution.ReadBinaryWatch, 5);
+            Test.Print(solution.ReadBinaryWatch, 6);
+            Test.Print(solution.ReadBinaryWatch, 7);
+            Test.Print(solution.ReadBinaryWatch, 8);
+            Test.Print(solution.ReadBinaryWatch, 9);
+            Test.Print(solution.ReadBinaryWatch, 10);
+        }
+
+    }
+}
---
+++ problems/0401_BinaryWatch/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+
+        private static readonly string[][] HOURS = {
+            /* 0 */ new[] { "0" },
+            /* 1 */ new[] { "1","2","4","8" },
+            /* 2 */ new[] { "3","5","6","9","10" },
+            /* 3 */ new[] { "7","11" }
+        };
+        
+        private static readonly string[][] MINUTES = {
+            /* 0 */ new[] { "00" }, 
+            /* 1 */ new[] { "01","02","04","08","16","32" }, 
+            /* 2 */ new[] { "03","05","06","09","10","12","17","18","20","24","33","34","36","40","48" }, 
+            /* 3 */ new[] { "07","11","13","14","19","21","22","25","26","28","35","37","38","41","42","44","49","50","52","56" }, 
+            /* 4 */ new[] { "15","23","27","29","30","39","43","45","46","51","53","54","57","58"}, 
+            /* 5 */ new[] { "31","47","55","59" }
+        };
+        
+        public IList<string> ReadBinaryWatch(int num) {
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < Math.Min(HOURS.Length, num + 1); i++) {
+                string[] hours = HOURS[i];
+                if (num - i < MINUTES.Length) {
+                    string[] minutes = MINUTES[num - i];
+                    foreach (string hour in hours) {
+                        foreach (string minute in minutes) {
+                            list.Add(String.Format("{0}:{1}", hour, minute));
+                        }
+                    }
+                }
+            }
+            
+            return list;
+        }
+        
+    }
+}
