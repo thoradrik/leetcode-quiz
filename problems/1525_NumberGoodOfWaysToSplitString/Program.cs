b5ad5e5bd9fd227bb29bbe93b7f993b613183913
thoradrik <thoradrik@gmail.com>
2020-12-29 18:55:08 +0500

    Solved 1525. Number of Good Ways to Split a String https://leetcode.com/problems/number-of-good-ways-to-split-a-string/

---
+++ problems/1525_NumberGoodOfWaysToSplitString/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.NumSplits, "", 0);
+            Test.Check(solution.NumSplits, "a", 0);
+            Test.Check(solution.NumSplits, "aa", 1);
+            Test.Check(solution.NumSplits, "ab", 1);
+            Test.Check(solution.NumSplits, "abc", 0);
+            Test.Check(solution.NumSplits, "abcc", 0);
+            Test.Check(solution.NumSplits, "abcd", 1);
+            Test.Check(solution.NumSplits, "aacaba", 2);
+            Test.Check(solution.NumSplits, "abcd", 1);
+            Test.Check(solution.NumSplits, "aaaaa", 4);
+            Test.Check(solution.NumSplits, "acbadbaada", 2);
+        }
+
+    }
+}
---
+++ problems/1525_NumberGoodOfWaysToSplitString/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+        
+        public int NumSplits(string s) {
+            if (s.Length < 2) {
+                return 0;
+            } else if (s.Length < 3) {
+                return 1;
+            }
+            
+            HashSet<char> left_hs = new HashSet<char>();
+            HashSet<char> right_hs = new HashSet<char>();
+
+            int[] left_uniques = new int[s.Length - 1];
+            int[] right_uniques = new int[s.Length - 1];
+            for (int i = 0; i < s.Length - 1; i++) {
+                char lc = s[i];
+                left_hs.Add(lc);
+                left_uniques[i] = left_hs.Count;
+
+                char rc = s[^(i + 1)];
+                right_hs.Add(rc);
+                right_uniques[^(i + 1)] = right_hs.Count;
+            }
+
+            int count = 0;
+            
+            for (int i = 0; i < s.Length - 1; i++) {
+                if (left_uniques[i] == right_uniques[i]) {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+        
+    }
+}
