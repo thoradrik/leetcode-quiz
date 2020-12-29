d24a1969013454fe4006faf79ecef2b0829d04e9
thoradrik <thoradrik@gmail.com>
2020-12-29 19:00:40 +0500

    Solved 58. Length of Last Word https://leetcode.com/problems/length-of-last-word/

---
+++ problems/0058_LengthOfLastWord/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.LengthOfLastWord, "", 0);
+            Test.Check(solution.LengthOfLastWord, "H", 1);
+            Test.Check(solution.LengthOfLastWord, " H ", 1);
+            Test.Check(solution.LengthOfLastWord, "Hell", 4);
+            Test.Check(solution.LengthOfLastWord, "Hell ", 4);
+            Test.Check(solution.LengthOfLastWord, "Hello", 5);
+            Test.Check(solution.LengthOfLastWord, "Hello ", 5);
+            Test.Check(solution.LengthOfLastWord, "Hello World", 5);
+            Test.Check(solution.LengthOfLastWord, "Hello World X ", 1);
+            Test.Check(solution.LengthOfLastWord, "Hello World XXX  ", 3);
+        }
+
+    }
+}
---
+++ problems/0058_LengthOfLastWord/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public int LengthOfLastWord(string s) {
+            int index = s.Length - 1;
+            
+            while (index >= 0 && s[index] == ' ') {
+                index--;
+            }
+
+            int end = index;
+            
+            while (index >= 0 && s[index] != ' ') {
+                index--;
+            }
+
+            int start = index;
+
+            return end - start;
+        }
+        
+    }
+}
