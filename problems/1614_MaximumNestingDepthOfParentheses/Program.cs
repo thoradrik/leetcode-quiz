1edf00af0c2f23533f8013eb75ffec703fdc2f60
thoradrik <thoradrik@gmail.com>
2020-12-29 18:35:37 +0500

    Solved 1614. Maximum Nesting Depth of the Parentheses https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/

---
+++ problems/1614_MaximumNestingDepthOfParentheses/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.MaxDepth, "", 0);
+            Test.Check(solution.MaxDepth, "()(D)", 1);
+            Test.Check(solution.MaxDepth, "(A)((B)(C))", 2);
+            Test.Check(solution.MaxDepth, "(1+(2*3)+((8)/4))+1", 3);
+            Test.Check(solution.MaxDepth, "(1)+((2))+(((3)))", 3);
+            Test.Check(solution.MaxDepth, "1+(2*3)/(2-1)", 1);
+        }
+
+    }
+}
---
+++ problems/1614_MaximumNestingDepthOfParentheses/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Quiz {
+    public class Solution {
+        
+        public int MaxDepth(string s) {
+            int max_depth = 0;
+            int depth = 0;
+            
+            foreach (char c in s) {
+                if (c == '(') {
+                    depth++;
+                    max_depth = Math.Max(max_depth, depth);
+                } else if (c == ')') {
+                    depth--;
+                }
+            }
+
+            return max_depth;
+        }
+        
+    }
+}
