33e2557324e29c71f75bed511b3d8c73d08d2531
thoradrik <thoradrik@gmail.com>
2021-01-05 20:15:39 +0500

    Solved 46. Permutations https://leetcode.com/problems/permutations/submissions/

---
+++ problems/0046_Permutations/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Print(solution.Permute, IntArray("[]"));
+            Test.Print(solution.Permute, IntArray("[1]"));
+            Test.Print(solution.Permute, IntArray("[1,2]"));
+            Test.Print(solution.Permute, IntArray("[1,2,3]"));
+            Test.Print(solution.Permute, IntArray("[1,2,3,4]"));
+            Test.Print(solution.Permute, IntArray("[1,2,3,4,5]"));
+            Test.Print(solution.Permute, IntArray("[1,2,3,4,5,6]"));
+            Test.Print(solution.Permute, IntArray("[1,2,3,4,5,6,7]"));
+        }
+
+    }
+}
---
+++ problems/0046_Permutations/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLib;
+
+namespace Quiz {
+    public class Solution {
+        
+        public IList<IList<int>> Permute(int[] nums) {
+            IList<IList<int>> list = new List<IList<int>>();
+            
+            PermutationGenerator.Generate(nums, variant => list.Add(variant.ToArray()));
+            
+            int[] stack = new int[nums.Length];
+            int ptr = 0;
+
+            list.Add(new List<int>(nums));
+
+            while (ptr < nums.Length) {
+                if (stack[ptr] < ptr) {
+                    if (ptr % 2 == 0) {
+                        Swap(ref nums[0], ref nums[ptr]);
+                    } else {
+                        Swap(ref nums[stack[ptr]], ref nums[ptr]);
+                    }
+
+                    list.Add(new List<int>(nums));
+
+                    stack[ptr]++;
+                    ptr = 0;
+                } else {
+                    stack[ptr] = 0;
+                    ptr++;
+                }
+            }
+            
+            return list;
+        }
+
+        private static void Swap<T>(ref T a, ref T b) {
+            T tmp = a;
+            a = b;
+            b = tmp;
+        }
+        
+    }
+}
