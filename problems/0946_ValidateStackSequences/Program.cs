2a6214d64e4d7c1e0726e93dd3c0d279be81fc9e
thoradrik <thoradrik@gmail.com>
2021-01-08 18:23:34 +0500

    Solved 946. Validate Stack Sequences https://leetcode.com/problems/validate-stack-sequences/

---
+++ problems/0946_ValidateStackSequences/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.ValidateStackSequences, IntArray("[]"), IntArray("[]"), true);
+            Test.Check(solution.ValidateStackSequences, IntArray("[1]"), IntArray("[1]"), true);
+            Test.Check(solution.ValidateStackSequences, IntArray("[1,2]"), IntArray("[2,1]"), true);
+            Test.Check(solution.ValidateStackSequences, IntArray("[1,2]"), IntArray("[1,2]"), true);
+            Test.Check(solution.ValidateStackSequences, IntArray("[1,2,3]"), IntArray("[1,2,3]"), true);
+            Test.Check(solution.ValidateStackSequences, IntArray("[1,2,3]"), IntArray("[2,1,3]"), true);
+            Test.Check(solution.ValidateStackSequences, IntArray("[1,2,3]"), IntArray("[2,3,1]"), true);
+            Test.Check(solution.ValidateStackSequences, IntArray("[1,2,3]"), IntArray("[3,2,1]"), true);
+            Test.Check(solution.ValidateStackSequences, IntArray("[1,2,3]"), IntArray("[1,3,2]"), true);
+            Test.Check(solution.ValidateStackSequences, IntArray("[1,2,3]"), IntArray("[3,1,2]"), false);
+            Test.Check(solution.ValidateStackSequences, IntArray("[1,2,3,4]"), IntArray("[1,2,3,4]"), true);
+            Test.Check(solution.ValidateStackSequences, IntArray("[1,2,3,4]"), IntArray("[1,2,4,3]"), true);
+            Test.Check(solution.ValidateStackSequences, IntArray("[1,2,3,4]"), IntArray("[1,3,2,4]"), true);
+            Test.Check(solution.ValidateStackSequences, IntArray("[1,2,3,4]"), IntArray("[1,4,3,2]"), true);
+            Test.Check(solution.ValidateStackSequences, IntArray("[1,2,3,4]"), IntArray("[4,3,2,1]"), true);
+            Test.Check(solution.ValidateStackSequences, IntArray("[1,2,3,4,5]"), IntArray("[4,5,3,2,1]"), true);
+            Test.Check(solution.ValidateStackSequences, IntArray("[1,2,3,4,5]"), IntArray("[4,3,5,1,2]"), false);
+            Test.Check(solution.ValidateStackSequences, IntArray("[0,2,1]"), IntArray("[0,1,2]"), true);
+        }
+
+    }
+}
---
+++ problems/0946_ValidateStackSequences/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+        
+        public bool ValidateStackSequences(int[] pushed, int[] popped) {
+            Stack<int> stack = new Stack<int>();
+
+            int i = 0;
+            
+            foreach (int push in pushed) {
+                stack.Push(push);
+                
+                while (stack.TryPeek(out int peek) && i < popped.Length && peek == popped[i]) {
+                    stack.Pop();
+                    i++;
+                }
+            }
+
+            return i == popped.Length;
+        }
+        
+    }
+}
