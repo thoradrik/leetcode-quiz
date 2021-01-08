43b31c7255c6d6b55f880858b49a18ca10079978
thoradrik <thoradrik@gmail.com>
2021-01-08 18:47:02 +0500

    Solved 1544. Make The String Great https://leetcode.com/problems/make-the-string-great/

---
+++ problems/1544_MakeStringGreat/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.MakeGood, "leEeetcode", "leetcode");
+            Test.Check(solution.MakeGood, "abBAcC", "");
+            Test.Check(solution.MakeGood, "dabBAcCD", "");
+            Test.Check(solution.MakeGood, "eEdabBAcCD", "");
+            Test.Check(solution.MakeGood, "FeEdabBAcCDf", "");
+            Test.Check(solution.MakeGood, "FeEdabBAczZCDf", "");
+            Test.Check(solution.MakeGood, "FeEdabBAczZCDfQq1", "1");
+            Test.Check(solution.MakeGood, "s", "s");
+        }
+
+    }
+}
---
+++ problems/1544_MakeStringGreat/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Quiz {
+    public class Solution {
+        
+        public string MakeGood(string s) {
+            char[] stack = new char[s.Length];
+            int ptr = 0;
+
+            for (int i = 0; i < s.Length; i++) {
+                char c = s[i];
+                if (ptr > 0) {
+                    char prev = stack[ptr - 1];
+                    if (prev - c == 32 || c - prev == 32) {
+                        ptr--;
+                    } else {
+                        stack[ptr++] = c;
+                    }
+                } else {
+                    stack[ptr++] = c;
+                }
+            }
+            
+            return new string(stack, 0, ptr);
+        }
+        
+    }
+}
