7f8ed4839c93a25b26e79a8589cc557647e51987
thoradrik <thoradrik@gmail.com>
2021-01-08 17:26:28 +0500

    Solved 636. Exclusive Time of Functions https://leetcode.com/problems/exclusive-time-of-functions/

---
+++ problems/0636_ExclusiveTimeOfFunctions/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.ExclusiveTime, 1, StringArray("[\"0:start:0\",\"0:end:0\"]"), IntArray("[1]"));
+            Test.Check(solution.ExclusiveTime, 1, StringArray("[\"0:start:0\",\"0:end:1\"]"), IntArray("[2]"));
+            Test.Check(solution.ExclusiveTime, 2, StringArray("[\"0:start:0\",\"1:start:2\",\"1:end:5\",\"0:end:6\"]"), IntArray("[3,4]"));
+            Test.Check(solution.ExclusiveTime, 1, StringArray("[\"0:start:0\",\"0:start:2\",\"0:end:5\",\"0:start:6\",\"0:end:6\",\"0:end:7\"]"), IntArray("[8]"));
+            Test.Check(solution.ExclusiveTime, 2, StringArray("[\"0:start:0\",\"0:start:2\",\"0:end:5\",\"1:start:6\",\"1:end:6\",\"0:end:7\"]"), IntArray("[7,1]"));
+            Test.Check(solution.ExclusiveTime, 2, StringArray("[\"0:start:0\",\"0:start:2\",\"0:end:5\",\"1:start:7\",\"1:end:7\",\"0:end:8\"]"), IntArray("[8,1]"));
+            Test.Check(solution.ExclusiveTime, 1, StringArray("[\"0:start:0\",\"0:start:1\",\"0:start:2\",\"0:end:3\",\"0:end:4\",\"0:end:5\"]"), IntArray("[6]"));
+        }
+
+    }
+}
---
+++ problems/0636_ExclusiveTimeOfFunctions/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+
+        private class Frame {
+
+            public Frame(int function, int time_stamp) {
+                Function = function;
+                Start = time_stamp;
+            }
+
+            public readonly int Function;
+
+            public int Length;
+
+            public readonly int Start;
+
+        }
+        
+        public int[] ExclusiveTime(int n, IList<string> logs) {
+            int[] times = new int[n];
+
+            Stack<Frame> stack = new Stack<Frame>();
+
+            foreach (string s in logs) {
+                string[] ss = s.Split(':');
+                int function = Int32.Parse(ss[0]);
+                int time_stamp = Int32.Parse(ss[2]);
+
+                if (ss[1][0] == 's') {
+                    stack.Push(new Frame(function, time_stamp));
+                } else {
+                    Frame frame = stack.Pop();
+                    
+                    int frame_length = time_stamp - frame.Start + 1;
+                    if (stack.TryPeek(out Frame prev)) {
+                        prev.Length -= frame_length;
+                    }
+                    
+                    frame.Length += frame_length;
+
+                    times[frame.Function] += frame.Length;
+                }
+            }
+            
+            return times;
+        }
+        
+    }
+}
