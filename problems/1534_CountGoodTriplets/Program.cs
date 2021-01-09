7a2a825909c0225265311069f0845b204fedc3c4
thoradrik <thoradrik@gmail.com>
2021-01-09 15:09:47 +0500

    Solved 1534. Count Good Triplets https://leetcode.com/problems/count-good-triplets/

---
+++ problems/1534_CountGoodTriplets/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.CountGoodTriplets, IntArray("[3,0,1,1,9,7]"), 7, 2, 3, 4);
+            Test.Check(solution.CountGoodTriplets, IntArray("[1,1,2,2,3]"), 0, 0, 1, 0);
+        }
+
+    }
+}
---
+++ problems/1534_CountGoodTriplets/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Quiz {
+    public class Solution {
+
+        public int CountGoodTriplets(int[] arr, int a, int b, int c) {
+            int count = 0;
+            
+            for (int i = 0; i < arr.Length - 2; i++) {
+                int ai = arr[i];
+                
+                for (int j = i + 1; j < arr.Length - 1; j++) {
+                    int aj = arr[j];
+                    if (Math.Abs(ai - aj) > a) {
+                        continue;
+                    }
+                    
+                    for (int k = j + 1; k < arr.Length; k++) {
+                        int ak = arr[k];
+                        if (Math.Abs(aj - ak) > b) {
+                            continue;
+                        }
+                        
+                        if (Math.Abs(ak - ai) > c) {
+                            continue;
+                        }
+
+                        count++;
+                    }
+                }   
+            }
+
+            return count;
+        }
+
+    }
+}
