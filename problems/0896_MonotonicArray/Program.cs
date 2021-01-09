a1d56520733019bc9a49f7ba32cd194bb528e67f
thoradrik <thoradrik@gmail.com>
2021-01-09 13:35:29 +0500

    Solved 896. Monotonic Array https://leetcode.com/problems/monotonic-array/

---
+++ problems/0896_MonotonicArray/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.IsMonotonic, IntArray("[1,2,2,3]"), true);
+            Test.Check(solution.IsMonotonic, IntArray("[6,5,4,4]"), true);
+            Test.Check(solution.IsMonotonic, IntArray("[1,3,5]"), true);
+            Test.Check(solution.IsMonotonic, IntArray("[1,3,2]"), false);
+            Test.Check(solution.IsMonotonic, IntArray("[1,2,4,5]"), true);
+            Test.Check(solution.IsMonotonic, IntArray("[1,2,5,4]"), false);
+            Test.Check(solution.IsMonotonic, IntArray("[1,1,1]"), true);
+            Test.Check(solution.IsMonotonic, IntArray("[1,1,2]"), true);
+            Test.Check(solution.IsMonotonic, IntArray("[1,2,1]"), false);
+        }
+
+    }
+}
---
+++ problems/0896_MonotonicArray/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public bool IsMonotonic(int[] A) {
+            int direction = 0;
+            
+            for (int i = 1; i < A.Length; i++) {
+                int a_prev = A[i - 1];
+                int a_i = A[i];
+                if (a_prev != a_i) {
+                    if (a_prev < a_i) {
+                        if (direction > 0) {
+                            return false;
+                        } else {
+                            direction = -1;
+                        }
+                    } else {
+                        if (direction < 0) {
+                            return false;
+                        } else {
+                            direction = 1;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+        
+    }
+}
