3521fda2f084ab73a5d1cf59a8a4edfa995a5f70
thoradrik <thoradrik@gmail.com>
2021-01-09 15:02:09 +0500

    Solved 1480. Running Sum of 1d Array https://leetcode.com/problems/running-sum-of-1d-array/

---
+++ problems/1480_RunningSumOf1DArray/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.RunningSum, IntArray("[]"), IntArray("[]"));
+            Test.Check(solution.RunningSum, IntArray("[0]"), IntArray("[0]"));
+            Test.Check(solution.RunningSum, IntArray("[-10]"), IntArray("[-10]"));
+            Test.Check(solution.RunningSum, IntArray("[1,2]"), IntArray("[1,3]"));
+            Test.Check(solution.RunningSum, IntArray("[0,1,2]"), IntArray("[0,1,3]"));
+            Test.Check(solution.RunningSum, IntArray("[-1,0,1]"), IntArray("[-1,-1,0]"));
+            Test.Check(solution.RunningSum, IntArray("[1,2,3,4]"), IntArray("[1,3,6,10]"));
+            Test.Check(solution.RunningSum, IntArray("[1,1,1,1,1]"), IntArray("[1,2,3,4,5]"));
+        }
+
+    }
+}
