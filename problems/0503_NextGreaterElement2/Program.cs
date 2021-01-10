b8ee6ee06a43b56468665b5c9b15cfc1f3cd1a9e
thoradrik <thoradrik@gmail.com>
2021-01-10 14:17:42 +0500

    Solved 503. Next Greater Element II https://leetcode.com/problems/next-greater-element-ii/

---
+++ problems/0503_NextGreaterElement2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.NextGreaterElements, IntArray("[]"), IntArray("[]"));
+            Test.Check(solution.NextGreaterElements, IntArray("[1]"), IntArray("[-1]"));
+            Test.Check(solution.NextGreaterElements, IntArray("[1,1]"), IntArray("[-1,-1]"));
+            Test.Check(solution.NextGreaterElements, IntArray("[1,1,2]"), IntArray("[2,2,-1]"));
+            Test.Check(solution.NextGreaterElements, IntArray("[3,1,2]"), IntArray("[-1,2,3]"));
+            Test.Check(solution.NextGreaterElements, IntArray("[4,3,1,2]"), IntArray("[-1,4,2,4]"));
+            Test.Check(solution.NextGreaterElements, IntArray("[4,5,3,1,2]"), IntArray("[5,-1,4,2,4]"));
+            Test.Check(solution.NextGreaterElements, IntArray("[6,4,5,3,1,2]"), IntArray("[-1,5,6,6,2,6]"));
+            Test.Check(solution.NextGreaterElements, IntArray("[6,4,5,3,6,1,2]"), IntArray("[-1,5,6,6,-1,2,6]"));
+        }
+
+    }
+}
---
+++ problems/0503_NextGreaterElement2/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+        
+        public int[] NextGreaterElements(int[] nums) {
+            int[] nge = new int[nums.Length];
+            
+            Stack<int> stack = new Stack<int>();
+            
+            for (int j = 2 * nums.Length - 1; j >= 0; j--) {
+                int i = j % nums.Length;
+
+                while (stack.TryPeek(out int i_top) && nums[i_top] <= nums[i]) {
+                    stack.Pop();
+                }
+
+                if (stack.TryPeek(out int i_nge)) {
+                    nge[i] = nums[i_nge];
+                } else {
+                    nge[i] = -1;
+                }
+
+                stack.Push(i);
+            }
+
+            return nge;
+        }
+        
+    }
+}
