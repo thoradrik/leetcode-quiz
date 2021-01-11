a803d5fadece24cfffa33ae6b48cf2e8a6b05753
thoradrik <thoradrik@gmail.com>
2021-01-11 20:36:49 +0500

    Solved 556. Next Greater Element III https://leetcode.com/problems/next-greater-element-iii/submissions/

---
+++ problems/0556_NextGreaterElement3/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.NextGreaterElement, 241, 412);
+            Test.Check(solution.NextGreaterElement, 23241, 23412);
+            Test.Check(solution.NextGreaterElement, 230241, 230412);
+            Test.Check(solution.NextGreaterElement, 132, 312);
+            Test.Check(solution.NextGreaterElement, 1132, 1213);
+            Test.Check(solution.NextGreaterElement, 15421, 25114);
+            Test.Check(solution.NextGreaterElement, 511, -1);
+            Test.Check(solution.NextGreaterElement, 516, 561);
+            Test.Check(solution.NextGreaterElement, 5116, 5161);
+            Test.Check(solution.NextGreaterElement, 51166, 51616);
+            
+            Test.Check(solution.NextGreaterElement, 1, -1);
+            Test.Check(solution.NextGreaterElement, 11, -1);
+            Test.Check(solution.NextGreaterElement, 102, 120);
+            Test.Check(solution.NextGreaterElement, 101, 110);
+            Test.Check(solution.NextGreaterElement, 12, 21);
+            Test.Check(solution.NextGreaterElement, 21, -1);
+            Test.Check(solution.NextGreaterElement, 123, 132);
+            Test.Check(solution.NextGreaterElement, 1234, 1243);
+            Test.Check(solution.NextGreaterElement, 1243, 1324);
+            Test.Check(solution.NextGreaterElement, 2147483647, -1);
+            Test.Check(solution.NextGreaterElement, 2147473648, 2147473684);
+        }
+
+    }
+}
---
+++ problems/0556_NextGreaterElement3/Solution.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+        
+        public int NextGreaterElement(int n) {
+            int[] digits = GetDigits(n);
+
+            if (TryNextGreaterElement(digits)) {
+                return GetNumber(digits);
+            } else {
+                return -1;
+            }
+        }
+        
+        public bool TryNextGreaterElement(int[] digits) {
+            for (int i = digits.Length - 2; i >= 0; i--) {
+                int d_i = digits[i];
+
+                int min = 10;
+                int min_j = -1;
+                
+                for (int j = i + 1; j < digits.Length; j++) {
+                    int d_j = digits[j];
+
+                    if (d_i < d_j) {
+                        if (d_j < min) {
+                            min = d_j;
+                            min_j = j;
+                        }
+                    }
+                }
+
+                if (min_j > 0) {
+                    int tmp = digits[i];
+                    digits[i] = digits[min_j];
+                    digits[min_j] = tmp;
+
+                    TryMinimizeElement(digits, i + 1);
+                    
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void TryMinimizeElement(int[] digits, int end) {
+            Array.Sort(digits, end, digits.Length - end);    
+        }
+
+        private static int[] GetDigits(int n) {
+            List<int> list = new List<int>();
+
+            while (n > 0) {
+                list.Insert(0, n % 10);
+                n /= 10;
+            }
+
+            return list.ToArray();
+        }
+
+        private static int GetNumber(int[] digits) {
+            long result = 0;
+
+            for (int i = 0; i < digits.Length; i++) {
+                result = result * 10 + digits[i];
+            }
+
+            return result > Int32.MaxValue ? -1 : (int)result;
+        }
+
+    }
+}
