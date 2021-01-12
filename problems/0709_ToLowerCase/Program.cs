1d3a218865eb5686ce08e0813446364360f82076
thoradrik <thoradrik@gmail.com>
2021-01-12 19:19:57 +0500

    Solved 709. To Lower Case https://leetcode.com/problems/to-lower-case/

---
+++ problems/0709_ToLowerCase/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.ToLowerCase, "", "");
+            Test.Check(solution.ToLowerCase, "a", "a");
+            Test.Check(solution.ToLowerCase, "A", "a");
+            Test.Check(solution.ToLowerCase, "1", "1");
+            Test.Check(solution.ToLowerCase, "Ab", "ab");
+            Test.Check(solution.ToLowerCase, "AB", "ab");
+            Test.Check(solution.ToLowerCase, "LEETcode", "leetcode");
+            Test.Check(solution.ToLowerCase, "LeetCode", "leetcode");
+        }
+
+    }
+}
---
+++ problems/0709_ToLowerCase/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace Quiz {
+    public class Solution {
+        
+        public string ToLowerCase(string str) {
+            StringBuilder sb = new StringBuilder(str.Length);
+
+            foreach (char c in str) {
+                if (c >= 'A' && c <= 'Z') {
+                    sb.Append((char) (c + 32));
+                } else {
+                    sb.Append(c);
+                }
+            }
+            
+            return sb.ToString();
+        }
+        
+    }
+}
