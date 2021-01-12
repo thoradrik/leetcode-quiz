894ae8dbeb518f037f442284e6518605b438de16
thoradrik <thoradrik@gmail.com>
2021-01-12 20:31:19 +0500

    Solved 1163. Last Substring in Lexicographical Order https://leetcode.com/problems/last-substring-in-lexicographical-order/

---
+++ problems/1163_LastSubstringInLexicographicalOrder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.LastSubstring, "", "");
+            Test.Check(solution.LastSubstring, "a", "a");
+            Test.Check(solution.LastSubstring, "ab", "b");
+            Test.Check(solution.LastSubstring, "abc", "c");
+            Test.Check(solution.LastSubstring, "abcb", "cb");
+            Test.Check(solution.LastSubstring, "abcba", "cba");
+            Test.Check(solution.LastSubstring, "aaa", "aaa");
+            Test.Check(solution.LastSubstring, "zyx", "zyx");
+            Test.Check(solution.LastSubstring, "zyxzzyx", "zzyx");
+            Test.Check(solution.LastSubstring, "abab", "bab");
+            Test.Check(solution.LastSubstring, "leetcode", "tcode");
+            Test.Check(solution.LastSubstring, "cacacb", "cb");
+            Test.Check(solution.LastSubstring, "cacacbcbd", "d");
+            Test.Check(solution.LastSubstring, "cacacbcbdadae", "e");
+            Test.Check(solution.LastSubstring, "ecacacbcbdadae", "ecacacbcbdadae");
+            Test.Check(solution.LastSubstring, "decacacbcbdadae", "ecacacbcbdadae");
+            Test.Check(solution.LastSubstring, new String('A', 40000), new String('A', 40000));
+            Test.Check(solution.LastSubstring, new String('A', 40000)+"B", "B");
+        }
+
+    }
+}
---
+++ problems/1163_LastSubstringInLexicographicalOrder/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Quiz {
+    public class Solution {
+        
+        public string LastSubstring(string s) {
+            int max_start = 0;
+            int max_length = s.Length;
+            
+            for (int start = 1; start < s.Length; start++) {
+                int step = 0;
+                
+                for (int i = 0; i < Math.Min(s.Length - start, max_length); i++) {
+                    char c_s = s[start + i];
+                    char c_m = s[max_start + i];
+                    if (c_s < c_m) {
+                        break;
+                    } else if (c_s > c_m) {
+                        max_start = start;
+                        max_length = s.Length - max_start;
+                        step = Math.Max(step - 1, 0);
+                        break;
+                    } else {
+                        step++;
+                    }
+                }
+
+                start += Math.Max(step - 1, 0);
+            }
+
+            return s.Substring(max_start, max_length);
+        }
+        
+    }
+}
