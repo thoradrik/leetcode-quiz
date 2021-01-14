5ba050d5e764991afb5dcaccecdcffcedde5b72f
thoradrik <thoradrik@gmail.com>
2021-01-14 18:23:30 +0500

    Solved 150. Evaluate Reverse Polish Notation https://leetcode.com/problems/evaluate-reverse-polish-notation/

---
+++ problems/0150_EvaluateReversePolishNotation/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.EvalRPN, StringArray("[\"2\",\"1\",\"+\",\"3\",\"*\"]"), 9);
+            Test.Check(solution.EvalRPN, StringArray("[\"4\",\"13\",\"5\",\"/\",\"+\"]"), 6);
+            Test.Check(solution.EvalRPN, StringArray("[\"10\",\"6\",\"9\",\"3\",\"+\",\"-11\",\"*\",\"/\",\"*\",\"17\",\"+\",\"5\",\"+\"]"), 22);
+        }
+
+    }
+}
---
+++ problems/0150_EvaluateReversePolishNotation/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+        
+        public int EvalRPN(string[] tokens) {
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < tokens.Length; i++) {
+                string token = tokens[i];
+                if (token == "+" || token == "-" || token == "*" || token == "/") {
+                    if (stack.TryPop(out int b) && stack.TryPop(out int a)) {
+                        switch (token) {
+                            case "+": stack.Push(a + b); break;
+                            case "-": stack.Push(a - b); break;
+                            case "*": stack.Push(a * b); break;
+                            case "/": stack.Push(a / b); break;
+                        }
+                    } else {
+                        throw new Exception("Got '" + token + "' token. But stack does not contains at least 2 numbers");
+                    }
+                } else if (Int32.TryParse(token, out int number)) {
+                    stack.Push(number);
+                } else {
+                    throw new Exception("Got invalid token '" + token + "'");
+                }
+            }
+
+            return stack.Pop();
+        }
+        
+    }
+}
