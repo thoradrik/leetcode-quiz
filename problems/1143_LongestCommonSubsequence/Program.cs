44e680ce5f53993ff165300a3f99aa27ff80a14a
thoradrik <thoradrik@gmail.com>
2021-01-17 19:27:19 +0500

    Solved 1143. Longest Common Subsequence https://leetcode.com/problems/longest-common-subsequence/

---
+++ problems/1143_LongestCommonSubsequence/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.LongestCommonSubsequence, "", "", 0);
+            Test.Check(solution.LongestCommonSubsequence, "a", "", 0);
+            Test.Check(solution.LongestCommonSubsequence, "a", "b", 0);
+            Test.Check(solution.LongestCommonSubsequence, "a", "a", 1);
+            Test.Check(solution.LongestCommonSubsequence, "aa", "aa", 2);
+            Test.Check(solution.LongestCommonSubsequence, "aaa", "aab", 2);
+            Test.Check(solution.LongestCommonSubsequence, "aaa", "aaa", 3);
+            Test.Check(solution.LongestCommonSubsequence, "aaab", "aaac", 3);
+            Test.Check(solution.LongestCommonSubsequence, "aaab", "aaacaaabaaaa", 4);
+        }
+
+    }
+}
