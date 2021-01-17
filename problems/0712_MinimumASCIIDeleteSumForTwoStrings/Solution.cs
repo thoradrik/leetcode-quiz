e9301f2ddb94a5a58e0eea3bc1655669060c643c
thoradrik <thoradrik@gmail.com>
2021-01-17 19:04:36 +0500

    Solved 712. Minimum ASCII Delete Sum for Two Strings https://leetcode.com/problems/minimum-ascii-delete-sum-for-two-strings/

---
+++ problems/0712_MinimumASCIIDeleteSumForTwoStrings/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Quiz {
+    public class Solution {
+        
+        public int MinimumDeleteSum(string s1, string s2) {
+            int[,] dp = new int[s1.Length + 1, s2.Length + 1];
+
+            for (int i = s1.Length - 1; i >= 0; i--) {
+                dp[i, s2.Length] = dp[i + 1, s2.Length] + s1[i];
+            }
+            for (int j = s2.Length - 1; j >= 0; j--) {
+                dp[s1.Length, j] = dp[s1.Length, j + 1] + s2[j];
+            }
+
+            for (int i = s1.Length - 1; i >= 0; i--) {
+                for (int j = s2.Length - 1; j >= 0; j--) {
+                    if (s1[i] == s2[j]) {
+                        dp[i, j] = dp[i + 1, j + 1];
+                    } else {
+                        dp[i, j] = Math.Min(dp[i + 1, j] + s1[i], dp[i, j + 1] + s2[j]);
+                    }
+                }
+            }
+
+            return dp[0, 0];
+        }
+        
+    }
+}
