af266bbbe48afefa2925a9443338ce83d191636d
thoradrik <thoradrik@gmail.com>
2021-01-17 19:17:26 +0500

    Solved 583. Delete Operation for Two Strings https://leetcode.com/problems/delete-operation-for-two-strings/

---
+++ problems/0583_DeleteOperationForTwoStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.MinDistance, "", "", 0);
+            Test.Check(solution.MinDistance, "a", "", 1);
+            Test.Check(solution.MinDistance, "", "b", 1);
+            Test.Check(solution.MinDistance, "a", "b", 2);
+            Test.Check(solution.MinDistance, "aa", "ba", 2);
+            Test.Check(solution.MinDistance, "aa", "bac", 3);
+            Test.Check(solution.MinDistance, "aac", "bac", 2);
+            Test.Check(solution.MinDistance, "aa", "bb", 4);
+            Test.Check(solution.MinDistance, "sea", "eat", 2);
+            Test.Check(solution.MinDistance, "sea", "eatsea", 3);
+            Test.Check(solution.MinDistance, "sea", "eatseaz", 4);
+            Test.Check(solution.MinDistance, "seaz", "eatseaz", 3);
+            Test.Check(solution.MinDistance, "beer", "cake", 6);
+        }
+
+    }
+}
---
+++ problems/0583_DeleteOperationForTwoStrings/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Quiz {
+    public class Solution {
+        
+        public int MinDistance(string word1, string word2) {
+            int[,] dp = new int[word1.Length + 1, word2.Length + 1];
+
+            for (int i = word1.Length - 1; i >= 0; i--) {
+                dp[i, word2.Length] = dp[i + 1, word2.Length] + 1;
+            }
+            for (int j = word2.Length - 1; j >= 0; j--) {
+                dp[word1.Length, j] = dp[word1.Length, j + 1] + 1;
+            }
+
+            for (int i = word1.Length - 1; i >= 0; i--) {
+                for (int j = word2.Length - 1; j >= 0; j--) {
+                    if (word1[i] == word2[j]) {
+                        dp[i, j] = dp[i + 1, j + 1];
+                    } else {
+                        dp[i, j] = Math.Min(dp[i + 1, j], dp[i, j + 1]) + 1;
+                    }
+                }
+            }
+
+            return dp[0, 0];        
+        }
+        
+    }
+}
