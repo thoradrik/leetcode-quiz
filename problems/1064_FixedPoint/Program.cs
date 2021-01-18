1a3568aebc51ff949963a8517122636a37301c10
thoradrik <thoradrik@gmail.com>
2021-01-18 19:30:10 +0500

    Solved 1064. Fixed Point https://leetcode.com/problems/fixed-point/

---
+++ problems/1064_FixedPoint/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.FixedPoint, IntArray("[1]"), -1);
+            Test.Check(solution.FixedPoint, IntArray("[0]"), 0);
+            Test.Check(solution.FixedPoint, IntArray("[0,1]"), 0);
+            Test.Check(solution.FixedPoint, IntArray("[-1,0,1]"), -1);
+            Test.Check(solution.FixedPoint, IntArray("[-1,-2,2,3]"), 2);
+            Test.Check(solution.FixedPoint, IntArray("[-10,-5,0,3,7]"), 3);
+            Test.Check(solution.FixedPoint, IntArray("[0,2,5,8,17]"), 0);
+            Test.Check(solution.FixedPoint, IntArray("[-10,-5,3,4,7,9]"), -1);
+            Test.Check(solution.FixedPoint, IntArray("[-9,-8,-7,-5,3,2,6]"), 6);
+            Test.Check(solution.FixedPoint, IntArray("[-9,-8,-7,-5,3,2,5,7]"), 7);
+            Test.Check(solution.FixedPoint, IntArray("[-9,-8,-7,-5,3,2,5,7,8]"), 7);
+            Test.Check(solution.FixedPoint, IntArray("[-9,-8,-7,-5,3,2,5,6,8,9]"), 8);
+            Test.Check(solution.FixedPoint, IntArray("[-9,-8,-7,-5,3,2,5,6,7,9]"), 9);
+            Test.Check(solution.FixedPoint, IntArray("[-10,-5,-2,0,4,5,6,7,8,9,10]"), 4);
+        }
+
+    }
+}
---
+++ problems/1064_FixedPoint/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public int FixedPoint(int[] arr) {
+            int i_lo = 0;
+            int i_hi = arr.Length - 1;
+
+            int i_min = -1;
+            
+            do {
+                int i = (i_lo + i_hi) / 2;
+                int ai = arr[i];
+                if (ai >= i) {
+                    if (ai == i) {
+                        i_min = i;
+                    }
+                    
+                    i_hi = i - 1;
+                } else {
+                    i_lo = i + 1;
+                }
+            } while (i_lo <= i_hi);
+            
+            return i_min;
+        }
+        
+    }
+}
