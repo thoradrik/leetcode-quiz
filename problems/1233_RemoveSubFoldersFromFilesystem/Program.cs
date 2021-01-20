1973a4ad9e9d89de10b868a3d0b9b2990fbab4dd
thoradrik <thoradrik@gmail.com>
2021-01-20 17:36:23 +0500

    Solved 1233. Remove Sub-Folders from the Filesystem https://leetcode.com/problems/remove-sub-folders-from-the-filesystem/

---
+++ problems/1233_RemoveSubFoldersFromFilesystem/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.RemoveSubfolders, StringArray("[\"/a\"]"), StringArray("[\"/a\"]"));
+            Test.Check(solution.RemoveSubfolders, StringArray("[\"/a\",\"/b\"]"), StringArray("[\"/a\",\"/b\"]"));
+            Test.Check(solution.RemoveSubfolders, StringArray("[\"/a\",\"/a/b\"]"), StringArray("[\"/a\"]"));
+            Test.Check(solution.RemoveSubfolders, StringArray("[\"/a/b\",\"/a/c\"]"), StringArray("[\"/a/b\",\"/a/c\"]"));
+            Test.Check(solution.RemoveSubfolders, StringArray("[\"/a/b\",\"/a/c\"]"), StringArray("[\"/a/b\",\"/a/c\"]"));
+
+            Test.Check(solution.RemoveSubfolders, StringArray("[\"/a\",\"/a/b\",\"/c/d\",\"/c/d/e\",\"/c/f\"]"), StringArray("[\"/a\",\"/c/d\",\"/c/f\"]"));
+            Test.Check(solution.RemoveSubfolders, StringArray("[\"/a\",\"/a/b/c\",\"/a/b/d\"]"), StringArray("[\"/a\"]"));
+            Test.Check(solution.RemoveSubfolders, StringArray("[\"/a/b/c\",\"/a/b/ca\",\"/a/b/d\"]"), StringArray("[\"/a/b/c\",\"/a/b/ca\",\"/a/b/d\"]"));
+        }
+
+    }
+}
---
+++ problems/1233_RemoveSubFoldersFromFilesystem/Solution.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+
+        private class Node {
+
+            public string Name;
+            
+            public bool Delete;
+            
+            public Dictionary<string, Node> Children;
+
+        }
+        
+        public IList<string> RemoveSubfolders(string[] folder) {
+            void push(Node node, string path) {
+                if (String.IsNullOrEmpty(path)) {
+                    node.Delete = true;
+                } else {
+                    int index = path.IndexOf('/', 1);
+                    string name = index > 0 ? path.Substring(0, index) : path;
+
+                    if (node.Children == null) {
+                        node.Children = new Dictionary<string, Node>();
+                    }
+
+                    if (!node.Children.TryGetValue(name, out Node child_node)) {
+                        child_node = new Node {
+                            Name = name
+                        };
+                        node.Children[name] = child_node;
+                    }
+                    
+                    string least_path = index > 0 ? path.Substring(index) : String.Empty;
+                    push(child_node, least_path);
+                }
+            }
+            
+            Node root = new Node();
+
+            foreach (string f_path in folder) {
+                push(root, f_path);
+            }
+
+            List<string> list = new List<string>();
+
+            void pop(Node node, string parent_path) {
+                string node_path = parent_path + node.Name;
+                if (node.Delete) {
+                    list.Add(node_path);
+                } else {
+                    foreach (KeyValuePair<string, Node> pair in node.Children) {
+                        pop(pair.Value, node_path);
+                    }
+                }
+            }
+            
+            pop(root, String.Empty);
+            
+            return list;
+        }
+    }
+}
