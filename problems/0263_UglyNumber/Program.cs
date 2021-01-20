126e6591612d2ef998593601f5d0203eb3ab3deb
thoradrik <thoradrik@gmail.com>
2021-01-20 18:05:51 +0500

    Solved 263. Ugly Number https://leetcode.com/problems/ugly-number/

---
+++ problems/0263_UglyNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.IsUgly, -1024, false);
+            Test.Check(solution.IsUgly, -2, false);
+            Test.Check(solution.IsUgly, -3, false);
+            Test.Check(solution.IsUgly, -5, false);
+            Test.Check(solution.IsUgly, 0, false);
+            Test.Check(solution.IsUgly, 1, false);
+            Test.Check(solution.IsUgly, 2, true);
+            Test.Check(solution.IsUgly, 3, true);
+            Test.Check(solution.IsUgly, 5, true);
+            Test.Check(solution.IsUgly, 6, true);
+            Test.Check(solution.IsUgly, 7, false);
+            Test.Check(solution.IsUgly, 8, true);
+            Test.Check(solution.IsUgly, 9, true);
+            Test.Check(solution.IsUgly, 1073741824, true);
+            Test.Check(solution.IsUgly, 1073741823, false);
+            Test.Check(solution.IsUgly, 2147483647, false);
+            Test.Check(solution.IsUgly, -2147483644, false);
+        }
+
+    }
+}
