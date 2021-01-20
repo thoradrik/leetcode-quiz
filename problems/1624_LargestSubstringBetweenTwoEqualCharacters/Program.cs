b9ae56a87efd9fed0f73b8ec85ca39ec1d9c565e
thoradrik <thoradrik@gmail.com>
2021-01-20 17:54:13 +0500

    Solved 1624. Largest Substring Between Two Equal Characters https://leetcode.com/problems/largest-substring-between-two-equal-characters/

---
+++ problems/1624_LargestSubstringBetweenTwoEqualCharacters/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.MaxLengthBetweenEqualCharacters, "a", -1);
+            Test.Check(solution.MaxLengthBetweenEqualCharacters, "aa", 0);
+            Test.Check(solution.MaxLengthBetweenEqualCharacters, "abca", 2);
+            Test.Check(solution.MaxLengthBetweenEqualCharacters, "cbzxy", -1);
+            Test.Check(solution.MaxLengthBetweenEqualCharacters, "cabbac", 4);
+            Test.Check(solution.MaxLengthBetweenEqualCharacters, "cabcbac", 5);
+            Test.Check(solution.MaxLengthBetweenEqualCharacters, "acabcbac", 5);
+            Test.Check(solution.MaxLengthBetweenEqualCharacters, "acabccbac", 6);
+            Test.Check(solution.MaxLengthBetweenEqualCharacters, "daacabccbac", 7);
+            Test.Check(solution.MaxLengthBetweenEqualCharacters, "dcaacabccbac", 9);
+        }
+
+    }
+}
---
+++ problems/1624_LargestSubstringBetweenTwoEqualCharacters/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Quiz {
+    public class Solution {
+        
+        public int MaxLengthBetweenEqualCharacters(string s) {
+            int[] min = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+            int[] max = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+            
+            int max_lenght = -1;
+            
+            for (int i = 0; i < s.Length; i++) {
+                int c = s[i] - 'a';
+                
+                min[c] = min[c] >= 0 ? Math.Min(min[c], i) : i;
+                max[c] = max[c] >= 0 ? Math.Max(max[c], i) : i;
+                
+                if (max[c] != min[c]) {
+                    max_lenght = Math.Max(max_lenght, max[c] - min[c] - 1);
+                }
+            }
+            
+            return max_lenght;
+        }
+        
+    }
+}
