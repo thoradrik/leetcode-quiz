32b1fbe5775d67b7eb65bb0ddfa5cff3cc09db10
thoradrik <thoradrik@gmail.com>
2021-01-21 19:06:07 +0500

    Solved 313. Super Ugly Number https://leetcode.com/problems/super-ugly-number/

---
+++ problems/0313_SuperUglyNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.NthSuperUglyNumber, 10, IntArray("[2,3,5]"), 12);
+            Test.Check(solution.NthSuperUglyNumber, 12, IntArray("[2,7,13,19]"), 32);
+            Test.Check(solution.NthSuperUglyNumber, 1000, IntArray("[2,3,5,7,11,13]"), 1341988864);
+        }
+
+    }
+}
---
+++ problems/0313_SuperUglyNumber/Solution.cs
@@ -0,0 +1,37 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public int NthSuperUglyNumber(int n, int[] primes) {
+            int[] nums = new int[n];
+            nums[0] = 1;
+
+            int[] ptr = new int[primes.Length];
+            int[] muls = new int[primes.Length];
+            for (int p = 0; p < primes.Length; p++) {
+                muls[p] = nums[ptr[p]] * primes[p];
+            }
+            
+            for (int i = 1; i < n; i++) {
+                int min = muls[0];
+                
+                for (int p = 1; p < primes.Length; p++) {
+                    if (muls[p] < min) {
+                        min = muls[p];
+                    }
+                }
+
+                nums[i] = min;
+
+                for (int p = 0; p < primes.Length; p++) {
+                    if (muls[p] == min) {
+                        ptr[p]++;
+                        muls[p] = nums[ptr[p]] * primes[p];
+                    }
+                }
+            }
+
+            return nums[n - 1];
+        }
+        
+    }
+}
