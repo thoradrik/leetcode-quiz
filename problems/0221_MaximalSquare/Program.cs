2257941a87385975c2d267c435fd490c3a31c1e6
thoradrik <thoradrik@gmail.com>
2021-01-21 19:36:53 +0500

    Solved 221. Maximal Square https://leetcode.com/problems/maximal-square/

---
+++ problems/0221_MaximalSquare/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"0\"]]"), 0);
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"1\"]]"), 0);
+            
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"0\",\"0\"],[\"0\",\"0\"]]"), 0);
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"1\",\"0\"],[\"0\",\"0\"]]"), 1);
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"1\",\"1\"],[\"0\",\"0\"]]"), 1);
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"1\",\"1\"],[\"1\",\"0\"]]"), 1);
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"0\",\"1\"],[\"1\",\"0\"]]"), 1);
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"1\",\"1\"],[\"1\",\"1\"]]"), 4);
+            
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"0\",\"0\",\"0\"],[\"0\",\"0\",\"0\"]]"), 0);
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"1\",\"0\",\"0\"],[\"0\",\"0\",\"0\"]]"), 1);
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"1\",\"0\",\"0\"],[\"1\",\"0\",\"0\"]]"), 1);
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"1\",\"1\",\"0\"],[\"1\",\"0\",\"0\"]]"), 1);
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"1\",\"1\",\"0\"],[\"1\",\"1\",\"0\"]]"), 4);
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"1\",\"1\",\"1\"],[\"1\",\"1\",\"1\"]]"), 4);
+            
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"0\",\"0\",\"0\"],[\"0\",\"1\",\"0\"],[\"0\",\"0\",\"0\"]]"), 1);
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"0\",\"0\",\"0\"],[\"0\",\"1\",\"1\"],[\"0\",\"0\",\"0\"]]"), 1);
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"0\",\"0\",\"0\"],[\"0\",\"1\",\"1\"],[\"0\",\"1\",\"0\"]]"), 1);
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"0\",\"0\",\"0\"],[\"0\",\"1\",\"1\"],[\"0\",\"1\",\"1\"]]"), 4);
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"1\",\"1\",\"0\"],[\"0\",\"1\",\"1\"],[\"0\",\"1\",\"1\"]]"), 4);
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"1\",\"1\",\"0\"],[\"1\",\"1\",\"1\"],[\"0\",\"1\",\"1\"]]"), 4);
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"1\",\"1\",\"1\"],[\"1\",\"1\",\"1\"],[\"0\",\"1\",\"1\"]]"), 4);
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"1\",\"1\",\"1\"],[\"1\",\"1\",\"1\"],[\"1\",\"1\",\"1\"]]"), 9);
+            
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"1\",\"0\",\"1\",\"0\",\"0\"],[\"1\",\"0\",\"1\",\"1\",\"1\"],[\"1\",\"1\",\"1\",\"1\",\"1\"],[\"1\",\"0\",\"0\",\"1\",\"0\"]]"), 4);
+            Test.Check(solution.MaximalSquare, Char2Array("[[\"1\",\"1\",\"1\",\"1\",\"0\"],[\"1\",\"1\",\"1\",\"1\",\"0\"],[\"1\",\"1\",\"1\",\"1\",\"1\"],[\"1\",\"1\",\"1\",\"1\",\"1\"],[\"0\",\"0\",\"1\",\"1\",\"1\"]]"), 16);
+            
+        }
+
+    }
+}
---
+++ problems/0221_MaximalSquare/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Quiz {
+    public class Solution {
+        
+        public int MaximalSquare(char[][] matrix) {
+            int max_height = matrix.Length;
+            if (max_height <= 0) {
+                return 0;
+            }
+            int max_width = matrix[0].Length;
+            if (max_width <= 0) {
+                return 0;
+            }
+
+            int[,] dp = new int[max_height + 1, max_width + 1];
+
+            int max_area = 0;
+
+            for (int row = max_height - 1; row >= 0; row--) {
+                for (int col = max_width - 1; col >= 0; col--) {
+                    int length = 0;
+
+                    if (matrix[row][col] == '1') {
+                        int length_n = dp[row + 1, col + 1];
+                        int length_r = dp[row + 1, col];
+                        int length_b = dp[row, col + 1];
+                        if (length_n > 0 && length_r > 0 && length_b > 0) {
+                            length = Math.Min(length_n, Math.Min(length_r, length_b)) + 1;
+                        } else {
+                            length = 1;
+                        }
+                    }
+                    
+                    max_area = Math.Max(max_area, length * length);
+                    
+                    dp[row, col] = length;
+                }   
+            }
+            
+            
+            return max_area;
+        }
+        
+    }
+}
