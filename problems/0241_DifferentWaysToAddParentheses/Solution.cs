ba0733aa643cdc512e5abb0d08a3f80347f0aa9d
thoradrik <thoradrik@gmail.com>
2021-01-22 18:41:42 +0500

    Solved 241. Different Ways to Add Parentheses https://leetcode.com/problems/different-ways-to-add-parentheses/

---
+++ problems/0241_DifferentWaysToAddParentheses/Solution.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using AlgorithmLib;
+
+namespace Quiz {
+    public class Solution {
+        
+        public IList<int> DiffWaysToCompute(string input) {
+            Token[] tokens = Tokenize(input);
+
+            Dictionary<Tuple<int, int>, List<int>> memo = new Dictionary<Tuple<int, int>, List<int>>();
+
+            List<int> backtrace(int start, int end) {
+                Tuple<int, int> key = new Tuple<int, int>(start, end);
+
+                if (memo.TryGetValue(key, out List<int> result)) {
+                    return result;
+                }
+                
+                result = new List<int>();
+
+                if (start == end) {
+                    if (tokens[start] is Number n) {
+                        result.Add(n.Value);
+                    } else {
+                        throw new Exception("Not a number");
+                    }
+                } else {
+                    for (int i = start; i <= end; i++) {
+                        if (tokens[i] is Operation op) {
+                            foreach (int l_value in backtrace(start, i - 1)) {
+                                foreach (int r_value in backtrace(i + 1, end)) {
+                                    result.Add(op.Func(l_value, r_value));
+                                }
+                            }
+
+                            result.Sort();
+                        }
+                    }
+                }
+
+                memo[key] = result;
+
+                return result;
+            }
+
+            return backtrace(0, tokens.Length - 1);
+        }
+
+        
+        private class Token { }
+
+        private class Number : Token {
+
+            public int Value;
+
+            public override string ToString() => Value.ToString();
+            
+        }
+
+        private class Operation : Token {
+
+            public char Char;
+
+            public int Func(int x, int y) {
+                switch (Char) {
+                    case '+': return x + y;
+                    case '-':return x - y;
+                    case '*': return x * y;
+                }
+                return 0;
+            }
+
+            public override string ToString() => Char.ToString();
+            
+        }
+
+        private Token[] Tokenize(string input) {
+            List<Token> list = new List<Token>();
+
+            int v_index = -1;
+            int v_len = 0;
+
+            for (int index = 0; index < input.Length; index++) {
+                char c = input[index];
+                if (c >= '0' && c <= '9') {
+                    if (v_index >= 0) {
+                        v_len++;
+                    } else {
+                        v_index = index;
+                        v_len = 1;
+                    }
+                } else if (c == '+' || c == '-' || c == '*') {
+                    if (v_index >= 0) {
+                        list.Add(new Number { Value = Int32.Parse(input.Substring(v_index, v_len)) });
+                        
+                        v_index = -1;
+                        v_len = 0;
+                        
+                        list.Add(new Operation { Char = c });
+                    } else {
+                        throw new Exception("Invalid char");
+                    }
+                } else {
+                    throw new Exception("Invalid char");
+                }
+            }
+
+            if (v_index >= 0) {
+                list.Add(new Number { Value = Int32.Parse(input.Substring(v_index, v_len)) });
+            }
+            
+            return list.ToArray();
+        }
+        
+    }
+}
---
+++ problems/0241_DifferentWaysToAddParentheses/Solution2.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using AlgorithmLib;
+
+namespace Quiz {
+    public class Solution2 {
+        
+        public IList<int> DiffWaysToCompute(string input) {
+            List<int> results = new List<int>();
+
+            Token[] tokenized = Tokenize(input);
+            
+            PermutationGenerator.Generate(tokenized, tokens => {
+                for (int i = 0; i < tokens.Length; i++) {
+                    Token token = tokens[i];
+                    token.EvaluatedValue = null;
+                }
+                
+                for (int i = 0; i < tokens.Length; i++) {
+                    Token token = tokens[i];
+
+                    Token prev = token.Prev;
+                    int l_value = prev?.EvaluatedValue ?? token.LValue;
+                    
+                    Token next = token.Next;
+                    int r_value = next?.EvaluatedValue ?? token.RValue;
+
+                    token.EvaluatedValue = token.Op(l_value, r_value);
+
+                    while (prev != null && prev.EvaluatedValue != null) {
+                        prev.EvaluatedValue = token.EvaluatedValue;
+                        prev = prev.Prev;
+                    }
+                    while (next != null && next.EvaluatedValue != null) {
+                        next.EvaluatedValue = token.EvaluatedValue;
+                        next = next.Next;
+                    }
+
+                    Console.Write(token);
+
+                    if (i == tokens.Length - 1) {
+                        results.Add(token.EvaluatedValue.Value);
+                        
+                        Console.Write(" = ");
+                        Console.Write(token.EvaluatedValue.Value);
+                    } else {
+                        Console.Write(" -> ");
+                    }
+                    
+                }
+
+                Console.WriteLine();
+            });
+
+            results.Sort();
+            
+            return results;
+        }
+
+        
+        private class Token {
+
+            public Token Prev;
+            
+            public Token Next;
+            
+            public int LValue;
+            
+            public int RValue;
+            
+            public Func<int, int, int> Op;
+
+            public char Char;
+
+            public override string ToString() => String.Format("{0}{1}{2}", LValue, Char, RValue);
+            
+            public int? EvaluatedValue;
+
+        }
+        
+        private Token[] Tokenize(string input) {
+            List<Token> list = new List<Token>();
+
+            int v_index = -1;
+            int v_len = 0;
+
+            for (int index = 0; index < input.Length; index++) {
+                char c = input[index];
+                if (c >= '0' && c <= '9') {
+                    if (v_index > 0) {
+                        v_len++;
+                    } else {
+                        v_index = index;
+                        v_len = 1;
+                    }
+                } else if (c == '+' || c == '-' || c == '*') {
+                    int value = Int32.Parse(input.Substring(v_index, v_len));
+
+                    v_index = -1;
+                    v_len = 0;
+
+                    Token prev = list.Count > 0 ? list[^1] : null;
+
+                    Token token = new Token {
+                        Char = c,
+                        Prev = prev,
+                        LValue = value
+                    };
+                    
+                    switch (c) {
+                        case '+': 
+                            token.Op = (x, y) => x + y;
+                            break;
+                        case '-':
+                            token.Op = (x, y) => x - y;
+                            break;
+                        case '*': 
+                            token.Op = (x, y) => x * y;
+                            break;
+                    }
+                    
+                    list.Add(token);
+
+                    if (prev != null) {
+                        prev.Next = token;
+                        prev.RValue = value;
+                    }
+                } else {
+                    throw new Exception("Invalid char");
+                }
+            }
+
+            if (v_index >= 0) {
+                int value = Int32.Parse(input.Substring(v_index, v_len));
+
+                if (list.Count > 0) {
+                    Token last = list[^1];
+                    last.RValue = value;
+                } else {
+                    list.Add(new Token {
+                        Char = '=',
+                        RValue = value,
+                        Op = (_,_) => value 
+                    });
+                }
+            }
+            
+            return list.ToArray();
+        }
+        
+    }
+
+    
+}
