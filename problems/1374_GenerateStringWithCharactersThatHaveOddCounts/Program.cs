143dd80092b2175099b0898d93faea4db7a817bf
thoradrik <thoradrik@gmail.com>
2021-01-24 18:54:35 +0500

    Solved 1374. Generate a String With Characters That Have Odd Counts https://leetcode.com/problems/generate-a-string-with-characters-that-have-odd-counts/

---
+++ problems/1374_GenerateStringWithCharactersThatHaveOddCounts/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.GenerateTheString, 0, "");
+            Test.Check(solution.GenerateTheString, 1, "a");
+            Test.Check(solution.GenerateTheString, 2, "ab");
+            Test.Check(solution.GenerateTheString, 3, "aaa");
+            Test.Check(solution.GenerateTheString, 4, "aaab");
+            Test.Check(solution.GenerateTheString, 5, "aaaaa");
+            Test.Check(solution.GenerateTheString, 6, "aaaaab");
+            Test.Check(solution.GenerateTheString, 7, "aaaaaaa");
+            Test.Check(solution.GenerateTheString, 8, "aaaaaaab");
+            Test.Check(solution.GenerateTheString, 9, "aaaaaaaaa");
+        }
+
+    }
+}
---
+++ problems/1374_GenerateStringWithCharactersThatHaveOddCounts/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Quiz {
+    public class Solution {
+
+        private static readonly char[] CHARS = { 'a', 'b' };
+        
+        public string GenerateTheString(int n) {
+            StringBuilder sb = new StringBuilder(n);
+
+            int c_index = 0;
+            while (n > 0) {
+                int c_count = n;
+
+                while (c_count % 2 == 0) {
+                    c_count--;
+                }
+
+                sb.Append(CHARS[c_index], c_count);
+
+                n -= c_count;
+                c_index++;
+            }
+
+            return sb.ToString();
+        }
+        
+    }
+}
