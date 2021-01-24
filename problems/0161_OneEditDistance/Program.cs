eaf02dbe4ce1fa57aa65790bce420b2918718579
thoradrik <thoradrik@gmail.com>
2021-01-24 21:03:06 +0500

    Solved 161. One Edit Distance https://leetcode.com/problems/one-edit-distance/

---
+++ problems/0161_OneEditDistance/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.IsOneEditDistance, "", "", false);
+            Test.Check(solution.IsOneEditDistance, "a", "", true);
+            Test.Check(solution.IsOneEditDistance, "", "a", true);
+            Test.Check(solution.IsOneEditDistance, "a", "a", false);
+            Test.Check(solution.IsOneEditDistance, "a", "b", true);
+            Test.Check(solution.IsOneEditDistance, "b", "a", true);
+            Test.Check(solution.IsOneEditDistance, "aa", "", false);
+            Test.Check(solution.IsOneEditDistance, "aa", "a", true);
+            Test.Check(solution.IsOneEditDistance, "aa", "aa", false);
+            Test.Check(solution.IsOneEditDistance, "aa", "ab", true);
+            Test.Check(solution.IsOneEditDistance, "aa", "aba", true);
+            Test.Check(solution.IsOneEditDistance, "aa", "abaa", false);
+            Test.Check(solution.IsOneEditDistance, "aaa", "abaa", true);
+        }
+
+    }
+}
---
+++ problems/0161_OneEditDistance/Solution.cs
@@ -0,0 +1,36 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public bool IsOneEditDistance(string s, string t) {
+            int distance = s.Length - t.Length;
+            if (distance < -1 || distance > 1) {
+                return false;
+            } else {
+                return IsOneEditDistance(s, 0, s.Length - 1, t, 0, t.Length - 1);
+            }
+        }
+        
+        public bool IsOneEditDistance(string s, int s_start, int s_end, string t, int t_start, int t_end) {
+            while (s_start <= s_end && t_start <= t_end && s[s_start] == t[t_start]) {
+                s_start++;
+                t_start++;
+            }
+            
+            while (s_start <= s_end && t_start <= t_end && s[s_end] == t[t_end]) {
+                s_end--;
+                t_end--;
+            }
+
+            int s_len = s_end - s_start + 1;
+            int t_len = t_end - t_start + 1;
+            if (s_len > 1 || t_len > 1) {
+                return false;
+            } else if (s_len == 0) {
+                return t_len == 1;
+            } else {
+                return true;
+            }
+        }
+        
+    }
+}
