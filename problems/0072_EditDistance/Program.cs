c881e80877cf46589ef01eeac9db00f700f5b356
thoradrik <thoradrik@gmail.com>
2021-01-25 16:49:19 +0500

    Solved 72. Edit Distance https://leetcode.com/problems/edit-distance/submissions/

---
+++ problems/0072_EditDistance/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.MinDistance, "", "a", 1);
+            Test.Check(solution.MinDistance, "", "aa", 2);
+            Test.Check(solution.MinDistance, "a", "", 1);
+            Test.Check(solution.MinDistance, "a", "b", 1);
+            Test.Check(solution.MinDistance, "a", "ab", 1);
+            Test.Check(solution.MinDistance, "aa", "ab", 1);
+            Test.Check(solution.MinDistance, "aab", "ab", 1);
+            Test.Check(solution.MinDistance, "aaba", "ab", 2);
+            Test.Check(solution.MinDistance, "aaba", "abba", 1);
+            Test.Check(solution.MinDistance, "aaba", "abbb", 2);
+            Test.Check(solution.MinDistance, "aaa", "bbb", 3);
+            Test.Check(solution.MinDistance, "aaa", "bbba", 3);
+            Test.Check(solution.MinDistance, "aaa", "bbbaa", 3);
+            
+            Test.Check(solution.MinDistance, "abc", "cba", 2);
+            Test.Check(solution.MinDistance, "abcd", "cba", 3);
+            Test.Check(solution.MinDistance, "abcd", "cbad", 2);
+            Test.Check(solution.MinDistance, "abcdu", "cbady", 3);
+            Test.Check(solution.MinDistance, "kabcdu", "cbady", 4);
+            
+            Test.Check(solution.MinDistance, "horse", "ros", 3);
+            Test.Check(solution.MinDistance, "intention", "execution", 5);
+            Test.Check(solution.MinDistance, "pneumonoultramicroscopicsilicovolcanoconiosis", "ultramicroscopically", 27);
+            Test.Check(solution.MinDistance, "distance", "springbok", 9);
+        }
+
+    }
+}
---
+++ problems/0072_EditDistance/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Quiz {
+    public class Solution {
+        
+        public int MinDistance(string word1, string word2) {
+            if (word1.Length <= 0 || word2.Length <= 0) {
+                return word1.Length + word2.Length;
+            }
+
+            int[,] dp = new int[word1.Length + 1, word2.Length + 1];
+            
+            for (int i = 0; i <= word1.Length; i++) {
+                dp[i, 0] = i;
+            }
+            for (int j = 0; j <= word2.Length; j++) {
+                dp[0, j] = j;
+            }
+
+            for (int i = 1; i <= word1.Length; i++) {
+                for (int j = 1; j <= word2.Length; j++) {
+                    int i_1 = i - 1;
+                    int j_1 = j - 1;
+                    
+                    if (word1[i_1] != word2[j_1]) {
+                        dp[i, j] = Math.Min(dp[i_1, j_1], Math.Min(dp[i_1, j], dp[i, j_1])) + 1;
+                    } else {
+                        dp[i, j] = dp[i_1, j_1];
+                    }
+                }
+            }
+
+            return dp[word1.Length, word2.Length];
+        }
+        
+    }
+}
