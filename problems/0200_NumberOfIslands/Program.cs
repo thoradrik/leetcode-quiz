ea798188798c377dcd3c91a154fd01efa66b7b9d
thoradrik <thoradrik@gmail.com>
2021-01-26 19:21:43 +0500

    Solved 200. Number of Islands https://leetcode.com/problems/number-of-islands/

---
+++ problems/0200_NumberOfIslands/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.NumIslands, Char2Array("[]"), 0);
+            Test.Check(solution.NumIslands, Char2Array("[[]]"), 0);
+            Test.Check(solution.NumIslands, Char2Array("[[\"0\"]]"), 0);
+            Test.Check(solution.NumIslands, Char2Array("[[\"1\"]]"), 1);
+            Test.Check(solution.NumIslands, Char2Array("[[\"0\",\"0\"]]"), 0);
+            Test.Check(solution.NumIslands, Char2Array("[[\"1\",\"0\"]]"), 1);
+            Test.Check(solution.NumIslands, Char2Array("[[\"1\",\"1\"]]"), 1);
+            Test.Check(solution.NumIslands, Char2Array("[[\"1\"],[\"1\"]]"), 1);
+            Test.Check(solution.NumIslands, Char2Array("[[\"1\",\"0\"],[\"0\",\"0\"]]"), 1);
+            Test.Check(solution.NumIslands, Char2Array("[[\"1\",\"1\"],[\"0\",\"0\"]]"), 1);
+            Test.Check(solution.NumIslands, Char2Array("[[\"1\",\"1\"],[\"0\",\"1\"]]"), 1);
+            Test.Check(solution.NumIslands, Char2Array("[[\"1\",\"0\"],[\"0\",\"1\"]]"), 2);
+            Test.Check(solution.NumIslands, Char2Array("[[\"0\",\"1\"],[\"1\",\"0\"]]"), 2);
+            Test.Check(solution.NumIslands, Char2Array("[[\"1\",\"1\"],[\"1\",\"1\"]]"), 1);
+            Test.Check(solution.NumIslands, Char2Array("[[\"1\",\"1\",\"1\",\"1\",\"0\"],[\"1\",\"1\",\"0\",\"1\",\"0\"],[\"1\",\"1\",\"0\",\"0\",\"0\"],[\"0\",\"0\",\"0\",\"0\",\"0\"]]"), 1);
+            Test.Check(solution.NumIslands, Char2Array("[[\"1\",\"1\",\"0\",\"0\",\"0\"],[\"1\",\"1\",\"0\",\"0\",\"0\"],[\"0\",\"0\",\"1\",\"0\",\"0\"],[\"0\",\"0\",\"0\",\"1\",\"1\"]]"), 3);
+        }
+
+    }
+}
---
+++ problems/0200_NumberOfIslands/Solution.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+        
+        public int NumIslands(char[][] grid) {
+            int m = grid.Length;
+            if (m <= 0) {
+                return 0;
+            }
+            
+            int n = grid[0].Length;
+            if (n <= 0) {
+                return 0;
+            }
+
+            int[,] dp = new int[m, n];
+
+            int number = 0;
+
+            Stack<Tuple<int, int>> stack = new Stack<Tuple<int, int>>();
+
+            void push(Tuple<int, int> ptr, int ox, int oy) {
+                int x = ptr.Item1 + ox;
+                int y = ptr.Item2 + oy;
+
+                if (x >= 0 && y >= 0 && x < m && y < n) {
+                    if (grid[x][y] == '1') {
+                        if (dp[x,y] <= 0) {
+                            stack.Push(new Tuple<int, int>(x, y));
+                        }
+                    }
+                }
+            }
+            
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == '1') {
+                        if (dp[i, j] <= 0) {
+                            number++;
+                            
+                            stack.Push(new Tuple<int, int>(i, j));
+
+                            while (stack.TryPop(out Tuple<int, int> c)) {
+                                dp[c.Item1, c.Item2] = number;
+
+                                push(c, -1, 0);
+                                push(c, 0, -1);
+                                push(c, 1, 0);
+                                push(c, 0, 1);
+                            }
+                        }
+                    }
+                }    
+            }
+            
+            return number;
+        }
+        
+    }
+}
