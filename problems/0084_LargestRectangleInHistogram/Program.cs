239678489860c47a42af95e77f39ca79f7691e4c
thoradrik <thoradrik@gmail.com>
2021-02-01 17:45:23 +0500

    Solved 84. Largest Rectangle in Histogram https://leetcode.com/problems/largest-rectangle-in-histogram/

---
+++ problems/0084_LargestRectangleInHistogram/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.LargestRectangleArea, IntArray("[]"), 0);
+            Test.Check(solution.LargestRectangleArea, IntArray("[1]"), 1);
+            Test.Check(solution.LargestRectangleArea, IntArray("[2,1]"), 2);
+            Test.Check(solution.LargestRectangleArea, IntArray("[1,2]"), 2);
+            Test.Check(solution.LargestRectangleArea, IntArray("[1,2,1]"), 3);
+            Test.Check(solution.LargestRectangleArea, IntArray("[1,4,1]"), 4);
+            Test.Check(solution.LargestRectangleArea, IntArray("[1,4,0,1,1]"), 4);
+            Test.Check(solution.LargestRectangleArea, IntArray("[1,4,1,1,1]"), 5);
+            Test.Check(solution.LargestRectangleArea, IntArray("[1,4,1,6,1]"), 6);
+            Test.Check(solution.LargestRectangleArea, IntArray("[2,1,2]"), 3);
+            Test.Check(solution.LargestRectangleArea, IntArray("[1,1,2]"), 3);
+            Test.Check(solution.LargestRectangleArea, IntArray("[2,1,1]"), 3);
+            Test.Check(solution.LargestRectangleArea, IntArray("[2,2,1]"), 4);
+            Test.Check(solution.LargestRectangleArea, IntArray("[1,2,2]"), 4);
+            Test.Check(solution.LargestRectangleArea, IntArray("[2,1,4]"), 4);
+            Test.Check(solution.LargestRectangleArea, IntArray("[2,1,4,2]"), 4);
+            Test.Check(solution.LargestRectangleArea, IntArray("[2,2,4,2]"), 8);
+            Test.Check(solution.LargestRectangleArea, IntArray("[2,1,5,4,2]"), 8);
+            Test.Check(solution.LargestRectangleArea, IntArray("[2,2,5,4,2]"), 10);
+            Test.Check(solution.LargestRectangleArea, IntArray("[2,1,4,3]"), 6);
+            Test.Check(solution.LargestRectangleArea, IntArray("[2,1,5,4,3]"), 9);
+            Test.Check(solution.LargestRectangleArea, IntArray("[2,2,5,4,3]"), 10);
+            Test.Check(solution.LargestRectangleArea, IntArray("[2,4,5,4,3]"), 12);
+            
+            Test.Check(solution.LargestRectangleArea, IntArray("[2,4,0,4,3]"), 6);
+            
+            Test.Check(solution.LargestRectangleArea, IntArray("[2,1,5,6,2,3]"), 10);
+            Test.Check(solution.LargestRectangleArea, IntArray("[2,4]"), 4);
+        }
+
+    }
+}
---
+++ problems/0084_LargestRectangleInHistogram/Solution.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+        
+        private class Rect {
+
+            public Rect(int height, int length) {
+                Height = height;
+                Length = length;
+            }
+
+            public int Height { get; }
+
+            public int Length { get; set; }
+
+            public int Area => Length * Height;
+
+        } 
+        
+        public int LargestRectangleArea(int[] heights) {
+            int max_area = 0;
+
+            List<Rect> rects = null;
+
+            for (int i = 0; i < heights.Length; i++) {
+                int height = heights[i];
+
+                int max_length = 0;
+
+                List<Rect> new_rects = new List<Rect>();
+
+                if (rects != null) {
+                    foreach (Rect rect in rects) {
+                        if (rect.Height < height) {
+                            rect.Length += 1;
+                            new_rects.Add(rect);
+                        } else {
+                            max_area = Math.Max(max_area, rect.Area);
+                            max_length = Math.Max(max_length, rect.Length);
+                        }
+                    }
+                }
+                
+                if (height > 0) {
+                    if (max_length > 0) {
+                        new_rects.Add(new Rect(height, max_length + 1));
+                    } else {
+                        new_rects.Add(new Rect(height, 1));
+                    }
+                }
+
+                rects = new_rects;
+            }
+
+            if (rects != null) {
+                foreach (Rect rect in rects) {
+                    max_area = Math.Max(max_area, rect.Area);
+                }
+            }
+
+            return max_area;
+        }
+        
+    }
+}
