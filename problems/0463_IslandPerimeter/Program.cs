d86d4adc1ebde1005e5327183f2a85dd0244c4ba
thoradrik <thoradrik@gmail.com>
2021-02-01 19:13:55 +0500

    Solved 463. Island Perimeter https://leetcode.com/problems/island-perimeter/

---
+++ problems/0463_IslandPerimeter/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.IslandPerimeter, Int2Array("[]"), 0);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[]]"), 0);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[0]]"), 0);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[1]]"), 4);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[0,0]]"), 0);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[1,0]]"), 4);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[0,1]]"), 4);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[1,1]]"), 6);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[0],[0]]"), 0);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[1],[0]]"), 4);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[0],[1]]"), 4);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[1],[1]]"), 6);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[0,0],[0,0]]"), 0);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[1,0],[0,0]]"), 1);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[1,1],[0,0]]"), 6);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[1,0],[1,0]]"), 6);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[1,1],[1,0]]"), 8);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[1,1],[0,1]]"), 8);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[1,0],[1,1]]"), 8);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[0,1],[1,1]]"), 8);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[1,1],[1,1]]"), 8);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[1,1],[1,1],[1,1]]"), 10);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[1,1],[0,1],[1,1]]"), 12);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]"), 16);
+            Test.Check(solution.IslandPerimeter, Int2Array("[[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]"), 16);
+        }
+
+    }
+}
---
+++ problems/0463_IslandPerimeter/Solution.cs
@@ -0,0 +1,42 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public int IslandPerimeter(int[][] grid) {
+            int m = grid.Length;
+            if (m <= 0) {
+                return 0;
+            }
+            
+            int n = grid[0].Length;
+            if (n <= 0) {
+                return 0;
+            }
+
+            int perimeter = 0;
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == 1) {
+                        perimeter += 4;
+
+                        if (i > 0 && grid[i - 1][j] == 1) {
+                            perimeter--;
+                        }
+                        if (i < m - 1 && grid[i + 1][j] == 1) {
+                            perimeter--;
+                        }
+                        if (j > 0 && grid[i][j - 1] == 1) {
+                            perimeter--;
+                        }
+                        if (j < n - 1 && grid[i][j + 1] == 1) {
+                            perimeter--;
+                        }
+                    }
+                } 
+            } 
+            
+            return perimeter;
+        }
+        
+    }
+}
