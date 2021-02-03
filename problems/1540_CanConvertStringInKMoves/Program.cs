1f3feae352239acd1552f2342123121216f459e0
thoradrik <thoradrik@gmail.com>
2021-02-03 12:03:28 +0500

    Solved 1540. Can Convert String in K Moves https://leetcode.com/problems/can-convert-string-in-k-moves/

---
+++ problems/1540_CanConvertStringInKMoves/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.CanConvertString, "a", "b", 1, true);
+            Test.Check(solution.CanConvertString, "a", "c", 1, false);
+            Test.Check(solution.CanConvertString, "a", "c", 2, true);
+            Test.Check(solution.CanConvertString, "input", "ouput", 9, true);
+            Test.Check(solution.CanConvertString, "abc", "bcd", 10, false);
+            Test.Check(solution.CanConvertString, "aab", "bbb", 27, true);
+        }
+
+    }
+}
---
+++ problems/1540_CanConvertStringInKMoves/Solution.cs
@@ -0,0 +1,45 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public bool CanConvertString(string s, string t, int k) {
+            if (s.Length != t.Length) {
+                return false;
+            } else if (s.Length == 0) {
+                return true;
+            }
+
+            int[] shifts = new int[26];
+            
+            for (int i = 1; i < shifts.Length; i++) {
+                shifts[i] = k / 26;
+            }
+            
+            for (int i = 1; i <= k % 26; i++) {
+                shifts[i]++;
+            }
+            
+            for (int i = 0; i < s.Length; i++) {
+                int c_s = s[i];
+                int c_t = t[i];
+                
+                if (c_s == c_t) {
+                    continue;
+                } 
+                
+                int delta = c_t - c_s;
+                if (delta < 0) {
+                    delta += 26;
+                }
+
+                shifts[delta]--;
+
+                if (shifts[delta] < 0) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        
+    }
+}
