79c4384aab5acac03fe47e27a9538b9ff3c14835
thoradrik <thoradrik@gmail.com>
2021-02-04 18:16:25 +0500

    Solved 1099. Two Sum Less Than K https://leetcode.com/problems/two-sum-less-than-k/

---
+++ problems/1099_TwoSumLessThanK/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Quiz {
+    public class Solution {
+        
+        public int TwoSumLessThanK(int[] nums, int k) {
+            Array.Sort(nums);
+
+            int max = -1;
+            
+            int j_lo = 0;
+            int j_hi = nums.Length - 1;
+            do {
+                int sum = nums[j_lo] + nums[j_hi];
+
+                if (sum >= k) {
+                    j_hi--;
+                } else {
+                    max = Math.Max(max, sum);
+                
+                    j_lo++;
+                }
+            } while (j_lo < j_hi);
+            
+            return max;
+        }
+        
+    }
+}
