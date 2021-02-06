0abe9aaece94813e1da9ad88105c509c23288ded
thoradrik <thoradrik@gmail.com>
2021-02-06 23:39:42 +0500

    Solved 28. Implement strStr() https://leetcode.com/problems/implement-strstr/

---
+++ problems/0028_ImplementStrStr/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+/*
+            Test.Check(solution.StrStr, "", "", 0);
+            Test.Check(solution.StrStr, "a", "b", -1);
+            Test.Check(solution.StrStr, "a", "a", 0);
+            Test.Check(solution.StrStr, "aa", "a", 0);
+            Test.Check(solution.StrStr, "ba", "a", 1);
+            Test.Check(solution.StrStr, "baca", "a", 1);
+            Test.Check(solution.StrStr, "baca", "ac", 1);
+            Test.Check(solution.StrStr, "baca", "aca", 1);
+            Test.Check(solution.StrStr, "bacac", "acb", -1);
+            Test.Check(solution.StrStr, "bacad", "ac", 1);
+            Test.Check(solution.StrStr, "bacad", "ad", 3);
+            Test.Check(solution.StrStr, "babaabaa", "aa", 3);
+            Test.Check(solution.StrStr, "baaabaabaacbaad", "baac", 7);
+            Test.Check(solution.StrStr, "hello", "hello1", -1);
+            Test.Check(solution.StrStr, "hello", "ll", 2);
+*/
+
+            string s = "abcdefghzzzzzzzzzzzlrjflkwerfljwerlkfjweklrfjlwkejrfkl";
+            int len = 10;
+
+            int index = 6;
+            int hash = Solution.Hash(s, index, len);
+            Console.WriteLine("{0} = {1} *", s.Substring(index, len), hash);
+
+            for (int i = 0; i < s.Length - len; i++) {
+                if (i == 0) {
+                    hash = Solution.Hash(s, 0, len);
+                } else {
+                    hash = Solution.Hash(hash, s[i - 1], s[i + len - 1]);
+                }
+                
+                Console.WriteLine("{0} = {1}", s.Substring(i, len), hash);
+            }
+        }
+
+    }
+}
---
+++ problems/0028_ImplementStrStr/Solution.cs
@@ -0,0 +1,51 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public int StrStr(string haystack, string needle) {
+            if (haystack.Length < needle.Length) {
+                return -1;
+            } else if (needle.Length == 0) {
+                return 0;
+            }
+
+            int n_hash = Hash(needle, 0, needle.Length);
+            int h_hash = 0;
+
+            for (int i = 0; i <= haystack.Length - needle.Length; i++) {
+                if (i <= 0) {
+                    h_hash = Hash(haystack, 0, needle.Length);
+                } else {
+                    h_hash = Hash(h_hash, haystack[i - 1], haystack[i + needle.Length - 1]);
+                }
+                
+                if (n_hash == h_hash) {
+                    for (int j = 0; j < needle.Length; j++) {
+                        if (haystack[i + j] != needle[j]) {
+                            goto miss;
+                        }
+                    }
+                    return i;
+                miss: 
+                    ;
+                }
+            }
+            
+            return -1;
+        }
+        
+        public static int Hash(string input, int start, int length) {
+            int hash = 17;
+            
+            for (int i = 0; i < length; i++) {
+                hash ^= input[i + start];
+            }
+            
+            return hash;
+        }
+
+        public static int Hash(int hash, int prev_char, int next_char) {
+            return hash ^ prev_char ^ next_char;
+        }
+
+    }
+}
