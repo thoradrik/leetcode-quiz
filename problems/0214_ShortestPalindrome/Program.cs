0fd2bbb4cdb6a179e723df6538f5a6d97af98ce3
thoradrik <thoradrik@gmail.com>
2021-02-08 19:17:26 +0500

    Solved 214. Shortest Palindrome https://leetcode.com/problems/shortest-palindrome/

---
+++ problems/0214_ShortestPalindrome/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.ShortestPalindrome, "", "");
+            Test.Check(solution.ShortestPalindrome, "a", "a");
+            Test.Check(solution.ShortestPalindrome, "ab", "bab");
+            Test.Check(solution.ShortestPalindrome, "aa", "aa");
+            Test.Check(solution.ShortestPalindrome, "aba", "aba");
+            Test.Check(solution.ShortestPalindrome, "abba", "abba");
+            Test.Check(solution.ShortestPalindrome, "abbac", "cabbac");
+            Test.Check(solution.ShortestPalindrome, "aacecaaa", "aaacecaaa");
+            Test.Check(solution.ShortestPalindrome, "abcd", "dcbabcd");
+        }
+
+    }
+}
---
+++ problems/0214_ShortestPalindrome/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace Quiz {
+    public class Solution {
+
+        public string ShortestPalindrome(string s) {
+            int length = s.Length;
+            
+            for (; length > 0; length--) {
+                int start = 0;
+                int end = length - 1;
+
+                while (s[start] == s[end] && start < end) {
+                    start++;
+                    end--;
+                }
+
+                if (start >= end) {
+                    break;
+                }
+            }
+
+            if (length <= 0) {
+                return s;
+            }
+            
+            StringBuilder sb = new StringBuilder(length + s.Length);
+
+            for (int i = s.Length - 1; i >= length; i--) {
+                sb.Append(s[i]);
+            }
+
+            sb.Append(s);
+
+            return sb.ToString();
+        }
+        
+    }
+}
