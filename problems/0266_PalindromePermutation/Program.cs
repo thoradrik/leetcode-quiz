ffa4c5aff9248857ca69bc06b2b82d30d1d673bd
thoradrik <thoradrik@gmail.com>
2021-02-08 18:51:21 +0500

    Solved 266. Palindrome Permutation https://leetcode.com/problems/palindrome-permutation/submissions/

---
+++ problems/0266_PalindromePermutation/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.CanPermutePalindrome, "a", true);
+            Test.Check(solution.CanPermutePalindrome, "aa", true);
+            Test.Check(solution.CanPermutePalindrome, "ab", false);
+            Test.Check(solution.CanPermutePalindrome, "aaa", true);
+            Test.Check(solution.CanPermutePalindrome, "aab", true);
+            Test.Check(solution.CanPermutePalindrome, "aabc", false);
+            Test.Check(solution.CanPermutePalindrome, "aabcc", true);
+            Test.Check(solution.CanPermutePalindrome, "aabbcc", true);
+            Test.Check(solution.CanPermutePalindrome, "daabbcc", true);
+            Test.Check(solution.CanPermutePalindrome, "edaabbcc", false);
+        }
+
+    }
+}
---
+++ problems/0266_PalindromePermutation/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public bool CanPermutePalindrome(string s) {
+            int[] counts = new int[256];
+            
+            for (int i = 0; i < s.Length; i++) {
+                counts[s[i]]++;
+            }
+
+            int odds = 0;
+            
+            for (int i = 0; i < counts.Length; i++) {
+                if (counts[i] % 2 == 1) {
+                    odds++;
+                }
+            }
+
+            return odds < 2;
+        }
+        
+    }
+}
