63b372b0b9721332f8dc3b517f0f0ee553df4ea6
thoradrik <thoradrik@gmail.com>
2021-02-08 18:44:20 +0500

    Solved 5. Longest Palindromic Substring https://leetcode.com/problems/longest-palindromic-substring/

---
+++ problems/0005_LongestPalindromicSubstring/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.LongestPalindrome, "a", "a");
+            Test.Check(solution.LongestPalindrome, "ab", "a");
+            Test.Check(solution.LongestPalindrome, "abcc", "cc");
+            Test.Check(solution.LongestPalindrome, "abccbbb", "bbb");
+            Test.Check(solution.LongestPalindrome, "aa", "aa");
+            Test.Check(solution.LongestPalindrome, "aaa", "aaa");
+            Test.Check(solution.LongestPalindrome, "aaaa", "aaaa");
+            Test.Check(solution.LongestPalindrome, "aba", "aba");
+            Test.Check(solution.LongestPalindrome, "abac", "aba");
+            Test.Check(solution.LongestPalindrome, "cabac", "cabac");
+            Test.Check(solution.LongestPalindrome, "xyzcabacxyz", "cabac");
+            Test.Check(solution.LongestPalindrome, "xyzcabacxyzyx", "cabac");
+            Test.Check(solution.LongestPalindrome, "xyzcabacxyzzyx", "xyzzyx");
+            Test.Check(solution.LongestPalindrome, "xyzcabacxyzqzyx", "xyzqzyx");
+            Test.Check(solution.LongestPalindrome, "xyzcabbbacxyzqzyx", "cabbbac");
+            Test.Check(solution.LongestPalindrome, "zyxcabacxyz", "zyxcabacxyz");
+            Test.Check(solution.LongestPalindrome, "babad", "bab");
+            Test.Check(solution.LongestPalindrome, "cbbd", "bb");
+        }
+
+    }
+}
---
+++ problems/0005_LongestPalindromicSubstring/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public string LongestPalindrome(string s) {
+            string longest = null;
+            
+            void promote(int start, int end) {
+                while (s[start] == s[end] && start > 0 && end < s.Length - 1) {
+                    start--;
+                    end++;
+                }
+
+                if (s[start] != s[end]) {
+                    start++;
+                    end--;
+                }
+
+                int length = end - start + 1;
+                
+                if (longest == null || length > longest.Length) {
+                    longest = s.Substring(start, length);
+                }
+            }
+            
+            for (int i = 0; i < s.Length; i++) {
+                if (i > 0) {
+                    if (s[i - 1] == s[i]) {
+                        promote(i - 1, i);
+                    }
+                }
+                
+                promote(i, i);
+            }
+
+            return longest;
+        } 
+
+    }
+}
