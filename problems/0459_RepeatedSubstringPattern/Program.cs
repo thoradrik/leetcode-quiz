a3250da74060da38c72185238033210e7b5ed171
thoradrik <thoradrik@gmail.com>
2021-02-08 18:20:14 +0500

    Solved 459. Repeated Substring Pattern https://leetcode.com/problems/repeated-substring-pattern/

---
+++ problems/0459_RepeatedSubstringPattern/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            // Test.Check(solution.RepeatedSubstringPattern, "", false);
+            // Test.Check(solution.RepeatedSubstringPattern, "a", false);
+            // Test.Check(solution.RepeatedSubstringPattern, "aa", true);
+            // Test.Check(solution.RepeatedSubstringPattern, "aba", false);
+            // Test.Check(solution.RepeatedSubstringPattern, "abab", true);
+            // Test.Check(solution.RepeatedSubstringPattern, "ababa", false);
+            // Test.Check(solution.RepeatedSubstringPattern, "ababab", true);
+            // Test.Check(solution.RepeatedSubstringPattern, "ababac", false);
+            Test.Check(solution.RepeatedSubstringPattern, "ababacababac", true);
+            Test.Check(solution.RepeatedSubstringPattern, "ababacababad", false);
+            Test.Check(solution.RepeatedSubstringPattern, "ababacababadababacababad", true);
+            Test.Check(solution.RepeatedSubstringPattern, "ababacababadababacababae", false);
+            Test.Check(solution.RepeatedSubstringPattern, "ababacababadababacababaeababacababadababacababae", true);
+            Test.Check(solution.RepeatedSubstringPattern, "ababacababadababacababaeababacababadababacababaf", false);
+            Test.Check(solution.RepeatedSubstringPattern, "ababacababadababacababaeababacababadababacababafababacababadababacababaeababacababadababacababaf", true);
+            Test.Check(solution.RepeatedSubstringPattern, "ababacababadababacababaeababacababadababacababafababacababadababacababaeababacababadababacababag", false);
+            Test.Check(solution.RepeatedSubstringPattern, "ababacababadababacababaeababacababadababacababafababacababadababacababaeababacababadababacababagababacababadababacababaeababacababadababacababafababacababadababacababaeababacababadababacababag", true);
+            Test.Check(solution.RepeatedSubstringPattern, "ababacababadababacababaeababacababadababacababafababacababadababacababaeababacababadababacababagababacababadababacababaeababacababadababacababafababacababadababacababaeababacababadababacababagababacababadababacababaeababacababadababacababafababacababadababacababaeababacababadababacababagababacababadababacababaeababacababadababacababafababacababadababacababaeababacababadababacababag", true);
+            Test.Check(solution.RepeatedSubstringPattern, "ababacababadababacababaeababacababadababacababafababacababadababacababaeababacababadababacababagababacababadababacababaeababacababadababacababafababacababadababacababaeababacababadababacababagababacababadababacababaeababacababadababacababafababacababadababacababaeababacababadababacababagababacababadababacababaeababacababadababacababafababacababadababacababaeababacababadababacababagababacababadababacababaeababacababadababacababafababacababadababacababaeababacababadababacababagababacababadababacababaeababacababadababacababafababacababadababacababaeababacababadababacababag", true);
+            Test.Check(solution.RepeatedSubstringPattern, "abcabcabcabc", true);
+            Test.Check(solution.RepeatedSubstringPattern, "aabaaba", false);
+        }
+
+    }
+}
---
+++ problems/0459_RepeatedSubstringPattern/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Quiz {
+    public class Solution {
+        
+        public bool RepeatedSubstringPattern(string s) {
+            if (s.Length <= 1) {
+                return false;
+            }
+
+            for (int len = 1; len <= s.Length / 2; len++) {
+                if (s.Length % len == 0) {
+                    ReadOnlySpan<char> sample = s.AsSpan(0, len);
+                
+                    for (int i = len; i < s.Length; i += len) {
+                        ReadOnlySpan<char> test = s.AsSpan(i, len);
+
+                        if (!MemoryExtensions.Equals(sample, test, StringComparison.Ordinal)) {
+                            goto next_length;
+                        }
+                    }
+
+                    return true;
+                    
+                next_length:
+                
+                    ;
+                }
+            }
+            
+            return false;
+        }
+        
+    }
+}
