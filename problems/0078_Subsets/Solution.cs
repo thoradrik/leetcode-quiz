1a9d7c63c36a91f57e35c7f7f1034d7d4ec7d180
thoradrik <thoradrik@gmail.com>
2021-02-09 17:50:02 +0500

    Solved 78. Subsets https://leetcode.com/problems/subsets/

---
+++ problems/0078_Subsets/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using AlgorithmLib;
+
+namespace Quiz {
+    public class Solution {
+        
+        public IList<IList<int>> Subsets(int[] nums) {
+            IList<IList<int>> list = new List<IList<int>>();
+            
+            CombinationGenerator.Generate(new[] { false, true }, nums.Length, flags => {
+                List<int> items = new List<int>();
+                
+                for (int index = 0; index < flags.Length; index++) {
+                    if (flags[index]) {
+                        items.Add(nums[index]);
+                    }
+                }
+                
+                list.Add(items);
+            });    
+            
+            return list;
+        }
+        
+    }
+}
