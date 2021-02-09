70635f86c681c11c71630826d1e1d84c6c88ee8d
thoradrik <thoradrik@gmail.com>
2021-02-09 17:32:43 +0500

    Not Solved 516. Longest Palindromic Subsequence https://leetcode.com/problems/longest-palindromic-subsequence/

---
+++ problems/0516_LongestPalindromicSubsequence/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.LongestPalindromeSubseq, "a", 1);
+            Test.Check(solution.LongestPalindromeSubseq, "ab", 1);
+            Test.Check(solution.LongestPalindromeSubseq, "aa", 2);
+            Test.Check(solution.LongestPalindromeSubseq, "aab", 2);
+            Test.Check(solution.LongestPalindromeSubseq, "aabb", 2);
+            Test.Check(solution.LongestPalindromeSubseq, "aabbb", 3);
+            Test.Check(solution.LongestPalindromeSubseq, "aabbba", 5);
+            Test.Check(solution.LongestPalindromeSubseq, "aabbbaa", 7);
+            Test.Check(solution.LongestPalindromeSubseq, "paabbbaa", 7);
+            Test.Check(solution.LongestPalindromeSubseq, "paabbbaaq", 7);
+            Test.Check(solution.LongestPalindromeSubseq, "qaabbbaaq", 9);
+            Test.Check(solution.LongestPalindromeSubseq, "ppaabbbaa", 7);
+            Test.Check(solution.LongestPalindromeSubseq, "aabbbaaqpaabbbaa", 7);
+            Test.Check(solution.LongestPalindromeSubseq, "aabbbbaaqpaabbbaa", 8);
+        }
+
+    }
+}
---
+++ problems/0516_LongestPalindromicSubsequence/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Quiz {
+    public class Solution {
+        
+        public int LongestPalindromeSubseq(string s) {
+            int max = 0;
+            
+            void promote(int start, int end) {
+                while (s[start] == s[end] && start > 0 && end < s.Length - 1) {
+                    start--;
+                    end++;
+                }
+
+                if (s[start] != s[end]) {
+                    start++;
+                    end--;
+                }
+
+                max = Math.Max(max, end - start + 1);
+            }
+            
+            for (int i = 0; i < s.Length; i++) {
+                if (i > 0) {
+                    if (s[i - 1] == s[i]) {
+                        promote(i - 1, i);
+                    }
+                }
+                
+                promote(i, i);
+            }
+
+            return max;
+        } 
+
+    }
+}
