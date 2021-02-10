1937677c0fe623e8e8bafeb42e016d8e9d92caa4
thoradrik <thoradrik@gmail.com>
2021-02-10 19:41:14 +0500

    Solved 1087. Brace Expansion https://leetcode.com/problems/brace-expansion/

--- libs/AlgorithmLib/CombinationGenerator.cs
+++ libs/AlgorithmLib/CombinationGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AlgorithmLib {
     public static class CombinationGenerator {
@@ -14,6 +15,64 @@ namespace AlgorithmLib {
                     int index = stack[ptr];
                     if (index < array.Length) {
                         result[ptr] = array[index];
+                        ptr++;
+                    } else {
+                        stack[ptr] = 0;
+                        ptr--;
+                        if (ptr < 0) {
+                            break;
+                        } else {
+                            stack[ptr]++;
+                        }
+                    }
+                } else {
+                    output(result);
+                    ptr--;
+                    stack[ptr]++;
+                }
+            } while (true);
+        }
+
+        public static void Generate<T>(int length, T[][] array, Action<T[]> output) {
+            int[] stack = new int[length];
+            int ptr = 0;
+            
+            T[] result = new T[length];
+
+            do {
+                if (ptr < length) {
+                    int index = stack[ptr];
+                    if (index < array[ptr].Length) {
+                        result[ptr] = array[ptr][index];
+                        ptr++;
+                    } else {
+                        stack[ptr] = 0;
+                        ptr--;
+                        if (ptr < 0) {
+                            break;
+                        } else {
+                            stack[ptr]++;
+                        }
+                    }
+                } else {
+                    output(result);
+                    ptr--;
+                    stack[ptr]++;
+                }
+            } while (true);
+        }
+
+        public static void Generate<T>(int length, IList<IList<T>> array, Action<T[]> output) {
+            int[] stack = new int[length];
+            int ptr = 0;
+            
+            T[] result = new T[length];
+
+            do {
+                if (ptr < length) {
+                    int index = stack[ptr];
+                    if (index < array[ptr].Count) {
+                        result[ptr] = array[ptr][index];
                         ptr++;
                     } else {
                         stack[ptr] = 0;
---
+++ problems/1087_BraceExpansion/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Print(solution.Expand, "a");
+            Test.Print(solution.Expand, "ab");
+            Test.Print(solution.Expand, "abc");
+            Test.Print(solution.Expand, "a{b,c}");
+            Test.Print(solution.Expand, "a{b,c}d");
+            Test.Print(solution.Expand, "a{b,c}{d,e}");
+            Test.Print(solution.Expand, "a{b,c}{d,e}f");
+            Test.Print(solution.Expand, "a{b,c}{d,e}{f}");
+            Test.Print(solution.Expand, "a{b,c}{d,e}{f,g}");
+            Test.Print(solution.Expand, "a{b,c}{d,e}{f,g,h}");
+            Test.Print(solution.Expand, "{a,b}{z,x,y}");
+        }
+
+    }
+}
