4aeadb97d12983cfea77c7952f7a34e2f4a3e98e
thoradrik <thoradrik@gmail.com>
2021-02-13 13:01:10 +0500

    Solved 720. Longest Word in Dictionary https://leetcode.com/problems/longest-word-in-dictionary/

---
+++ problems/0720_LongestWordInDictionary/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.LongestWord, StringArray("[\"a\",\"banana\",\"app\",\"appl\",\"ap\",\"apply\",\"apple\"]"), "apple");
+            Test.Check(solution.LongestWord, StringArray("[\"w\",\"wo\",\"wor\",\"worl\",\"world\"]"), "world");
+        }
+
+    }
+}
