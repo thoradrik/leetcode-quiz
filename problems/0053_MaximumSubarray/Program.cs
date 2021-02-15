492e36da22ed3890ccea4462191ac9f5a1040828
thoradrik <thoradrik@gmail.com>
2021-02-15 17:42:34 +0500

    Solved 53. Maximum Subarray https://leetcode.com/problems/maximum-subarray/

---
+++ problems/0053_MaximumSubarray/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.MaxSubArray, IntArray("[0]"), 0);
+            Test.Check(solution.MaxSubArray, IntArray("[1]"), 1);
+            Test.Check(solution.MaxSubArray, IntArray("[-1,-2]"), -1);
+            Test.Check(solution.MaxSubArray, IntArray("[1,-1]"), 1);
+            Test.Check(solution.MaxSubArray, IntArray("[2,-1,1]"), 2);
+            Test.Check(solution.MaxSubArray, IntArray("[3,-1,1]"), 3);
+            Test.Check(solution.MaxSubArray, IntArray("[3,-1,1,3]"), 6);
+            Test.Check(solution.MaxSubArray, IntArray("[3,-1,1,3,-2,5]"), 9);
+            Test.Check(solution.MaxSubArray, IntArray("[3,-1,1,3,-2,5,-9,8]"), 9);
+            Test.Check(solution.MaxSubArray, IntArray("[-2,1,-3,4,-1,2,1,-5,4]"), 6);
+        }
+
+    }
+}
---
+++ problems/0053_MaximumSubarray/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Quiz {
+    public class Solution {
+
+        public int MaxSubArray(int[] nums) {
+            int[] dp = new int[nums.Length];
+
+            int max = nums[0];
+            
+            dp[0] = nums[0];
+            
+            for (int i = 1; i < nums.Length; i++) {
+                dp[i] = dp[i - 1] > 0 ? nums[i] + dp[i - 1] : nums[i];
+
+                max = Math.Max(max, dp[i]);
+            }
+
+            return max;
+        }
+        
+    }
+}
