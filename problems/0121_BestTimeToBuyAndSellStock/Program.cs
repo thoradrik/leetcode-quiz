86b38cf1756ef409c14fc0a746b3c64ac8b47c8e
thoradrik <thoradrik@gmail.com>
2021-02-15 17:23:13 +0500

    Solved 121. Best Time to Buy and Sell Stock https://leetcode.com/problems/best-time-to-buy-and-sell-stock/

---
+++ problems/0121_BestTimeToBuyAndSellStock/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.MaxProfit, IntArray("[1]"), 0);
+            Test.Check(solution.MaxProfit, IntArray("[1,2]"), 1);
+            Test.Check(solution.MaxProfit, IntArray("[1,2,3]"), 2);
+            Test.Check(solution.MaxProfit, IntArray("[1,3,2]"), 2);
+            Test.Check(solution.MaxProfit, IntArray("[2,4,2,1]"), 2);
+            Test.Check(solution.MaxProfit, IntArray("[2,4,2,1,4]"), 3);
+            Test.Check(solution.MaxProfit, IntArray("[7,1,5,3,6,4]"), 5);
+            Test.Check(solution.MaxProfit, IntArray("[7,6,4,3,1]"), 0);
+        }
+
+    }
+}
