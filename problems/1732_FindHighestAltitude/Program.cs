975c71e5a86b48f6bd6a5b71f1642ebdbfd57b6f
thoradrik <thoradrik@gmail.com>
2021-02-16 13:41:24 +0500

    Solved 1732. Find the Highest Altitude https://leetcode.com/problems/find-the-highest-altitude/

---
+++ problems/1732_FindHighestAltitude/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.LargestAltitude, IntArray("[-5,1,5,0,-7]"), 1);
+            Test.Check(solution.LargestAltitude, IntArray("[-4,-3,-2,-1,4,3,2]"), 0);
+        }
+
+    }
+}
---
+++ problems/1732_FindHighestAltitude/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Quiz {
+    public class Solution {
+        
+        public int LargestAltitude(int[] gain) {
+            int max = Math.Max(0, gain[0]);
+
+            for (int i = 1; i < gain.Length; i++) {
+                gain[i] += gain[i - 1];
+            
+                max = Math.Max(max, gain[i]);
+            }
+
+            return max;
+        }
+        
+    }
+}
