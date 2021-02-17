4c2230de93f025049b172fc994a123868ed659ee
thoradrik <thoradrik@gmail.com>
2021-02-17 15:12:26 +0500

    Solved 199. Binary Tree Right Side View https://leetcode.com/problems/binary-tree-right-side-view/

---
+++ problems/0199_BinaryTreeRightSideView/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            TreeNode root;
+            
+            root = null;
+            Test.Check(solution.RightSideView, root, IntArray("[]"));
+
+            root = new TreeNode(1);
+            Test.Check(solution.RightSideView, root, IntArray("[1]"));
+
+            root = new TreeNode(
+                1,
+                new TreeNode(2),
+                null
+            );
+            Test.Check(solution.RightSideView, root, IntArray("[1,2]"));
+
+            root = new TreeNode(
+                1,
+                new TreeNode(2),
+                new TreeNode(3)
+            );
+            Test.Check(solution.RightSideView, root, IntArray("[1,3]"));
+
+            root = new TreeNode(
+                1,
+                new TreeNode(
+                    2,
+                    new TreeNode(4)
+                ),
+                new TreeNode(3)
+            );
+            Test.Check(solution.RightSideView, root, IntArray("[1,3,4]"));
+
+            root = new TreeNode(
+                1,
+                new TreeNode(
+                    2,
+                    new TreeNode(4),
+                    new TreeNode(5)
+                ),
+                new TreeNode(
+                    3,
+                    new TreeNode(6),
+                    null
+                )
+            );
+            Test.Check(solution.RightSideView, root, IntArray("[1,3,6]"));
+
+            root = new TreeNode(
+                1,
+                new TreeNode(
+                    2,
+                    new TreeNode(4),
+                    new TreeNode(5)
+                ),
+                new TreeNode(
+                    3,
+                    new TreeNode(6),
+                    new TreeNode(7)
+                )
+            );
+            Test.Check(solution.RightSideView, root, IntArray("[1,3,7]"));
+        }
+
+    }
+}
---
+++ problems/0199_BinaryTreeRightSideView/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+        
+        public IList<int> RightSideView(TreeNode root) {
+            List<int> list = new List<int>();
+            
+            void backtrace(TreeNode node, int level) {
+                if (list.Count <= level) {
+                    list.Add(node.val);
+                }
+
+                if (node.right != null) {
+                    backtrace(node.right, level + 1);
+                }
+                if (node.left != null) {
+                    backtrace(node.left, level + 1);
+                }
+            }
+
+            if (root != null) {
+                backtrace(root, 0);
+            }
+
+            return list;
+        }
+        
+    }
+}
