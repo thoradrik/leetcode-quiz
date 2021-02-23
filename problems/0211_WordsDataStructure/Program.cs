12c390565170013da44ad3ae0ac5b52536b8dec2
thoradrik <thoradrik@gmail.com>
2021-02-23 19:05:18 +0500

    Solved 211. Design Add and Search Words Data Structure https://leetcode.com/problems/design-add-and-search-words-data-structure/

---
+++ problems/0211_WordsDataStructure/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            WordDictionary solution = new WordDictionary();
+
+            solution.AddWord("apple");
+            solution.AddWord("apply");
+            solution.AddWord("door");
+            solution.AddWord("poor");
+            solution.AddWord("pool");
+            solution.AddWord("bad");
+            solution.AddWord("dad");
+            solution.AddWord("mad");
+            solution.AddWord("sad");
+            solution.AddWord("pad");
+            solution.AddWord("bar");
+            solution.AddWord("man");
+            solution.AddWord("pan");
+            solution.AddWord("fan");
+            solution.AddWord("dan");
+            
+            // Test.Check(solution.Search, "apple", true);
+            // Test.Check(solution.Search, "apple.", false);
+            // Test.Check(solution.Search, ".pple.", false);
+            // Test.Check(solution.Search, ".p.le.", false);
+            // Test.Check(solution.Search, ".p.le", true);
+            // Test.Check(solution.Search, "apples", false);
+            // Test.Check(solution.Search, "pad", true);
+            // Test.Check(solution.Search, "bad", true);
+            // Test.Check(solution.Search, "ban", false);
+            // Test.Check(solution.Search, ".ad", true);
+            // Test.Check(solution.Search, "..d", true);
+            // Test.Check(solution.Search, ".a.", true);
+            // Test.Check(solution.Search, ".e.", false);
+            // Test.Check(solution.Search, "m..", true);
+            // Test.Check(solution.Search, "n..", false);
+            // Test.Check(solution.Search, "..n", true);
+            Test.Check(solution.Search, "...", true);
+        }
+
+    }
+}
---
+++ problems/0211_WordsDataStructure/WordDictionary.cs
@@ -0,0 +1,78 @@
+﻿namespace Quiz {
+    public class WordDictionary {
+
+        private class Node {
+
+            public bool IsWord;
+            
+            public readonly Node[] Nodes = new Node[26];
+
+        }
+
+        private Node m_Root;
+    
+        public void AddWord(string word) {
+            ref Node current = ref m_Root;
+
+            for (int ptr = 0; ptr < word.Length; ptr++) {
+                if (current == null) {
+                    current = new Node();
+                }
+
+                current = ref current.Nodes[word[ptr] - 'a'];
+            }
+            
+            if (current == null) {
+                current = new Node();
+            }
+
+            current.IsWord = true;
+        }
+    
+        public bool Search(string word) {
+            Node search(Node node, int ptr) {
+                if (node == null) {
+                    return null;
+                }
+
+                if (ptr >= word.Length) {
+                    return node;
+                }
+
+                if (node.Nodes == null) {
+                    return null;
+                }
+
+                while (node != null && ptr < word.Length) {
+                    if (node.Nodes == null) {
+                        return null;
+                    }
+                    
+                    int c = word[ptr];
+                    if (c == '.') {
+                        for (int index = 0; index < node.Nodes.Length; index++) {
+                            Node found = search(node.Nodes[index], ptr + 1);
+                            if (found != null) {
+                                if (found.IsWord) {
+                                    return found;
+                                }
+                            }
+                        }
+
+                        return null;
+                    } else {
+                        node = node.Nodes[c - 'a'];
+                        ptr++;
+                    }
+                } 
+
+                return node;
+            }
+
+            Node result = search(m_Root, 0);
+            
+            return result != null && result.IsWord;
+        }
+        
+    }
+}
