63ece05403e612ba3ee5069920e821510fa8eb04
thoradrik <thoradrik@gmail.com>
2021-02-24 19:27:04 +0500

    Solved 676. Implement Magic Dictionary https://leetcode.com/problems/implement-magic-dictionary/

---
+++ problems/0676_ImplementMagicDictionary/MagicDictionary.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+
+namespace Quiz {
+    public class MagicDictionary {
+
+        private class Node : Dictionary<char, Node> {
+
+            public bool IsWord;
+
+        }
+
+        private readonly Node m_Root = new Node();
+        
+        public void Insert(string s) {
+            Node current = m_Root;
+
+            foreach (char c in s) {
+                if (!current.TryGetValue(c, out Node node)) {
+                    node = new Node();
+                    current.Add(c, node);
+                }
+
+                current = node;
+            }
+
+            current.IsWord = true;
+        }
+    
+        public void BuildDict(string[] dictionary) {
+            foreach (string s in dictionary) {
+                Insert(s);
+            }
+        }
+        
+        public bool Search(string searchWord) {
+            Node search(Node node, int ptr, int replaces) {
+                if (ptr < searchWord.Length) {
+                    if (replaces > 0) {
+                        foreach ((char key, Node child) in node) {
+                            Node result = key == searchWord[ptr] ? search(child, ptr + 1, replaces) : search(child, ptr + 1, replaces - 1);
+
+                            if (result != null) {
+                                if (result.IsWord) {
+                                    return result;
+                                }
+                            }
+                        }
+                    }
+
+                    for (int index = ptr; index < searchWord.Length; index++) {
+                        char c = searchWord[index];
+
+                        if (!node.TryGetValue(c, out node)) {
+                            return null;
+                        }
+                    }
+                }
+
+                if (replaces > 0) {
+                    return null;
+                }
+
+                return node.IsWord ? node : null;
+            }
+
+            return search(m_Root, 0, 1) != null;
+        }
+        
+    }
+}
---
+++ problems/0676_ImplementMagicDictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            MagicDictionary solution = new MagicDictionary();
+
+            solution.BuildDict(new[] { "h", "hh", "hal", "hello", "welly", "leetcode" });
+
+            Test.Check(solution.Search, "hell", false);
+            Test.Check(solution.Search, "hello", false);
+            Test.Check(solution.Search, "hellow", false);
+            
+            Test.Check(solution.Search, "gello", true);
+            Test.Check(solution.Search, "hhllo", true);
+            Test.Check(solution.Search, "heplo", true);
+            Test.Check(solution.Search, "helpo", true);
+            Test.Check(solution.Search, "hellz", true);
+            
+            Test.Check(solution.Search, "hhllz", false);
+            
+            Test.Check(solution.Search, "leetcode", false);
+            Test.Check(solution.Search, "leetcodz", true);
+            Test.Check(solution.Search, "leetcade", true);
+            Test.Check(solution.Search, "le3tcode", true);
+            Test.Check(solution.Search, "lootcode", false);
+        }
+
+    }
+}
