75b41674d33b62c4a05a53ba6c040b01e898820e
thoradrik <thoradrik@gmail.com>
2021-02-26 19:44:21 +0500

    Solved 1768. Merge Strings Alternately https://leetcode.com/problems/merge-strings-alternately/

---
+++ problems/1768_MergeStringsAlternately/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.MergeAlternately, "a", "b", "ab");
+            Test.Check(solution.MergeAlternately, "aa", "b", "aba");
+            Test.Check(solution.MergeAlternately, "a", "bb", "abb");
+            Test.Check(solution.MergeAlternately, "aa", "bb", "abab");
+            Test.Check(solution.MergeAlternately, "aaa", "bb", "ababa");
+            Test.Check(solution.MergeAlternately, "aa", "bbb", "ababb");
+            Test.Check(solution.MergeAlternately, "aa", "bbbbb", "ababbbb");
+            Test.Check(solution.MergeAlternately, "abc", "pqr", "apbqcr");
+            Test.Check(solution.MergeAlternately, "ab", "pqrs", "apbqrs");
+            Test.Check(solution.MergeAlternately, "abcd", "pq", "apbqcd");
+        }
+
+    }
+}
---
+++ problems/1768_MergeStringsAlternately/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace Quiz {
+    public class Solution {
+        
+        public string MergeAlternately(string word1, string word2) {
+            StringBuilder sb = new StringBuilder(word1.Length + word2.Length);
+
+            int min_length = Math.Min(word1.Length, word2.Length);
+            
+            for (int i = 0; i < min_length; i++) {
+                sb.Append(word1[i]);
+                sb.Append(word2[i]);
+            }
+
+            if (word1.Length > min_length) {
+                sb.Append(word1.Substring(min_length));
+            }
+            if (word2.Length > min_length) {
+                sb.Append(word2.Substring(min_length));
+            }
+
+            return sb.ToString();
+        }
+        
+    }
+}
