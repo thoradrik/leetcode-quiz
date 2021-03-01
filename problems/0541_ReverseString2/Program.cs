5757d74a833edc047e9c50498cbad21992056cad
thoradrik <thoradrik@gmail.com>
2021-03-01 21:32:21 +0500

    Solved 541. Reverse String II https://leetcode.com/problems/reverse-string-ii/

---
+++ problems/0541_ReverseString2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.ReverseStr, "", 2, "");
+            Test.Check(solution.ReverseStr, "a", 2, "a");
+            Test.Check(solution.ReverseStr, "ab", 2, "ba");
+            Test.Check(solution.ReverseStr, "abc", 2, "bac");
+            Test.Check(solution.ReverseStr, "abcd", 2, "bacd");
+            Test.Check(solution.ReverseStr, "abcde", 2, "bacde");
+            Test.Check(solution.ReverseStr, "abcdef", 2, "bacdfe");
+            Test.Check(solution.ReverseStr, "abcdefg", 2, "bacdfeg");
+            Test.Check(solution.ReverseStr, "abcdefg", 3, "cbadefg");
+            Test.Check(solution.ReverseStr, "abcdefgh", 3, "cbadefhg");
+            Test.Check(solution.ReverseStr, "abcdefghi", 3, "cbadefihg");
+            Test.Check(solution.ReverseStr, "abcdefghij", 3, "cbadefihgj");
+        }
+
+    }
+}
---
+++ problems/0541_ReverseString2/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Quiz {
+    public class Solution {
+        
+        public string ReverseStr(string s, int k) {
+            char[] chars = s.ToCharArray();
+
+            for (int i = 0; i < s.Length; i += k * 2) {
+                for (int start = i, end = Math.Min(chars.Length, i + k) - 1; start < end; start++, end--) {
+                    ref char x = ref chars[start];
+                    ref char y = ref chars[end];
+                    x ^= y;
+                    y ^= x;
+                    x ^= y;
+                }
+            }
+            
+            return new string(chars);
+        }
+        
+    }
+}
