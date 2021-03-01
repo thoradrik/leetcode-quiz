d45eaf51e519be4afcd88612715ac2dd8fb5b298
thoradrik <thoradrik@gmail.com>
2021-03-01 18:59:36 +0500

    Solved 1119. Remove Vowels from a String https://leetcode.com/problems/remove-vowels-from-a-string/

---
+++ problems/1119_RemoveVowelsFromString/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.RemoveVowels, "", "");
+            Test.Check(solution.RemoveVowels, "a", "");
+            Test.Check(solution.RemoveVowels, "A", "");
+            Test.Check(solution.RemoveVowels, "Ab", "b");
+            Test.Check(solution.RemoveVowels, "Yb", "Yb");
+            Test.Check(solution.RemoveVowels, "aeiou", "");
+            Test.Check(solution.RemoveVowels, "leetcodeisacommunityforcoders", "ltcdscmmntyfrcdrs");
+        }
+
+    }
+}
---
+++ problems/1119_RemoveVowelsFromString/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace Quiz {
+    public class Solution {
+        
+        public string RemoveVowels(string s) {
+            bool is_vowel(char c) => c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' ||  c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U';
+            
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in s) {
+                if (!is_vowel(c)) {
+                    sb.Append(c);
+                }
+            }
+
+            return sb.ToString();
+        }
+        
+    }
+}
