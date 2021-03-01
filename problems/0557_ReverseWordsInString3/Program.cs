52830b9b3be273e95d0cc4099941cf97c2af0b08
thoradrik <thoradrik@gmail.com>
2021-03-01 21:41:16 +0500

    Solved 557. Reverse Words in a String III https://leetcode.com/problems/reverse-words-in-a-string-iii/

---
+++ problems/0557_ReverseWordsInString3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.ReverseWords, "", "");
+            Test.Check(solution.ReverseWords, "a", "a");
+            Test.Check(solution.ReverseWords, "ab", "ba");
+            Test.Check(solution.ReverseWords, "abc", "cba");
+            Test.Check(solution.ReverseWords, "abc e", "cba e");
+            Test.Check(solution.ReverseWords, "abc ef", "cba fe");
+            Test.Check(solution.ReverseWords, "abc efg", "cba gfe");
+            Test.Check(solution.ReverseWords, "abc efgh", "cba hgfe");
+            Test.Check(solution.ReverseWords, "abc efgh i", "cba hgfe i");
+            Test.Check(solution.ReverseWords, "abc efgh ij", "cba hgfe ji");
+            Test.Check(solution.ReverseWords, "Let's take LeetCode contest", "s'teL ekat edoCteeL tsetnoc");
+        }
+
+    }
+}
---
+++ problems/0557_ReverseWordsInString3/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public string ReverseWords(string s) {
+            char[] chars = s.ToCharArray();
+
+            void swap(int start, int end) {
+                for (; start < end; start++, end--) {
+                    ref char x = ref chars[start];
+                    ref char y = ref chars[end];
+                    x ^= y;
+                    y ^= x;
+                    x ^= y;
+                }
+            }
+
+            int w_start = 0;
+            
+            for (int i = 1; i < s.Length; i++) {
+                if (chars[i] == ' ') {
+                    int w_end = i - 1;
+                    
+                    swap(w_start, w_end);
+
+                    w_start = i + 1;
+                }
+            }
+            
+            swap(w_start, s.Length - 1);
+
+            return new string(chars);
+        }
+        
+    }
+}
