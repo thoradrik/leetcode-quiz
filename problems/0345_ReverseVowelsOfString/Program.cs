276b3bd9646e7a420bea04b732d47d866aadf7a2
thoradrik <thoradrik@gmail.com>
2021-03-01 18:54:49 +0500

    Solved 345. Reverse Vowels of a String https://leetcode.com/problems/reverse-vowels-of-a-string/

---
+++ problems/0345_ReverseVowelsOfString/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.ReverseVowels, "", "");
+            Test.Check(solution.ReverseVowels, "a", "a");
+            Test.Check(solution.ReverseVowels, "b", "b");
+            Test.Check(solution.ReverseVowels, "ab", "ab");
+            Test.Check(solution.ReverseVowels, "abe", "eba");
+            Test.Check(solution.ReverseVowels, "babe", "beba");
+            Test.Check(solution.ReverseVowels, "ababe", "ebaba");
+            Test.Check(solution.ReverseVowels, "abobe", "eboba");
+            Test.Check(solution.ReverseVowels, "abobez", "ebobaz");
+            Test.Check(solution.ReverseVowels, "abobezi", "ibeboza");
+            
+            Test.Check(solution.ReverseVowels, "aA", "Aa");
+            Test.Check(solution.ReverseVowels, "Yo! Bottoms up, U.S. Motto, boy!", "Yo! Bottoms Up, u.S. Motto, boy!");
+        }
+
+    }
+}
---
+++ problems/0345_ReverseVowelsOfString/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public string ReverseVowels(string s) {
+            char[] chars = s.ToCharArray();
+
+            bool is_vowel(char c) => c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' ||  c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U';
+            
+            int start = 0;
+            int end = chars.Length - 1;
+
+            do {
+                while (start < chars.Length && !is_vowel(chars[start])) {
+                    start++;
+                }
+
+                while (end > 0 && !is_vowel(chars[end])) {
+                    end--;
+                }
+
+                if (start < end) {
+                    ref char x = ref chars[start];
+                    ref char y = ref chars[end];
+                
+                    x ^= y;
+                    y ^= x;
+                    x ^= y;
+
+                    start++;
+                    end--;
+                } else {
+                    break;
+                }
+            } while (start < end);
+
+            return new string(chars);
+        }
+        
+    }
+}
