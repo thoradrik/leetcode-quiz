483d6c5158d1120d1025d30ab9c428fcbeaa55ae
thoradrik <thoradrik@gmail.com>
2021-03-02 09:27:27 +0500

    Solved 929. Unique Email Addresses https://leetcode.com/problems/unique-email-addresses/

---
+++ problems/0929_UniqueEmailAddresses/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.NumUniqueEmails, StringArray("[\"test.email+alex@leetcode.com\",\"test.e.mail+bob.cathy@leetcode.com\",\"testemail+david@lee.tcode.com\"]"), 2);
+        }
+
+    }
+}
---
+++ problems/0929_UniqueEmailAddresses/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+        
+        public int NumUniqueEmails(string[] emails) {
+            HashSet<string> set = new HashSet<string>();
+
+            foreach (string email in emails) {
+                int a_index = email.IndexOf('@', StringComparison.Ordinal);
+                
+                string name = email.Substring(0, a_index);
+
+                name = name.Replace(".", String.Empty);
+
+                int p_index = name.IndexOf('+', StringComparison.Ordinal);
+                if (p_index >= 0) {
+                    name = name.Substring(0, p_index);
+                }
+
+                string domain = email.Substring(a_index);
+
+                set.Add(name + domain);
+            }
+
+            return set.Count;
+        }
+        
+    }
+}
