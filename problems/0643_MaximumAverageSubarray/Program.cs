d9b0effda86ba6817effc6f73dc67580b4cbc0e1
thoradrik <thoradrik@gmail.com>
2021-03-02 09:48:16 +0500

    Solved 643. Maximum Average Subarray I https://leetcode.com/problems/maximum-average-subarray-i/

---
+++ problems/0643_MaximumAverageSubarray/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.FindMaxAverage, IntArray("[1,2]"), 1, 2);
+            Test.Check(solution.FindMaxAverage, IntArray("[1,2,2]"), 2, 2);
+            Test.Check(solution.FindMaxAverage, IntArray("[1,2,2,4]"), 2, 3);
+            Test.Check(solution.FindMaxAverage, IntArray("[1,12,-5,-6,50,3]"), 4, 12.75);
+        }
+
+    }
+}
---
+++ problems/0643_MaximumAverageSubarray/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Quiz {
+    public class Solution {
+        
+        public double FindMaxAverage(int[] nums, int k) {
+            double candidate = 0.0d;
+            
+            for (int i = 0; i < k; i++) {
+                candidate += nums[i];
+            }
+
+            double max = candidate;
+
+            for (int i = k; i < nums.Length; i++) {
+                candidate += nums[i] - nums[i - k];
+                
+                max = Math.Max(max, candidate);
+            }
+
+            return max / k;
+        }
+        
+    }
+}
