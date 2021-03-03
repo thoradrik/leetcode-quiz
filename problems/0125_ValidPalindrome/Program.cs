59b58cb0c5ff5d82cd5b03d3029f7d141849a336
thoradrik <thoradrik@gmail.com>
2021-03-03 18:40:15 +0500

    Solved 125. Valid Palindrome https://leetcode.com/problems/valid-palindrome/

---
+++ problems/0125_ValidPalindrome/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.IsPalindrome, "", true);
+            Test.Check(solution.IsPalindrome, ".,", true);
+            Test.Check(solution.IsPalindrome, "0p", false);
+            Test.Check(solution.IsPalindrome, "a", true);
+            Test.Check(solution.IsPalindrome, "aA", true);
+            Test.Check(solution.IsPalindrome, "aAb", false);
+            Test.Check(solution.IsPalindrome, "aAbaA", true);
+            Test.Check(solution.IsPalindrome, "aA baA", true);
+            Test.Check(solution.IsPalindrome, "aA ba A", true);
+            Test.Check(solution.IsPalindrome, "aA ba A ;", true);
+            Test.Check(solution.IsPalindrome, "aA ba A ; c", false);
+            Test.Check(solution.IsPalindrome, "A man, a plan, a canal: Panama", true);
+        }
+
+    }
+}
---
+++ problems/0125_ValidPalindrome/Solution.cs
@@ -0,0 +1,37 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public bool IsPalindrome(string s) {
+            int start = 0;
+            int end = s.Length - 1;
+
+            s = s.ToLower();
+
+            bool is_letter_or_digit(char c) => (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+
+            while (start < end) {
+                char c_start = s[start];
+                if (!is_letter_or_digit(c_start)) {
+                    start++;
+                    continue;
+                }
+
+                char c_end = s[end];
+                if (!is_letter_or_digit(c_end)) {
+                    end--;
+                    continue;
+                }
+                
+                if (c_start != c_end) {
+                    return false;
+                }
+
+                start++;
+                end--;
+            }
+
+            return true;
+        }
+
+    }
+}
