af0bc45ced1b81f988decda60cb7681d1adbf0be
thoradrik <thoradrik@gmail.com>
2021-03-03 18:21:05 +0500

    Solved 680. Valid Palindrome II https://leetcode.com/problems/valid-palindrome-ii/

---
+++ problems/0680_ValidPalindrome2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.ValidPalindrome, "", true);
+            Test.Check(solution.ValidPalindrome, "a", true);
+            Test.Check(solution.ValidPalindrome, "ab", true);
+            Test.Check(solution.ValidPalindrome, "aba", true);
+            Test.Check(solution.ValidPalindrome, "abba", true);
+            Test.Check(solution.ValidPalindrome, "abdba", true);
+            Test.Check(solution.ValidPalindrome, "abdgba", true);
+            Test.Check(solution.ValidPalindrome, "abgdba", true);
+            Test.Check(solution.ValidPalindrome, "abdggba", true);
+            Test.Check(solution.ValidPalindrome, "abdgeba", false);
+            Test.Check(solution.ValidPalindrome, "abac", true);
+            Test.Check(solution.ValidPalindrome, "abacd", false);
+            Test.Check(solution.ValidPalindrome, "dabacd", true);
+            Test.Check(solution.ValidPalindrome, "cdabacd", false);
+            Test.Check(solution.ValidPalindrome, "cdabacd", false);
+            Test.Check(solution.ValidPalindrome, "cdabacdc", true);
+            Test.Check(solution.ValidPalindrome, "cdcabacdc", true);
+            Test.Check(solution.ValidPalindrome, "cdcxabacdc", true);
+            Test.Check(solution.ValidPalindrome, "cdcxabaxcdc", true);
+            Test.Check(solution.ValidPalindrome, "cdcxabzaxcdc", true);
+            Test.Check(solution.ValidPalindrome, "cdcxaybzaxcdc", false);
+        }
+
+    }
+}
---
+++ problems/0680_ValidPalindrome2/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public bool ValidPalindrome(string s) {
+            if (s.Length <= 2) {
+                return true;
+            }
+
+            bool try_match_palindrome(int f_start, int f_end, out int r_start, out int r_end) {
+                for (r_start = f_start, r_end = f_end; r_start < r_end; r_start++, r_end--) {
+                    if (s[r_start] != s[r_end]) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            if (try_match_palindrome(0, s.Length - 1, out int start, out int end)) {
+                return true;
+            } else {
+                return try_match_palindrome(start + 1, end, out int _, out int _) || try_match_palindrome(start, end - 1, out int _, out int _);
+            }
+        }
+        
+    }
+} 
