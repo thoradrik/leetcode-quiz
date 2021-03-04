ebc699a2be0cd1f772a65b1f2b5d751fdf112704
thoradrik <thoradrik@gmail.com>
2021-03-04 20:04:48 +0500

    Solved 1078. Occurrences After Bigram https://leetcode.com/problems/occurrences-after-bigram/

---
+++ problems/1078_OccurrencesAfterBigram/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.FindOcurrences, "alice is a good girl she is a good student", "a", "good", StringArray("[\"girl\",\"student\"]"));
+            Test.Check(solution.FindOcurrences, "we will we will rock you", "we", "will", StringArray("[\"we\",\"rock\"]"));
+            Test.Check(solution.FindOcurrences, "we will we will rock ", "we", "will", StringArray("[\"we\",\"rock\"]"));
+        }
+
+    }
+}
---
+++ problems/1078_OccurrencesAfterBigram/Solution.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+        
+        public string[] FindOcurrences(string text, string first, string second) {
+            
+            bool compare(int from_index, int length, string fragment) {
+                if (length != fragment.Length) {
+                    return false;
+                }
+
+                for (int i = 0, j = from_index; i < fragment.Length; i++, j++) {
+                    if (fragment[i] != text[j]) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            List<string> list = new List<string>();
+
+            int start_index = 0;
+            do {
+                int first_space_index = text.IndexOf(' ', start_index);
+                if (first_space_index < 0) {
+                    break;
+                }
+
+                if (!compare(start_index, first_space_index - start_index, first)) {
+                    start_index = first_space_index + 1;
+                    continue;
+                }
+
+                int second_space_index = text.IndexOf(' ', first_space_index + 1);
+                if (second_space_index < 0) {
+                    break;
+                }
+
+                if (!compare(first_space_index + 1, second_space_index - first_space_index - 1, second)) {
+                    start_index = first_space_index + 1;
+                    continue;
+                }
+                
+                int end_index = text.IndexOf(' ', second_space_index + 1);
+                if (end_index < 0) {
+                    end_index = text.Length;
+                }
+
+                list.Add(text.Substring(second_space_index + 1, end_index - second_space_index - 1));
+                
+                start_index = first_space_index + 1;
+            } while (true);
+
+            return list.ToArray();
+        }
+        
+    }
+}
