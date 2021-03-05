952e73f144cfb39efe35eac9891db9991bd98821
thoradrik <thoradrik@gmail.com>
2021-03-05 19:05:02 +0500

    Solved 800. Similar RGB Color https://leetcode.com/problems/similar-rgb-color/

---
+++ problems/0800_SimilarRGBColor/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.SimilarRGB, "#000000", "#000000");
+            Test.Check(solution.SimilarRGB, "#080808", "#000000");
+            Test.Check(solution.SimilarRGB, "#090909", "#111111");
+            Test.Check(solution.SimilarRGB, "#0a0a0a", "#111111");
+            Test.Check(solution.SimilarRGB, "#111111", "#111111");
+            Test.Check(solution.SimilarRGB, "#121212", "#111111");
+            Test.Check(solution.SimilarRGB, "#181818", "#111111");
+            Test.Check(solution.SimilarRGB, "#1a1a1a", "#222222");
+            Test.Check(solution.SimilarRGB, "#09f166", "#11ee66");
+            Test.Check(solution.SimilarRGB, "#f0f0f0", "#eeeeee");
+            Test.Check(solution.SimilarRGB, "#ffffff", "#ffffff");
+        }
+
+    }
+}
---
+++ problems/0800_SimilarRGBColor/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+        
+        public string SimilarRGB(string color) {
+            
+            int parse_hex(string s, int index) {
+                char c = s[index];
+                if (c >= '0' && c <= '9') {
+                    return c - '0';
+                } else {
+                    return c - 'a' + 10;
+                }
+            }
+            
+            int parse_hex2(string s, int index) {
+                return (parse_hex(s, index) << 4) + parse_hex(s, index + 1);
+            }
+
+            int r = parse_hex2(color, 1);
+            int g = parse_hex2(color, 3);
+            int b = parse_hex2(color, 5);
+            
+            IEnumerable<int> values(int c) {
+                int h = c >> 4;
+                int l = c & 0xF;
+                if (h == l) {
+                    yield return c;
+                } else if (h < l) {
+                    yield return (h << 4) + h;
+                    yield return ((h + 1) << 4) + (h + 1);
+                } else {
+                    yield return ((h - 1) << 4) + (h - 1);
+                    yield return (h << 4) + h;
+                }
+            }
+
+            int min_distance = 0xFFFFFF;
+            int min_color = 0;
+            
+            foreach (int rr in values(r)) {
+                foreach (int gg in values(g)) {
+                    foreach (int bb in values(b)) {
+                        int distance = (r-rr)*(r-rr) + (g-gg)*(g-gg) + (b-bb)*(b-bb);
+                        if (distance < min_distance) {
+                            min_distance = distance;
+                            min_color = (rr << 16) + (gg << 8) + bb;
+                        }
+                    }
+                }
+            }
+            
+            return String.Format("#{0:x6}", min_color);
+        }
+        
+    }
+}
