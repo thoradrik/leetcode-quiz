57cb4c820580282a2af310bc5c80d5457c049657
thoradrik <thoradrik@gmail.com>
2021-03-06 12:02:30 +0500

    Solved 338. Counting Bits https://leetcode.com/problems/counting-bits/

---
+++ problems/0338_CountingBits/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.CountBits, 0,  IntArray("[0]"));
+            Test.Check(solution.CountBits, 1,  IntArray("[0,1]"));
+            
+            Test.Check(solution.CountBits, 2,  IntArray("[0,1,1]"));
+            Test.Check(solution.CountBits, 3,  IntArray("[0,1,1,2]"));
+            
+            Test.Check(solution.CountBits, 4,  IntArray("[0,1,1,2,1]"));
+            Test.Check(solution.CountBits, 5,  IntArray("[0,1,1,2,1,2]"));
+            Test.Check(solution.CountBits, 6,  IntArray("[0,1,1,2,1,2,2]"));
+            Test.Check(solution.CountBits, 7,  IntArray("[0,1,1,2,1,2,2,3]"));
+            
+            Test.Check(solution.CountBits, 8,  IntArray("[0,1,1,2,1,2,2,3,1]"));
+            Test.Check(solution.CountBits, 9,  IntArray("[0,1,1,2,1,2,2,3,1,2]"));
+            Test.Check(solution.CountBits, 10, IntArray("[0,1,1,2,1,2,2,3,1,2,2]"));
+            Test.Check(solution.CountBits, 11, IntArray("[0,1,1,2,1,2,2,3,1,2,2,3]"));
+            Test.Check(solution.CountBits, 12, IntArray("[0,1,1,2,1,2,2,3,1,2,2,3,2]"));
+            Test.Check(solution.CountBits, 13, IntArray("[0,1,1,2,1,2,2,3,1,2,2,3,2,3]"));
+            Test.Check(solution.CountBits, 14, IntArray("[0,1,1,2,1,2,2,3,1,2,2,3,2,3,3]"));
+            Test.Check(solution.CountBits, 15, IntArray("[0,1,1,2,1,2,2,3,1,2,2,3,2,3,3,4]"));
+        }
+
+    }
+}
---
+++ problems/0338_CountingBits/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public int[] CountBits(int num) {
+            int[] counts = new int[num + 1];
+            
+            counts[0] = 0;
+            
+            for (int i = 1; i <= num; i++) {
+                counts[i] = counts[i >> 1] + (i & 0b1);
+            }
+
+            return counts;
+        }
+        
+    }
+}
