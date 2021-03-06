db0999afd3dfcfe9c20daaf8024082bd8f8144a3
thoradrik <thoradrik@gmail.com>
2021-03-06 13:08:03 +0500

    Solved 535. Encode and Decode TinyURL https://leetcode.com/problems/encode-and-decode-tinyurl/

---
+++ problems/0535_EncodeAndDecodeTinyURL/Codec.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quiz {
+    public class Codec {
+
+        private static readonly Dictionary<string, string> m_Map = new Dictionary<string, string>();
+
+        public string encode(string longUrl) {
+            string key = Guid.NewGuid().ToString("N");
+            m_Map[key] = longUrl;
+            return "http://tinyurl.com/" + key;
+        }
+
+        public string decode(string shortUrl) {
+            string key = shortUrl.Replace("http://tinyurl.com/", String.Empty);
+            return m_Map.TryGetValue(key, out string longUrl) ? longUrl : null;
+        }
+    }
+}
---
+++ problems/0535_EncodeAndDecodeTinyURL/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Codec codec = new Codec();
+
+            string[] urls = {
+                "https://leetcode.com/problems/encode-and-decode/",
+                "https://leetcode.com/problems/encode-and-decode-tinyurl/"
+            };
+            
+            foreach (var url in urls) {
+                Test.Check(codec.decode, codec.encode(url), url);
+            }
+        }
+
+    }
+}
