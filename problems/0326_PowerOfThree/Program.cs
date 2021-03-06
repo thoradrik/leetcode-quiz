eea0185d966057863d334870fd64abea2802af19
thoradrik <thoradrik@gmail.com>
2021-03-06 12:59:34 +0500

    Solved 326. Power of Three https://leetcode.com/problems/power-of-three/

---
+++ problems/0326_PowerOfThree/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.IsPowerOfThree, 1, true);
+            Test.Check(solution.IsPowerOfThree, 2, false);
+            Test.Check(solution.IsPowerOfThree, 3, true);
+            Test.Check(solution.IsPowerOfThree, 6, false);
+            Test.Check(solution.IsPowerOfThree, 9, true);
+            Test.Check(solution.IsPowerOfThree, 10, false);
+            Test.Check(solution.IsPowerOfThree, 15, false);
+            Test.Check(solution.IsPowerOfThree, 16, false);
+            Test.Check(solution.IsPowerOfThree, 27, true);
+            Test.Check(solution.IsPowerOfThree, 81, true);
+            Test.Check(solution.IsPowerOfThree, 243, true);
+            Test.Check(solution.IsPowerOfThree, 729, true);
+            Test.Check(solution.IsPowerOfThree, 2187, true);
+            Test.Check(solution.IsPowerOfThree, 6561, true);
+            Test.Check(solution.IsPowerOfThree, 19683, true);
+            Test.Check(solution.IsPowerOfThree, 59049, true);
+            Test.Check(solution.IsPowerOfThree, 216513, false);
+            Test.Check(solution.IsPowerOfThree, 649539, false);
+            Test.Check(solution.IsPowerOfThree, 1948617, false);
+            Test.Check(solution.IsPowerOfThree, 177147, true);
+            Test.Check(solution.IsPowerOfThree, 531441, true);
+            Test.Check(solution.IsPowerOfThree, 1594323, true);
+            Test.Check(solution.IsPowerOfThree, 4782969, true);
+            Test.Check(solution.IsPowerOfThree, 14348907, true);
+            Test.Check(solution.IsPowerOfThree, 43046721, true);
+            Test.Check(solution.IsPowerOfThree, 129140163, true);
+            Test.Check(solution.IsPowerOfThree, 387420489, true);
+            Test.Check(solution.IsPowerOfThree, 1162261467, true);
+            Test.Check(solution.IsPowerOfThree, -1162261467, false);
+            Test.Check(solution.IsPowerOfThree, -9, false);
+        }
+
+    }
+}
---
+++ problems/0326_PowerOfThree/Solution.cs
@@ -0,0 +1,13 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public bool IsPowerOfThree(int n) {
+            if (n > 0) {
+                return 1162261467 % n == 0;
+            } else {
+                return false;
+            }
+        }
+        
+    }
+}
