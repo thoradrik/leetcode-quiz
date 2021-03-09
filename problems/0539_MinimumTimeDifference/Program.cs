9d11f6567015ff8e55d554768ee6e221079663d5
thoradrik <thoradrik@gmail.com>
2021-03-09 18:08:05 +0500

    Solved 539. Minimum Time Difference https://leetcode.com/problems/minimum-time-difference/

---
+++ problems/0539_MinimumTimeDifference/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.FindMinDifference, StringArray("[\"00:00\",\"00:00\"]"), 0);
+            Test.Check(solution.FindMinDifference, StringArray("[\"00:00\",\"00:01\"]"), 1);
+            Test.Check(solution.FindMinDifference, StringArray("[\"00:00\",\"23:59\"]"), 1);
+            Test.Check(solution.FindMinDifference, StringArray("[\"00:00\",\"23:58\"]"), 2);
+            Test.Check(solution.FindMinDifference, StringArray("[\"00:00\",\"00:01\",\"23:58\"]"), 1);
+            Test.Check(solution.FindMinDifference, StringArray("[\"00:00\",\"00:05\",\"23:55\"]"), 5);
+            Test.Check(solution.FindMinDifference, StringArray("[\"00:00\",\"00:05\",\"23:56\"]"), 4);
+            Test.Check(solution.FindMinDifference, StringArray("[\"00:00\",\"00:03\",\"23:55\"]"), 3);
+            Test.Check(solution.FindMinDifference, StringArray("[\"23:50\",\"00:05\",\"00:25\",\"23:55\"]"), 5);
+            Test.Check(solution.FindMinDifference, StringArray("[\"23:10\",\"00:05\",\"00:25\",\"23:55\"]"), 10);
+            Test.Check(solution.FindMinDifference, StringArray("[\"23:50\",\"00:05\",\"00:25\",\"00:25\",\"23:55\"]"), 0);
+        }
+
+    }
+}
---
+++ problems/0539_MinimumTimeDifference/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+        
+        public int FindMinDifference(IList<string> time_points) {
+            HashSet<int> points_hs = new HashSet<int>();
+
+            foreach (string time_point in time_points) {
+                string[] parts = time_point.Split(':');
+
+                int point = Int32.Parse(parts[0]) * 60 + Int32.Parse(parts[1]);
+                if (points_hs.Contains(point)) {
+                    return 0;
+                }
+                
+                points_hs.Add(point);
+            }
+
+            List<int> points = new List<int>();
+            
+            foreach (int point in points_hs) {
+                points.Add(point);
+                points.Add(point + 1440);
+            }
+            
+            points.Sort();
+
+            int min_distance = points[1] - points[0];
+            
+            for (int i = 2; i < points.Count; i++) {
+                int point_j = points[i - 1];
+                int point_i = points[i];
+                min_distance = Math.Min(min_distance, (point_i - point_j) % 1400);
+            }
+
+            return min_distance;
+        }
+        
+    }
+}
