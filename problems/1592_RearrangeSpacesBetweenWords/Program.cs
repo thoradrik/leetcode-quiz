5b1404439c2e987c691fbd054254425aed9ae0c2
thoradrik <thoradrik@gmail.com>
2021-03-09 18:34:22 +0500

    Solved 1592. Rearrange Spaces Between Words https://leetcode.com/problems/rearrange-spaces-between-words/

---
+++ problems/1592_RearrangeSpacesBetweenWords/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.ReorderSpaces, "one", "one");
+            Test.Check(solution.ReorderSpaces, " one", "one ");
+            Test.Check(solution.ReorderSpaces, " one ", "one  ");
+            Test.Check(solution.ReorderSpaces, " one  ", "one   ");
+            Test.Check(solution.ReorderSpaces, "  one  ", "one    ");
+            Test.Check(solution.ReorderSpaces, "hi there", "hi there");
+            Test.Check(solution.ReorderSpaces, " hi there", "hi  there");
+            Test.Check(solution.ReorderSpaces, " hi there ", "hi   there");
+            Test.Check(solution.ReorderSpaces, " hi  there ", "hi    there");
+            Test.Check(solution.ReorderSpaces, " hi  there  ", "hi     there");
+            Test.Check(solution.ReorderSpaces, "hi there  some code", "hi there some code ");
+            Test.Check(solution.ReorderSpaces, "hi  there  some code", "hi there some code  ");
+            Test.Check(solution.ReorderSpaces, "hi  there   some  code", "hi  there  some  code ");
+            Test.Check(solution.ReorderSpaces, "hi   there   some  code", "hi  there  some  code  ");
+            Test.Check(solution.ReorderSpaces, "hi   there   some    code", "hi   there   some   code ");
+            Test.Check(solution.ReorderSpaces, "hi    there   some    code", "hi   there   some   code  ");
+            Test.Check(solution.ReorderSpaces, "hi    there    some    code", "hi    there    some    code");
+            Test.Check(solution.ReorderSpaces, " hi     there    some    code", "hi    there    some    code  ");
+            Test.Check(solution.ReorderSpaces, " hi     there    some    code ", "hi     there     some     code");
+        }
+
+    }
+}
---
+++ problems/1592_RearrangeSpacesBetweenWords/Solution.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Quiz {
+    public class Solution {
+        
+        public string ReorderSpaces(string text) {
+            int spaces = 0;
+
+            List<(int, int)> words = new List<(int,int)>();
+
+            int word_start = -1;
+            int word_length = 0;
+
+            for (int i = 0; i < text.Length; i++) {
+                if (text[i] == ' ') {
+                    if (word_length > 0) {
+                        words.Add((word_start, word_length));
+
+                        word_start = -1;
+                        word_length = 0;
+                    }
+                    
+                    spaces++;
+                } else {
+                    if (word_length > 0) {
+                        word_length++;
+                    } else {
+                        word_start = i;
+                        word_length = 1;
+                    }
+                }
+            }
+
+            if (word_length > 0) {
+                words.Add((word_start, word_length));
+            }
+
+            StringBuilder sb = new StringBuilder(text.Length);
+
+            if (words.Count > 1) {
+                int gaps_count = words.Count - 1;
+                int middle_spaces = spaces / gaps_count;
+                int trailing_spaces = spaces % gaps_count;
+                
+                for (int index = 0; index < words.Count; index++) {
+                    (int start, int length) = words[index];
+                    if (index > 0) {
+                        sb.Append(' ', middle_spaces);
+                    }
+
+                    sb.Append(text.Substring(start, length));
+                }
+                
+                sb.Append(' ', trailing_spaces);
+            } else {
+                (int start, int length) = words[0];
+                sb.Append(text.Substring(start, length));
+                sb.Append(' ', spaces);
+            }
+
+            return sb.ToString();
+        }
+        
+    }
+}
