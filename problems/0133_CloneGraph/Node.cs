d04e5b570758fcb4a1abc744f93014b773bfe087
thoradrik <thoradrik@gmail.com>
2021-03-10 10:15:14 +0500

    Solved 133. Clone Graph https://leetcode.com/problems/clone-graph/

---
+++ problems/0133_CloneGraph/Node.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+namespace Quiz {
+    public class Node {
+        
+        public int val;
+        
+        public IList<Node> neighbors;
+
+        public Node() {
+            val = 0;
+            neighbors = new List<Node>();
+        }
+
+        public Node(int _val) {
+            val = _val;
+            neighbors = new List<Node>();
+        }
+
+        public Node(int _val, List<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+        
+    }
+}
---
+++ problems/0133_CloneGraph/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+        
+        public Node CloneGraph(Node node) {
+            if (node == null) {
+                return null;
+            }
+            
+            Dictionary<int, Node> map = new Dictionary<int, Node>();
+
+            Node clone_node(Node original) {
+                if (map.TryGetValue(original.val, out Node clone)) {
+                    return clone;
+                } else {
+                    clone = new Node(original.val);
+                    map.Add(clone.val, clone);
+
+                    foreach (Node neighbor in original.neighbors) {
+                        clone.neighbors.Add(clone_node(neighbor));
+                    }
+
+                    return clone;
+                }
+            }
+
+            return clone_node(node);
+        }
+        
+    }
+}
