77f1020274676b602b60bda5e65977dbe3495758
thoradrik <thoradrik@gmail.com>
2021-03-10 10:19:51 +0500

    Solved 1490. Clone N-ary Tree https://leetcode.com/problems/clone-n-ary-tree/

---
+++ problems/1490_CloneNaryTree/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public Node CloneTree(Node root) {
+            if (root == null) {
+                return null;
+            }
+            
+            Node clone_node(Node original) {
+                Node clone = new Node(original.val);
+
+                foreach (Node child in original.children) {
+                    clone.children.Add(clone_node(child));
+                }
+                
+                return clone;
+            }
+
+            return clone_node(root);
+        }
+        
+    }
+}
