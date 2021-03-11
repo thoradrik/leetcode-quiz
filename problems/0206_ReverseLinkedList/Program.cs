84370baed559b0f09a1f4acf9319e42ce655712c
thoradrik <thoradrik@gmail.com>
2021-03-11 19:36:30 +0500

    Solved 206. Reverse Linked List https://leetcode.com/problems/reverse-linked-list/

---
+++ problems/0206_ReverseLinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.ReverseList, IntArray("[]"), IntArray("[]"));
+            Test.Check(solution.ReverseList, IntArray("[1]"), IntArray("[1]"));
+            Test.Check(solution.ReverseList, IntArray("[1,2]"), IntArray("[2,1]"));
+            Test.Check(solution.ReverseList, IntArray("[1,2,3]"), IntArray("[3,2,1]"));
+            Test.Check(solution.ReverseList, IntArray("[1,2,3,4]"), IntArray("[4,3,2,1]"));
+            Test.Check(solution.ReverseList, IntArray("[1,2,3,4,5]"), IntArray("[5,4,3,2,1]"));
+        }
+
+    }
+
+}
---
+++ problems/0206_ReverseLinkedList/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace Quiz {
+    public class Solution {
+
+        public int[] ReverseList(int[] arr) {
+            ListNode node = null;
+
+            for (int index = arr.Length - 1; index >= 0; index--) {
+                node = new ListNode(arr[index], node);
+            }
+
+            node = ReverseList(node);
+
+            List<int> list = new List<int>();
+            
+            while (node != null) {
+                list.Add(node.val);
+                
+                node = node.next;
+            }
+
+            return list.ToArray();
+        }
+
+        public ListNode ReverseList(ListNode head) {
+            ListNode prev = null;
+            ListNode current = head;
+
+            while (current != null) {
+                ListNode next = current.next;
+                current.next = prev;
+                
+                prev = current;
+                current = next;
+            }
+
+            return prev;
+        }
+
+    }
+}
