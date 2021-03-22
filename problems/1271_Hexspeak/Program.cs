99f7bf116f73589e38db2cc8abbf81a667aec4b8
thoradrik <thoradrik@gmail.com>
2021-03-22 18:15:54 +0500

    Solved 1271. Hexspeak https://leetcode.com/problems/hexspeak/

---
+++ problems/1271_Hexspeak/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.ToHexspeak, "0", "O");
+            Test.Check(solution.ToHexspeak, "1", "I");
+            Test.Check(solution.ToHexspeak, "15", "F");
+            Test.Check(solution.ToHexspeak, "16", "IO");
+            Test.Check(solution.ToHexspeak, "31", "IF");
+            Test.Check(solution.ToHexspeak, "255", "FF");
+            Test.Check(solution.ToHexspeak, "257", "IOI");
+            Test.Check(solution.ToHexspeak, "3", "ERROR");
+            Test.Check(solution.ToHexspeak, "1025921126416", "EEDDAAIOIO");
+        }
+
+    }
+}
---
+++ problems/1271_Hexspeak/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace Quiz {
+    public class Solution {
+        
+        public string ToHexspeak(string num) {
+            StringBuilder hex = new StringBuilder();
+
+            long acc = 0;
+
+            for (int i = 0; i < num.Length; i++) {
+                acc *= 10;
+                acc += num[i] - '0';
+            }
+
+            while (acc > 0 || hex.Length == 0) {
+                switch (acc % 0x10) {
+                    case 0x0: hex.Insert(0, "O"); break;
+                    case 0x1: hex.Insert(0, "I"); break;
+                    case 0xA: hex.Insert(0, "A"); break;
+                    case 0xB: hex.Insert(0, "B"); break;
+                    case 0xC: hex.Insert(0, "C"); break;
+                    case 0xD: hex.Insert(0, "D"); break;
+                    case 0xE: hex.Insert(0, "E"); break;
+                    case 0xF: hex.Insert(0, "F"); break;
+                    default: return "ERROR";
+                }
+                acc >>= 4;
+            }
+            
+            return hex.ToString();
+        }
+        
+    }
+}
