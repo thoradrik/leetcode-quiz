152ea9abeb0100e018116f0642ff006a5e72e6dd
thoradrik <thoradrik@gmail.com>
2021-03-23 16:14:28 +0500

    Solved 551. Student Attendance Record I https://leetcode.com/problems/student-attendance-record-i/

---
+++ problems/0551_StudentAttendanceRecord/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.CheckRecord, "A", true);
+            Test.Check(solution.CheckRecord, "AA", false);
+            Test.Check(solution.CheckRecord, "L", true);
+            Test.Check(solution.CheckRecord, "LL", true);
+            Test.Check(solution.CheckRecord, "LLL", false);
+            Test.Check(solution.CheckRecord, "P", true);
+            Test.Check(solution.CheckRecord, "PP", true);
+            Test.Check(solution.CheckRecord, "PPP", true);
+            Test.Check(solution.CheckRecord, "PPPP", true);
+            Test.Check(solution.CheckRecord, "PPPPL", true);
+            Test.Check(solution.CheckRecord, "LPPPPL", true);
+            Test.Check(solution.CheckRecord, "LPLPPPL", true);
+            Test.Check(solution.CheckRecord, "LPLLPPPL", true);
+            Test.Check(solution.CheckRecord, "LPLLPPLPL", true);
+            Test.Check(solution.CheckRecord, "LPLLPPLLPL", true);
+            Test.Check(solution.CheckRecord, "LPLALPPLLPL", true);
+            Test.Check(solution.CheckRecord, "LPLLPPLLLPL", false);
+            Test.Check(solution.CheckRecord, "LPLLPPLALPL", true);
+            Test.Check(solution.CheckRecord, "LPLLPPLALLPL", true);
+            Test.Check(solution.CheckRecord, "LPLLLPPLALLPL", false);
+            Test.Check(solution.CheckRecord, "LPLALPPLALLPL", false);
+        }
+
+    }
+}
---
+++ problems/0551_StudentAttendanceRecord/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public bool CheckRecord(string s) {
+            int a_total = 0;
+            int l_row = 0;
+            
+            for (int i = 0; i < s.Length; i++) {
+                char c = s[i];
+                if (c == 'P') {
+                    l_row = 0;
+                } else if (c == 'A') {
+                    a_total++;
+                    
+                    if (a_total >= 2) {
+                        return false;
+                    } else {
+                        l_row = 0;
+                    }
+                } else if (c == 'L') {
+                    l_row++;
+                    
+                    if (l_row >= 3) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+        
+    }
+}
