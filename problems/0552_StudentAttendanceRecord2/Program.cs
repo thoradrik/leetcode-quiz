79910547effe481b13e0efa36af55b87dd86809c
thoradrik <thoradrik@gmail.com>
2021-03-24 19:03:49 +0500

    Solved 552. Student Attendance Record II https://leetcode.com/problems/student-attendance-record-ii/

---
+++ problems/0552_StudentAttendanceRecord2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.CheckRecord, 1, 3);
+            Test.Check(solution.CheckRecord, 2, 8);
+            Test.Check(solution.CheckRecord, 3, 19);
+            Test.Check(solution.CheckRecord, 10101, 183236316);
+        }
+
+    }
+}
---
+++ problems/0552_StudentAttendanceRecord2/Solution.cs
@@ -0,0 +1,49 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public int CheckRecord(int n) {
+            long m = 1000000007;
+            
+            long a = 0;
+            long l = 0;
+            long a_l = 0;
+            long ll = 0;
+            long a_ll = 0;
+            long p = 1;
+
+            for (int i = 0; i < n; i++) {
+                long new_a =
+                    /* ....A + P */ a +
+                    /* ...AL + P */ a_l +
+                    /* ..ALL + P */ a_ll +
+                    /* ....L + A */ l +
+                    /* ...LL + A */ ll +
+                    /* ..... + A */ p;
+                long new_l = 
+                    /* ..... + L */ p;
+                long new_a_l = 
+                    /* ..A.. + L */ a;
+                long new_ll = 
+                    /* ....L + L */ l;
+                long new_a_ll = 
+                    /* ...AL + L */ a_l;
+                long new_p = 
+                    /* ....L + P */ l +
+                    /* ...LL + P */ ll +
+                    /* ..... + P */ p;
+
+                a = new_a % m;
+                l = new_l % m;
+                a_l = new_a_l % m;
+                ll = new_ll % m;
+                a_ll = new_a_ll % m;
+                p = new_p % m;            
+            }
+
+            long result = (a + a_l + a_ll + l + ll + p) % m;
+
+            return (int) result;
+        }
+        
+    }
+}
