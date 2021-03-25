0bbeebdddfce03d9c565a488a91190dd21a74a0a
thoradrik <thoradrik@gmail.com>
2021-03-25 15:51:46 +0500

    Solved 693. Binary Number with Alternating Bits https://leetcode.com/problems/binary-number-with-alternating-bits/

---
+++ problems/0693_BinaryNumberWithAlternatingBits/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.HasAlternatingBits, 0b0, true);
+            Test.Check(solution.HasAlternatingBits, 0b1, true);
+            Test.Check(solution.HasAlternatingBits, 0b10, true);
+            Test.Check(solution.HasAlternatingBits, 0b11, false);
+            Test.Check(solution.HasAlternatingBits, 0b110, false);
+            Test.Check(solution.HasAlternatingBits, 0b101, true);
+            Test.Check(solution.HasAlternatingBits, 0b1011, false);
+            Test.Check(solution.HasAlternatingBits, 0b1010, true);
+            Test.Check(solution.HasAlternatingBits, 0b1110, false);
+            Test.Check(solution.HasAlternatingBits, 0b101010, true);
+            Test.Check(solution.HasAlternatingBits, 0b101011, false);
+            Test.Check(solution.HasAlternatingBits, 0b1010101, true);
+            Test.Check(solution.HasAlternatingBits, 0b1110101, false);
+            Test.Check(solution.HasAlternatingBits, 0b10110101, false);
+            Test.Check(solution.HasAlternatingBits, 0b101010101, true);
+        }
+
+    }
+}
---
+++ problems/0693_BinaryNumberWithAlternatingBits/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public bool HasAlternatingBits(int n) {
+            int prev = n & 2;
+            n >>= 1;
+
+            while (n > 0) {
+                int current = n & 2;
+                if (current == prev) {
+                    return false;
+                }
+                
+                n >>= 1;
+                prev = current;
+            }
+
+            return true;
+        }
+        
+    }
+}
