a345651a9dbcf50de3eb9481cc85c3b9ea655452
thoradrik <thoradrik@gmail.com>
2021-03-25 16:53:12 +0500

    Solved 169. Majority Element https://leetcode.com/problems/majority-element/

---
+++ problems/0169_MajorityElement/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.MajorityElement, IntArray("[1]"), 1);
+            Test.Check(solution.MajorityElement, IntArray("[1,2,2]"), 2);
+            Test.Check(solution.MajorityElement, IntArray("[2,1,2,2]"), 2);
+            Test.Check(solution.MajorityElement, IntArray("[1,1,1,3,2,1,2,5]"), 1);
+            Test.Check(solution.MajorityElement, IntArray("[2,1,1,2,2,3,2,3,2]"), 2);
+            Test.Check(solution.MajorityElement, IntArray("[2,1,3,1,3,3,2,3,2]"), 3);
+            Test.Check(solution.MajorityElement, IntArray("[5,2,5,1,5,5,3,1,5,5,3,5,5,3,5,2,5,3,5,2]"), 5);
+        }
+
+    }
+}
---
+++ problems/0169_MajorityElement/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public int MajorityElement(int[] nums) {
+            int candidate = 0;
+            int count = 0;
+            
+            foreach (int num in nums) {
+                if (count == 0) {
+                    candidate = num;
+                }
+
+                if (candidate == num) {
+                    count++;
+                } else {
+                    count--;
+                }
+            }
+
+            return candidate;
+        }
+        
+    }
+}
