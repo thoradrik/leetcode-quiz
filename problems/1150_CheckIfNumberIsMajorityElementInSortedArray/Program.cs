b8892c47b6c04d90f5112234c3e5aadd65192168
thoradrik <thoradrik@gmail.com>
2021-03-27 11:49:19 +0500

    Solved 1150. Check If a Number Is Majority Element in a Sorted Array https://leetcode.com/problems/check-if-a-number-is-majority-element-in-a-sorted-array/

---
+++ problems/1150_CheckIfNumberIsMajorityElementInSortedArray/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            // Test.Check(solution.IsMajorityElement, IntArray("[0]"), 5, false);
+            // Test.Check(solution.IsMajorityElement, IntArray("[5]"), 5, true);
+            // Test.Check(solution.IsMajorityElement, IntArray("[1,2]"), 2, false);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,2,2]"), 2, true);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,2,3]"), 5, false);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,2,3,4,5]"), 5, false);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,2,3,4,4,5]"), 4, false);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,2,3,4,4,4,5]"), 4, false);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,2,3,4,4,4,4,5]"), 4, false);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,2,3,4,4,4,4,4,5]"), 4, true);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,2,2,3,4,4,4,4,4,5]"), 4, false);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,2,2,3,4,4,4,4,4,4,5]"), 4, true);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,2,2,3,4,4,4,4,4,4,4,5]"), 4, true);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,2,2,3,3,4,4,4,4,4,4,4,5]"), 4, true);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,1,2,2,3,3,4,4,4,4,4,4,4,5]"), 4, false);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,1,1,1,2,2,3,3,4,4,4,4,4,4,4,5]"), 1, false);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,1,1,1,1,1,1,1,2,2,3,3,4,4,4,4,4,4,4,5]"), 1, false);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,3,3,4,4,4,4,4,4,4,5]"), 1, true);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,3,3,4,4,4,4,4,4,4,5]"), 5, false);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,3,3,4,4,4,4,4,4,4,5,5,5,5]"), 5, false);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,3,3,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5]"), 5, false);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,3,3,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]"), 5, false);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,3,3,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]"), 5, false);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,3,3,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]"), 5, false);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,3,3,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]"), 5, true);
+            Test.Check(solution.IsMajorityElement, IntArray("[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,3,3,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]"), 5, true);
+        }
+
+    }
+}
---
+++ problems/1150_CheckIfNumberIsMajorityElementInSortedArray/Solution.cs
@@ -0,0 +1,47 @@
+﻿namespace Quiz {
+    public class Solution {
+        
+        public bool IsMajorityElement(int[] nums, int target) {
+            if (nums.Length < 2) {
+                return nums[0] == target;
+            }
+            
+            int search_start(int i, int j) {
+                if (nums[i] == target) return i;
+
+                while (j - i > 1) {
+                    int m = (i + j) / 2;
+                    if (nums[m] >= target) {
+                        j = m;
+                    } else {
+                        i = m;
+                    }
+                }
+
+                return j;
+            }
+
+            int search_end(int i, int j) {
+                if (nums[j] == target) return j;
+                
+                while (j - i > 1) {
+                    int m = (i + j) / 2;
+                    if (nums[m] <= target) {
+                        i = m;
+                    } else {
+                        j = m;
+                    }
+                }
+
+                return i;
+            }
+
+            int start = search_start(0, nums.Length - 1);
+            int end = search_end(0, nums.Length - 1);
+            int length = end - start + 1;
+            
+            return length > nums.Length / 2;
+        }
+        
+    }
+}
