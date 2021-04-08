43096bf2a8017b4b3076de6e21f0c4439c22e801
thoradrik <thoradrik@gmail.com>
2021-04-08 15:10:40 +0500

    Solved 101. Symmetric Tree https://leetcode.com/problems/symmetric-tree/

---
+++ problems/0101_SymmetricTree/Program.cs
@@ -0,0 +1,27 @@
+﻿using LeetcodeLib;
+
+namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.IsSymmetric, TreeHelper.CreateTreeFromBFS("[1]"), true);
+            Test.Check(solution.IsSymmetric, TreeHelper.CreateTreeFromBFS("[1,2,null]"), false);
+            Test.Check(solution.IsSymmetric, TreeHelper.CreateTreeFromBFS("[1,null,2]"), false);
+            Test.Check(solution.IsSymmetric, TreeHelper.CreateTreeFromBFS("[1,2,1]"), false);
+            Test.Check(solution.IsSymmetric, TreeHelper.CreateTreeFromBFS("[1,2,2]"), true);
+            Test.Check(solution.IsSymmetric, TreeHelper.CreateTreeFromBFS("[1,2,2,null,1,null,null]"), false);
+            Test.Check(solution.IsSymmetric, TreeHelper.CreateTreeFromBFS("[1,2,2,null,1,null,1]"), false);
+            Test.Check(solution.IsSymmetric, TreeHelper.CreateTreeFromBFS("[1,2,2,null,1,1,null]"), true);
+            Test.Check(solution.IsSymmetric, TreeHelper.CreateTreeFromBFS("[1,2,2,null,1,1,2]"), false);
+            Test.Check(solution.IsSymmetric, TreeHelper.CreateTreeFromBFS("[1,2,2,3,1,1,2]"), false);
+            Test.Check(solution.IsSymmetric, TreeHelper.CreateTreeFromBFS("[1,2,2,3,1,1,3]"), true);
+            Test.Check(solution.IsSymmetric, TreeHelper.CreateTreeFromBFS("[1,2,2,3,1,1,3,4,4,4,4,4,4,4,4]"), true);
+            Test.Check(solution.IsSymmetric, TreeHelper.CreateTreeFromBFS("[1,2,2,3,1,1,3,4,5,4,4,4,4,5,4]"), true);
+            Test.Check(solution.IsSymmetric, TreeHelper.CreateTreeFromBFS("[1,2,2,3,1,1,3,4,5,4,6,7,4,5,4]"), false);
+            Test.Check(solution.IsSymmetric, TreeHelper.CreateTreeFromBFS("[1,2,2,3,1,1,3,4,5,4,6,6,4,5,4]"), true);
+        }
+
+    }
+}
---
+++ problems/0101_SymmetricTree/Solution.cs
@@ -0,0 +1,25 @@
+﻿using LeetcodeLib;
+
+namespace Quiz {
+    public class Solution {
+        
+        public bool IsSymmetric(TreeNode root) {
+            return Check(root.left, root.right);
+        }
+
+        private bool Check(TreeNode l_node, TreeNode r_node) {
+            if (l_node == null && r_node == null) {
+                return true;
+            } 
+            
+            if (l_node != null && r_node != null) {
+                if (l_node.val == r_node.val) {
+                    return Check(l_node.right, r_node.left) && Check(l_node.left, r_node.right);
+                }
+            }
+            
+            return false;
+        }
+        
+    }
+}
