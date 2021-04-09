cca16ee1b55798cfa573597edfd06c66fec61417
thoradrik <thoradrik@gmail.com>
2021-04-09 20:36:33 +0500

    Solved 110. Balanced Binary Tree https://leetcode.com/problems/balanced-binary-tree/

---
+++ problems/0110_BalancedBinaryTree/Program.cs
@@ -0,0 +1,20 @@
+﻿using LeetcodeLib;
+
+namespace Quiz {
+    public class Program : ProgramBase {
+        
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.IsBalanced, TreeHelper.CreateTreeFromBFS("[]"), true);
+            Test.Check(solution.IsBalanced, TreeHelper.CreateTreeFromBFS("[1]"), true);
+            Test.Check(solution.IsBalanced, TreeHelper.CreateTreeFromBFS("[1,2,2]"), true);
+            Test.Check(solution.IsBalanced, TreeHelper.CreateTreeFromBFS("[1,2,2,3,null,3,3]"), true);
+            Test.Check(solution.IsBalanced, TreeHelper.CreateTreeFromBFS("[1,2,2,3,null,null,null]"), true);
+            Test.Check(solution.IsBalanced, TreeHelper.CreateTreeFromBFS("[1,2,2,3,null,null,null,4,4]"), false);
+            Test.Check(solution.IsBalanced, TreeHelper.CreateTreeFromBFS("[3,9,20,null,null,15,7]"), true);
+            Test.Check(solution.IsBalanced, TreeHelper.CreateTreeFromBFS("[1,2,2,3,3,null,null,4,4]"), false);
+        }
+
+    }
+}
---
+++ problems/0110_BalancedBinaryTree/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using LeetcodeLib;
+
+namespace Quiz {
+    public class Solution {
+        
+        public bool IsBalanced(TreeNode root) {
+            return IsBalanced(root, out _);
+        }
+        
+        private bool IsBalanced(TreeNode root, out int height) {
+            if (root == null) {
+                height = 0;
+                return true;
+            } else {
+                bool l_balanced = IsBalanced(root.left, out int l_height);
+                if (!l_balanced) {
+                    height = 0;
+                    return false;
+                }
+                
+                bool r_balanced = IsBalanced(root.right, out int r_height);
+                if (!r_balanced) {
+                    height = 0;
+                    return false;
+                }
+
+                int max = Math.Max(l_height, r_height);
+                int min = Math.Min(l_height, r_height);
+
+                height = max + 1;
+                
+                return max - min <= 1;
+            }
+        }
+        
+    }
+}
