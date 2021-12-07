1c0fedd410ec7b107ce39bb3d256c3650843693e
thoradrik <thoradrik@gmail.com>
2021-12-07 23:09:57 +0500

    Solved 14. Longest Common Prefix https://leetcode.com/problems/longest-common-prefix/

---
+++ problems/0014_LongestCommonPrefix/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Quiz {
+    public class Program : ProgramBase {
+
+        public static void Main() {
+            Solution solution = new Solution();
+
+            Test.Check(solution.LongestCommonPrefix, new[] { "" }, "");
+            Test.Check(solution.LongestCommonPrefix, new[] { "a" }, "a");
+            Test.Check(solution.LongestCommonPrefix, new[] { "a", "a" }, "a");
+            Test.Check(solution.LongestCommonPrefix, new[] { "a", "a", "a" }, "a");
+            Test.Check(solution.LongestCommonPrefix, new[] { "aa", "a", "a" }, "a");
+            Test.Check(solution.LongestCommonPrefix, new[] { "a", "a", "aa" }, "a");
+            Test.Check(solution.LongestCommonPrefix, new[] { "a", "a", "aa", "a" }, "a");
+            Test.Check(solution.LongestCommonPrefix, new[] { "aa", "a", "aa", "aa" }, "a");
+            Test.Check(solution.LongestCommonPrefix, new[] { "aa", "aaa", "aa", "aa" }, "aa");
+            Test.Check(solution.LongestCommonPrefix, new[] { "aa", "aba", "aa", "aa" }, "a");
+            Test.Check(solution.LongestCommonPrefix, new[] { "aa", "aab", "aa", "aa" }, "aa");
+            Test.Check(solution.LongestCommonPrefix, new[] { "ba", "aab", "aa", "aa" }, "");
+        }
+
+    }
+}
---
+++ problems/0014_LongestCommonPrefix/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace Quiz {
+    public class Solution {
+
+        public string LongestCommonPrefix(string[] strs) {
+            if (strs.Length == 1) {
+                return strs[0];
+            }
+
+            int lcp = 0;
+
+            string s0 = strs[0];
+
+            while (s0.Length > lcp) {
+                char p = s0[lcp];
+
+                for (int index = 1; index < strs.Length; index++) {
+                    string str = strs[index];
+                    if (str.Length <= lcp) {
+                        return str;
+                    }
+                    if (str[lcp] != p) {
+                        return s0.Substring(0, lcp);
+                    }
+                }
+
+                lcp++;
+            }
+
+            return s0;
+        }
+
+    }
+}
