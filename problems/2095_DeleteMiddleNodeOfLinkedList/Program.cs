5642a3c609faf4a5eecc5c6775a2c6b7d1766bcc
thoradrik <thoradrik@gmail.com>
2024-04-13 21:08:35 +0500

    Solved 2095. Delete the Middle Node of a Linked List https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/

---
+++ problems/2095_DeleteMiddleNodeOfLinkedList/Program.cs
@@ -0,0 +1,21 @@
+﻿using static LeetcodeLib.LinkedListHelper;
+
+namespace Quiz;
+
+public class Program : ProgramBase {
+    
+    public static void Main() {
+        Solution solution = new Solution();
+        
+        Test.Check(solution.DeleteMiddle, MakeLinkedList("[0]"),  MakeLinkedList("[]"));
+        Test.Check(solution.DeleteMiddle, MakeLinkedList("[1,2]"), MakeLinkedList("[1]"));
+        Test.Check(solution.DeleteMiddle, MakeLinkedList("[1,2,3]"), MakeLinkedList("[1,3]"));
+        Test.Check(solution.DeleteMiddle, MakeLinkedList("[1,2,3,4]"), MakeLinkedList("[1,2,4]"));
+        Test.Check(solution.DeleteMiddle, MakeLinkedList("[1,2,3,4,5]"), MakeLinkedList("[1,2,4,5]"));
+        Test.Check(solution.DeleteMiddle, MakeLinkedList("[1,2,3,4,5,6]"), MakeLinkedList("[1,2,3,5,6]"));
+        Test.Check(solution.DeleteMiddle, MakeLinkedList("[1,2,3,4,5,6,7]"), MakeLinkedList("[1,2,3,5,6,7]"));
+        Test.Check(solution.DeleteMiddle, MakeLinkedList("[1,2,3,4,5,6,7,8]"), MakeLinkedList("[1,2,3,4,6,7,8]"));
+        Test.Check(solution.DeleteMiddle, MakeLinkedList("[1,2,3,4,5,6,7,8,9]"), MakeLinkedList("[1,2,3,4,6,7,8,9]"));
+    }
+
+}
---
+++ problems/2095_DeleteMiddleNodeOfLinkedList/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace Quiz;
+
+public class Solution {
+
+    public ListNode DeleteMiddle(ListNode head) {
+        if (head.next == null) {
+            return null;
+        }
+        
+        ListNode current = head;
+        ListNode fast = head;
+
+        ListNode prev = null;
+        
+        while (fast != null && fast.next != null) {
+            prev = current;
+            current = current.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = current.next;
+        
+        return head;
+    }
+
+}
