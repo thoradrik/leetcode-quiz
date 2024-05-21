0f6fa76dbb191b4d754aeffc0f300928502b885c
thoradrik <thoradrik@gmail.com>
2024-05-21 14:17:23 +0500

    Solved 2073. Time Needed to Buy Tickets https://leetcode.com/problems/time-needed-to-buy-tickets/

---
+++ problems/2073_TimeNeededToBuyTickets/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Quiz;
+
+public class Program : ProgramBase {
+    
+    public static void Main() {
+        Solution solution = new Solution();
+
+        Test.Check(solution.TimeRequiredToBuy, IntArray("[1]"), 0, 1);
+        Test.Check(solution.TimeRequiredToBuy, IntArray("[2]"), 0, 2);
+        Test.Check(solution.TimeRequiredToBuy, IntArray("[10]"), 0, 10);
+        
+        Test.Check(solution.TimeRequiredToBuy, IntArray("[10,1]"), 0, 11);
+        Test.Check(solution.TimeRequiredToBuy, IntArray("[10,11]"), 0, 19);
+        
+        Test.Check(solution.TimeRequiredToBuy, IntArray("[10,1]"), 1, 2);
+        Test.Check(solution.TimeRequiredToBuy, IntArray("[10,9]"), 1, 18);
+        Test.Check(solution.TimeRequiredToBuy, IntArray("[10,10]"), 1, 20);
+        Test.Check(solution.TimeRequiredToBuy, IntArray("[10,11]"), 1, 21);
+        
+        Test.Check(solution.TimeRequiredToBuy, IntArray("[2,3,2]"), 2, 6);
+        Test.Check(solution.TimeRequiredToBuy, IntArray("[5,1,1,1]"), 0, 8);
+    }
+
+}
---
+++ problems/2073_TimeNeededToBuyTickets/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace Quiz;
+
+public class Solution {
+    
+    public int TimeRequiredToBuy(int[] tickets, int k) {
+        int mine = tickets[k];
+
+        int sum = mine;
+        
+        for (int i = 0; i < k; i++) {
+            sum += Math.Min(tickets[i], mine);
+        }
+        
+        for (int i = k + 1; i < tickets.Length; i++) {
+            sum += Math.Min(tickets[i], mine - 1);
+        }
+
+        return sum;
+    }
+    
+}
