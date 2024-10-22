b0e682857fc646d440b7c75a5a7ce6f4e5d114c8
thoradrik <thoradrik@gmail.com>
2024-10-22 13:10:16 +0500

    Solved 2652. Sum Multiples https://leetcode.com/problems/sum-multiples/description/

---
+++ problems/2652_SumMultiples/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Quiz;
+
+public class Program : ProgramBase {
+
+    public static void Main() {
+        var solution = new Solution();
+
+        Test.Check(solution.SumOfMultiples, 3, 3);
+        Test.Check(solution.SumOfMultiples, 4, 3);
+        Test.Check(solution.SumOfMultiples, 5, 8);
+        Test.Check(solution.SumOfMultiples, 6, 14);
+        Test.Check(solution.SumOfMultiples, 7, 21);
+        Test.Check(solution.SumOfMultiples, 9, 30);
+        Test.Check(solution.SumOfMultiples, 10, 40);
+        Test.Check(solution.SumOfMultiples, 11, 40);
+        Test.Check(solution.SumOfMultiples, 12, 52);
+        Test.Check(solution.SumOfMultiples, 13, 52);
+        Test.Check(solution.SumOfMultiples, 14, 66);
+        Test.Check(solution.SumOfMultiples, 15, 81);
+    }
+
+}
---
+++ problems/2652_SumMultiples/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace Quiz;
+
+public class Solution {
+    public int SumOfMultiples(int n) {
+        var result = 0;
+
+        for (var i = 3; i <= n; i++) {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+                result += i;
+            }
+        }
+
+        return result;
+    }
+}
