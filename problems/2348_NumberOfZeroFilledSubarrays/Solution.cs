f4f227f91301dfbc45cbd47975d9b31f177364cb
thoradrik <thoradrik@gmail.com>
2024-10-22 14:22:54 +0500

    Solved 2348. Number of Zero-Filled Subarrays https://leetcode.com/problems/number-of-zero-filled-subarrays/description/

---
+++ problems/2348_NumberOfZeroFilledSubarrays/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace Quiz;
+
+public class Solution {
+
+    public long ZeroFilledSubarray(int[] nums) {
+        var lengths = new int[nums.Length];
+
+        for (var i = 0; i < nums.Length; i++) {
+            if (nums[i] == 0) {
+                if (i > 0) {
+                    lengths[i] = lengths[i - 1] + 1;
+                    lengths[i - 1] = 0;
+                } else {
+                    lengths[i] = 1;
+                }
+            } else {
+                lengths[i] = 0;
+            }
+        }
+
+        long result = 0;
+
+        for (var i = 0; i < lengths.Length; i++) {
+            long length = lengths[i];
+            if (length > 0) {
+                result += length * (length + 1) / 2;
+            }
+        }
+
+        return result;
+    }
+
+}
