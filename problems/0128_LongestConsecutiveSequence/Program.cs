16388c9a83e1c6d234b4e912c189912ac2a28fa6
thoradrik <thoradrik@gmail.com>
2024-10-22 15:01:50 +0500

    Solved 128. Longest Consecutive Sequence https://leetcode.com/problems/longest-consecutive-sequence/description/

---
+++ problems/0128_LongestConsecutiveSequence/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Quiz;
+
+public class Program : ProgramBase {
+
+    public static void Main() {
+        var solution = new Solution();
+
+        Test.Check(solution.LongestConsecutive, IntArray("[]"), 0);
+        Test.Check(solution.LongestConsecutive, IntArray("[1]"), 1);
+        Test.Check(solution.LongestConsecutive, IntArray("[1,3]"), 1);
+        Test.Check(solution.LongestConsecutive, IntArray("[1,3,2]"), 3);
+        Test.Check(solution.LongestConsecutive, IntArray("[1,4,2]"), 2);
+        Test.Check(solution.LongestConsecutive, IntArray("[4,3,2,1]"), 4);
+        Test.Check(solution.LongestConsecutive, IntArray("[1,2,0,1]"), 3);
+        Test.Check(solution.LongestConsecutive, IntArray("[100,4,200,1,2]"), 2);
+        Test.Check(solution.LongestConsecutive, IntArray("[100,4,200,1,3,2]"), 4);
+        Test.Check(solution.LongestConsecutive, IntArray("[0,3,7,2,5,8,4,6,0,1]"), 9);
+    }
+
+}
---
+++ problems/0128_LongestConsecutiveSequence/Solution.cs
@@ -0,0 +1,50 @@
+﻿namespace Quiz;
+
+public class Solution {
+    public int LongestConsecutive_TrueOn_ButSlower(int[] nums) {
+        var set = new HashSet<int>(nums);
+
+        var max = 0;
+
+        for (var i = 0; i < nums.Length; i++) {
+            var prev_value = nums[i] - 1;
+            if (!set.Contains(prev_value)) {
+                var value = nums[i];
+                var length = 1;
+
+                while (set.Contains(value + length)) {
+                    length++;
+                }
+
+                max = Math.Max(max, length);
+            }
+        }
+
+        return max;
+    }
+
+    public int LongestConsecutive(int[] nums) {
+        Array.Sort(nums);
+
+        var length = new int[nums.Length];
+
+        var max = 0;
+
+        for (var i = 0; i < nums.Length; i++) {
+            if (i > 0) {
+                var diff = nums[i] - nums[i - 1];
+                if (diff <= 1) {
+                    length[i] = length[i - 1] + diff;
+                } else {
+                    length[i] = 1;
+                }
+            } else {
+                length[i] = 1;
+            }
+
+            max = Math.Max(max, length[i]);
+        }
+
+        return max;
+    }
+}
