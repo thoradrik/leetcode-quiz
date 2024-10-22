300ff42ebc931e147d858c24995842b617604892
thoradrik <thoradrik@gmail.com>
2024-10-22 13:58:56 +0500

    Solved 2527 Find Xor-Beauty of Array https://leetcode.com/problems/find-xor-beauty-of-array/description/

---
+++ problems/2527_XorBeautyOfArray/Solution.cs
@@ -0,0 +1,13 @@
+﻿namespace Quiz;
+
+public class Solution {
+    public int XorBeauty(int[] nums) {
+        var result = 0;
+
+        for (var i = 0; i < nums.Length; i++) {
+            result ^= nums[i];
+        }
+
+        return result;
+    }
+}
