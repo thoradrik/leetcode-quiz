70d0280c694adec3782bbc9f604d7635a2b16231
thoradrik <thoradrik@gmail.com>
2024-10-22 11:51:08 +0500

    Solved 13. Roman to Integer https://leetcode.com/problems/roman-to-integer/description/

---
+++ problems/0013_RomanToInteger/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Quiz;
+
+public class Program : ProgramBase {
+
+    public static void Main() {
+        var solution = new Solution();
+
+        Test.Check(solution.RomanToInt, "I", 1);
+        Test.Check(solution.RomanToInt, "II", 2);
+        Test.Check(solution.RomanToInt, "III", 3);
+        Test.Check(solution.RomanToInt, "IV", 4);
+        Test.Check(solution.RomanToInt, "V", 5);
+        Test.Check(solution.RomanToInt, "VI", 6);
+        Test.Check(solution.RomanToInt, "VII", 7);
+        Test.Check(solution.RomanToInt, "VIII", 8);
+        Test.Check(solution.RomanToInt, "IX", 9);
+        Test.Check(solution.RomanToInt, "X", 10);
+        Test.Check(solution.RomanToInt, "XI", 11);
+        Test.Check(solution.RomanToInt, "XII", 12);
+        Test.Check(solution.RomanToInt, "XIII", 13);
+        Test.Check(solution.RomanToInt, "XIV", 14);
+        Test.Check(solution.RomanToInt, "XV", 15);
+        Test.Check(solution.RomanToInt, "XXIV", 24);
+        Test.Check(solution.RomanToInt, "XXV", 25);
+        Test.Check(solution.RomanToInt, "XL", 40);
+        Test.Check(solution.RomanToInt, "L", 50);
+        Test.Check(solution.RomanToInt, "XC", 90);
+        Test.Check(solution.RomanToInt, "C", 100);
+        Test.Check(solution.RomanToInt, "CD", 400);
+        Test.Check(solution.RomanToInt, "CM", 900);
+        Test.Check(solution.RomanToInt, "D", 500);
+        Test.Check(solution.RomanToInt, "M", 1000);
+        Test.Check(solution.RomanToInt, "MCMXCIV", 1994);
+        Test.Check(solution.RomanToInt, "MMMDCCXLIX", 3749);
+    }
+
+}
---
+++ problems/0013_RomanToInteger/Solution.cs
@@ -0,0 +1,51 @@
+﻿namespace Quiz;
+
+public class Solution {
+    public int RomanToInt(string s) {
+        var result = 0;
+
+        var str = s.AsSpan();
+        while (str.Length > 0) {
+            if (TryToken(ref str, "CM")) {
+                result += 900;
+            } else if (TryToken(ref str, "CD")) {
+                result += 400;
+            } else if (TryToken(ref str, "XC")) {
+                result += 90;
+            } else if (TryToken(ref str, "XL")) {
+                result += 40;
+            } else if (TryToken(ref str, "IX")) {
+                result += 9;
+            } else if (TryToken(ref str, "IV")) {
+                result += 4;
+            } else if (TryToken(ref str, "M")) {
+                result += 1000;
+            } else if (TryToken(ref str, "D")) {
+                result += 500;
+            } else if (TryToken(ref str, "C")) {
+                result += 100;
+            } else if (TryToken(ref str, "L")) {
+                result += 50;
+            } else if (TryToken(ref str, "X")) {
+                result += 10;
+            } else if (TryToken(ref str, "V")) {
+                result += 5;
+            } else if (TryToken(ref str, "I")) {
+                result += 1;
+            } else {
+                throw new Exception("Invalid token");
+            }
+        }
+
+        return result;
+    }
+
+    private bool TryToken(ref ReadOnlySpan<char> s, ReadOnlySpan<char> template) {
+        if (s.Length >= template.Length && s.StartsWith(template)) {
+            s = s.Slice(template.Length);
+            return true;
+        }
+
+        return false;
+    }
+}
