5ba39414d234ceba2a4237e8b78105c6f4b2b421
thoradrik <thoradrik@gmail.com>
2024-10-22 14:38:44 +0500

    2414. Length of the Longest Alphabetical Continuous Substring https://leetcode.com/problems/length-of-the-longest-alphabetical-continuous-substring/description/

---
+++ problems/2414_LengthOfTheLongestAlphabeticalContinuousSubstring/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Quiz;
+
+public class Program : ProgramBase {
+
+    public static void Main() {
+        var solution = new Solution();
+
+        Test.Check(solution.LongestContinuousSubstring, "", 0);
+        Test.Check(solution.LongestContinuousSubstring, "a", 1);
+        Test.Check(solution.LongestContinuousSubstring, "aa", 1);
+        Test.Check(solution.LongestContinuousSubstring, "ab", 2);
+        Test.Check(solution.LongestContinuousSubstring, "ba", 1);
+        Test.Check(solution.LongestContinuousSubstring, "bac", 1);
+        Test.Check(solution.LongestContinuousSubstring, "bab", 2);
+        Test.Check(solution.LongestContinuousSubstring, "babc", 3);
+        Test.Check(solution.LongestContinuousSubstring, "babcba", 3);
+        Test.Check(solution.LongestContinuousSubstring, "babcba", 3);
+        Test.Check(solution.LongestContinuousSubstring, "bdbcba", 2);
+        Test.Check(solution.LongestContinuousSubstring, "abacaba", 2);
+        Test.Check(solution.LongestContinuousSubstring, "abcde", 5);
+        Test.Check(solution.LongestContinuousSubstring, "abcdetuvwxyz", 7);
+    }
+
+}
---
+++ problems/2414_LengthOfTheLongestAlphabeticalContinuousSubstring/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace Quiz;
+
+public class Solution {
+    public int LongestContinuousSubstring(string s) {
+        var str = s.AsSpan();
+
+        var length = new int[s.Length];
+
+        var max = 0;
+
+        for (var i = 0; i < str.Length; i++) {
+            if (i > 0) {
+                if (str[i] - str[i - 1] == 1) {
+                    length[i] = length[i - 1] + 1;
+                } else {
+                    length[i] = 1;
+                }
+            } else {
+                length[i] = 1;
+            }
+
+            max = Math.Max(max, length[i]);
+        }
+
+        return max;
+    }
+}
