5e917eee7b9ddb100798f2ec9dd576d185866c49
thoradrik <thoradrik@gmail.com>
2024-10-23 13:21:38 +0500

    Solved 1704. Determine if String Halves Are Alike https://leetcode.com/problems/determine-if-string-halves-are-alike/description/

---
+++ problems/1704_DetermineIfStringHalvesAreAlike/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Quiz;
+
+public class Program : ProgramBase {
+
+    public static void Main() {
+        var solution = new Solution();
+
+        Test.Check(solution.HalvesAreAlike, "ok", false);
+        Test.Check(solution.HalvesAreAlike, "zz", true);
+        Test.Check(solution.HalvesAreAlike, "book", true);
+        Test.Check(solution.HalvesAreAlike, "take", true);
+        Test.Check(solution.HalvesAreAlike, "textbook", false);
+        Test.Check(solution.HalvesAreAlike, "cookbook", true);
+        Test.Check(solution.HalvesAreAlike, "quicbaek", true);
+    }
+
+}
---
+++ problems/1704_DetermineIfStringHalvesAreAlike/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace Quiz;
+
+public class Solution {
+    public bool HalvesAreAlike(string s) {
+        var str = s.AsSpan();
+        var half = str.Length >> 1;
+        return CountVowels(str[..half]) == CountVowels(str[half..]);
+    }
+
+    private int CountVowels(ReadOnlySpan<char> str) {
+        var count = 0;
+
+        for (var i = 0; i < str.Length; i++) {
+            count += str[i] switch {
+                'a' => 1,
+                'e' => 1,
+                'i' => 1,
+                'o' => 1,
+                'u' => 1,
+                'A' => 1,
+                'E' => 1,
+                'I' => 1,
+                'O' => 1,
+                'U' => 1,
+                _ => 0
+            };
+        }
+
+        return count;
+    }
+}
