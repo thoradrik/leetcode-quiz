746bfe4eda993271e2407f3aada6cd62cb14f03a
thoradrik <thoradrik@gmail.com>
2025-02-18 15:04:57 +0500

    Solved 915. Partition Array into Disjoint Intervals https://leetcode.com/problems/partition-array-into-disjoint-intervals/

---
+++ problems/0915_PartitionArrayIntoDisjointIntervals/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Quiz;
+
+public class Program : ProgramBase {
+
+    public static void Main() {
+        var solution = new Solution();
+
+        Test.Check(solution.PartitionDisjoint, IntArray("[1,1]"), 1);
+        Test.Check(solution.PartitionDisjoint, IntArray("[1,2]"), 1);
+        Test.Check(solution.PartitionDisjoint, IntArray("[1,2,2]"), 1);
+        Test.Check(solution.PartitionDisjoint, IntArray("[1,1,2]"), 1);
+        Test.Check(solution.PartitionDisjoint, IntArray("[1,0,2]"), 2);
+        Test.Check(solution.PartitionDisjoint, IntArray("[1,0,2,3]"), 2);
+        Test.Check(solution.PartitionDisjoint, IntArray("[1,0,2,0,3]"), 4);
+        Test.Check(solution.PartitionDisjoint, IntArray("[1,0,2,1,0,3]"), 5);
+        Test.Check(solution.PartitionDisjoint, IntArray("[1,0,2,1,0,3,2]"), 5);
+        Test.Check(solution.PartitionDisjoint, IntArray("[1,0,2,1,0,3,2,3]"), 5);
+        Test.Check(solution.PartitionDisjoint, IntArray("[1,0,2,1,0,3,2,3,2,4]"), 5);
+        Test.Check(solution.PartitionDisjoint, IntArray("[1,0,2,1,0,3,2,3,1,4]"), 9);
+        Test.Check(solution.PartitionDisjoint, IntArray("[1,0,2,1,0,3,2,3,0,4]"), 9);
+        Test.Check(solution.PartitionDisjoint, IntArray("[5,0,3,8,6]"), 3);
+        Test.Check(solution.PartitionDisjoint, IntArray("[1,1,1,0,6,12]"), 4);
+    }
+
+}
---
+++ problems/0915_PartitionArrayIntoDisjointIntervals/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace Quiz;
+
+public class Solution {
+
+    public int PartitionDisjoint(int[] nums) {
+        var split_i = 0;
+        var split_max = nums[split_i];
+
+    try_next:
+        do {
+            split_i++;
+        } while (split_i < nums.Length - 1 && nums[split_i] < split_max);
+
+        var new_max = split_max;
+
+        for (var i = split_i; i < nums.Length; i++) {
+            var num_i = nums[i];
+
+            if (num_i < split_max) {
+                split_i = i;
+                split_max = new_max;
+                goto try_next;
+            }
+
+            if (num_i > new_max) {
+                new_max = num_i;
+            }
+        }
+
+        return split_i;
+    }
+
+}
